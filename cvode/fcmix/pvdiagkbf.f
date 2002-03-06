cad3023378267ac97876769886613c95d345060c
alanh <alanh@llnl.gov>
2002-03-06 22:18:27 +0000

    Added second case with reinitialization. (ACH)

--- cvode/fcmix/pvdiagkbf.f
+++ cvode/fcmix/pvdiagkbf.f
@@ -1,7 +1,8 @@
 C File: pvdiagkbf.f
 C Diagonal ODE example.  Stiff case, with diagonal preconditioner.
 C Uses FPVODE interfaces and FPVBBD interfaces.
-C Version of 11 January 2002
+C Solves problem twice -- with left and right preconditioning.
+C Version of 6 March 2002
 C
       IMPLICIT DOUBLE PRECISION (A-H, O-Z)
 C
@@ -52,28 +53,23 @@ C Set parameter alpha
       ALPHA  = 10.0D0
 C
       DO 10 I = 1,NLOCAL
-  10    Y(I) = 1.0D0
+ 10     Y(I) = 1.0D0
 C
       IF (MYPE .EQ. 0) THEN
-        WRITE(6,11) NEQ, ALPHA
-  11    FORMAT('Diagonal test problem, size NEQ =',I5,
-     1         '  parameter alpha =',F8.3)
-        WRITE(6,12)
-  12    FORMAT('  ydot_i = -alpha*i * y_i (i = 1,...,NEQ)'/)
-        WRITE(6,13)RTOL,ATOL
-  13    FORMAT('RTOL, ATOL =',2E10.1/)
-        WRITE(6,14)
-  14    FORMAT('Method is BDF/NEWTON/SPGMR'/
-     1         'Preconditioner is band-block-diagonal, using PVBBDPRE'/)
-        WRITE(6,15)NPES
-  15    FORMAT('Number of processors =',I3)
+        WRITE(6,15) NEQ, ALPHA, RTOL, ATOL, NPES
+ 15     FORMAT('Diagonal test problem, size NEQ =',I5,
+     1         '  parameter alpha = ',F8.3/
+     2         '  ydot_i = -alpha*i * y_i (i = 1,...,NEQ)'//
+     3         'RTOL, ATOL = ',2E10.1//'Method is BDF/NEWTON/SPGMR'/
+     4         'Preconditioner is band-block-diagonal, using PVBBDPRE'//
+     5         'Number of processors = ',I3)
         ENDIF
 C
       CALL FPVINITMPI (NLOCAL, NEQ, IER)
 C
       IF (IER .NE. 0) THEN
         WRITE(6,20) IER
-  20    FORMAT(///' FPVINITMPI returned IER =',I5)
+ 20     FORMAT(///' FPVINITMPI returned IER =',I5)
         STOP
         ENDIF
 C
@@ -82,7 +78,7 @@ C
 C
       IF (IER .NE. 0) THEN
         WRITE(6,30) IER
-  30    FORMAT(///' FPVMALLOC returned IER =',I5)
+ 30     FORMAT(///' FPVMALLOC returned IER =',I5)
         STOP
         ENDIF
 C
@@ -93,44 +89,50 @@ C
       CALL FPVBBDIN0 (MUDQ,MLDQ, MU,ML, 0.0D0, IPRE, IGS, 0, 0.0D0, IER)
       IF (IER .NE. 0) THEN
         WRITE(6,35) IER
-  35    FORMAT(///' FPVBBDIN0 returned IER =',I5)
+ 35     FORMAT(///' FPVBBDIN0 returned IER =',I5)
         STOP
         ENDIF
+      IF (MYPE.EQ. 0) WRITE(6,38)
+ 38   FORMAT(///'Preconditioning on left'//)
+C
+C Looping point for cases IPRE = 1 and 2.
+C
+ 40   CONTINUE
 C
 C Loop through tout values, call solver, print output, test for failure.
       TOUT = DTOUT
-      DO 70 IOUT = 1,NOUT
+      DO 60 IOUT = 1,NOUT
 C
         CALL FCVODE (TOUT, T, Y, ITASK, IER)
 C
-        IF (MYPE .EQ. 0) WRITE(6,40) T,IOPT(LNST),IOPT(LNFE)
-  40    FORMAT(/' t =',E10.2,5X,'no. steps =',I5,'   no. f-s =',I5)
+        IF (MYPE .EQ. 0) WRITE(6,45) T,IOPT(LNST),IOPT(LNFE)
+ 45     FORMAT(/' t =',E10.2,5X,'no. steps =',I5,'   no. f-s =',I5)
 C
         IF (IER .NE. 0) THEN
-          WRITE(6,60) IER
-  60      FORMAT(///' FCVODE returned IER =',I5)
+          WRITE(6,50) IER
+ 50       FORMAT(///' FCVODE returned IER =',I5)
           STOP
           ENDIF
 C
         TOUT = TOUT + DTOUT
-  70    CONTINUE
+ 60     CONTINUE
 C
 C Get max. absolute error in the local vector.
       ERMAX = 0.0D0
-      DO 75 I = 1,NLOCAL
+      DO 65 I = 1,NLOCAL
         ERRI  = Y(I) - EXP(-ALPHA*(MYPE*NLOCAL + I)*T)
         ERMAX = MAX(ERMAX,ABS(ERRI))
-  75    CONTINUE
+ 65     CONTINUE
 C Get global max. error from MPI_REDUCE call.
       CALL MPI_REDUCE (ERMAX, GERMAX, 1, MPI_DOUBLE_PRECISION, MPI_MAX,
      1                 0, MPI_COMM_WORLD, IER)
       IF (IER .NE. 0) THEN
-        WRITE(6,80) IER
-  80    FORMAT(///' MPI_REDUCE returned IER =',I5)
+        WRITE(6,70) IER
+ 70     FORMAT(///' MPI_REDUCE returned IER =',I5)
         STOP
         ENDIF
-      IF (MYPE .EQ. 0) WRITE(6,85) GERMAX
-  85  FORMAT(//'Max. absolute error is',E10.2)
+      IF (MYPE .EQ. 0) WRITE(6,75) GERMAX
+ 75   FORMAT(//'Max. absolute error is',E10.2)
 C
 C Print final statistics.
       IF (MYPE .EQ. 0) THEN
@@ -145,8 +147,8 @@ C Print final statistics.
       NCFN = IOPT(LNCF)
       NCFL = IOPT(LNCFL)
       NETF = IOPT(LNETF)
-      WRITE (6,90) NST,NFE,NPSET,NPE,NPS,NNI,NLI,AVDIM,NCFN,NCFL,NETF
-  90  FORMAT(//'Final statistics..'/
+      WRITE (6,80) NST,NFE,NPSET,NPE,NPS,NNI,NLI,AVDIM,NCFN,NCFL,NETF
+ 80   FORMAT(//'Final statistics..'/
      1 ' number of steps        =',I5,5X,'number of f evals.     =',I5/
      2 ' number of prec. setups =',I5/
      3 ' number of prec. evals. =',I5,5X,'number of prec. solves =',I5/
@@ -155,21 +157,40 @@ C Print final statistics.
      6 ' number of conv. failures..  nonlinear =',I3,'  linear =',I3/
      7 ' number of error test failures =',I3)
       CALL FPVBBDOPT (LENRPW, LENIPW, NGE)
-      WRITE (6,92) LENRPW, LENIPW, NGE
-  92  FORMAT(/'In PVBBDPRE: real/integer local work space sizes =',2I5/
+      WRITE (6,82) LENRPW, LENIPW, NGE
+ 82   FORMAT('In PVBBDPRE: real/integer local work space sizes =',2I5/
      1 ' number of g evals.     =',I5)
       ENDIF
 C
+C If IPRE = 1, re-initialize T, Y, and the solver, and loop for case IPRE = 2.
+C Otherwise jump to final block.
+      IF (IPRE .EQ. 2) GO TO 99
+      T = 0.0D0
+      DO 90 I = 1,NLOCAL
+ 90      Y(I) = 1.0D0
+      CALL FPVREINIT (T, Y, METH, ITMETH, IATOL, RTOL, ATOL,
+     1                INOPT, IOPT, ROPT, IER)
+      IF (IER .NE. 0) THEN
+        WRITE(6,91) IER
+ 91     FORMAT(///' FPVREINIT returned IER =',I5)
+        STOP
+        ENDIF
+      IPRE = 2
+      CALL FPVREINBBD0 (MUDQ,MLDQ, MU,ML, 0.0D0, IPRE,IGS, 0,0.0D0, IER)
+      IF (IER .NE. 0) THEN
+        WRITE(6,92) IER
+ 92     FORMAT(///' FPVREINBBD0 returned IER =',I5)
+        STOP
+        ENDIF
+      IF (MYPE .EQ. 0) WRITE (6,95)
+ 95   FORMAT(///60('-')///'Preconditioning on right'//)
+      GO TO 40
+C
 C Free the memory and finalize MPI.
-      CALL FPVBBDF
+ 99   CALL FPVBBDF
       CALL FCVFREE
       CALL FPVFREEMPI
       CALL MPI_FINALIZE(IER)
-      IF (IER .NE. 0) THEN
-        WRITE(6,95) IER
- 95     FORMAT(///' MPI_FINALIZE returned IER =',I5)
-        STOP
-        ENDIF
 C
       STOP
       END
