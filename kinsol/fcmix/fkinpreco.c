9d37d1899a90c18c287a898e636dff0d675f6fef
radu <serban1@llnl.gov>
2002-03-21 22:14:03 +0000

    Added prototype for Fortran routine (compilation warning) Changed macros to calls to N_VGetData

--- kinsol/fcmix/fkinpreco.c
+++ kinsol/fcmix/fkinpreco.c
@@ -1,8 +1,9 @@
 /*******************************************************************
  *                                                                 *
  * File          : fkinpreco.c                                     *
- * Programmers   : Allan G Taylor and Alan C. Hindmarsh @ LLNL     *
- * Version of    : 21 December 2001                                *
+ * Programmers   : Allan G Taylor, Alan C. Hindmarsh, and          *
+ *                 Radu Serban @ LLNL                              *
+ * Version of    : 8 March 2002                                    *
  *-----------------------------------------------------------------*
  * This C function KINPreco is to interface between routines that  *
  * call KINSpgmr(fkinspgmr20, fkinspgmr21) and the user-supplied   *
@@ -12,35 +13,42 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "llnltyps.h"  /* definitions of types real and integer            */
-#include "nvector.h"   /* definitions of type N_Vector and related macros  */
-#include "kinsol.h"    /* KINSOL constants and prototypes                  */
-#include "fkinsol.h"   /* prototypes of interfaces, global variables       */
+#include "llnltyps.h"  /* definitions of types real and integer      */
+#include "nvector.h"   /* definitions of type N_Vector               */
+#include "kinsol.h"    /* KINSOL constants and prototypes            */
+#include "fkinsol.h"   /* prototypes of interfaces, global variables */
 
-/***************************************************************************/
+/*********************************************************************/
 
+/* Prototypes of the Fortran routines */
+void K_PRECO(integer*, real*, real*, real*, real*, real*, real*, real*, long int*, int*);
 
-/* C function KINPreco to interface between KINSpgmr and KPRECO, the user-
-  supplied Fortran preconditioner setup routine. */
+/*********************************************************************/
+
+/* C function KINPreco to interface between KINSpgmr and KPRECO, 
+   the user-supplied Fortran preconditioner setup routine. */
 
 int KINPreco(integer Neq, N_Vector uu, N_Vector uscale, 
              N_Vector fval, N_Vector fscale,
-	     N_Vector vtemp1, N_Vector vtemp2,
-	     SysFn func, real u_round,
-	     long int *nfePtr, void *P_data)
+             N_Vector vtemp1, N_Vector vtemp2,
+             SysFn func, real u_round,
+             long int *nfePtr, void *P_data)
 {
   real *udata,*uscaledata, *fdata, *fscaledata, *vtemp1data, *vtemp2data;
   int retcode;
-
- udata        = N_VDATA(uu);
- uscaledata   = N_VDATA(uscale);
- fdata        = N_VDATA(fval);
- fscaledata   = N_VDATA(fscale);
- vtemp1data   = N_VDATA(vtemp1);
- vtemp2data   = N_VDATA(vtemp2);
+  
+ udata        = N_VGetData(uu);
+ uscaledata   = N_VGetData(uscale);
+ fdata        = N_VGetData(fval);
+ fscaledata   = N_VGetData(fscale);
+ vtemp1data   = N_VGetData(vtemp1);
+ vtemp2data   = N_VGetData(vtemp2);
  
  K_PRECO(&Neq, udata, uscaledata, fdata, fscaledata, vtemp1data, vtemp2data,
-       &u_round, nfePtr, &retcode);
+         &u_round, nfePtr, &retcode);
+
+ /* Note: there is no need to use N_VSetData since we are not getting back any
+    information that should go into an N_Vector */
 
  return(retcode);
 }
