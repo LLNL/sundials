5eb97a65fdcffe0d2afcbe8b0630fd5b7ed1e1e0
radu <serban1@llnl.gov>
2002-03-28 21:38:53 +0000

    Removed "Legal Notice"

--- kinsol/source/kinsol.c
+++ kinsol/source/kinsol.c
@@ -9,44 +9,6 @@
  * It is independent of the KINSol linear solver in use.          *
  *                                                                *
  *****************************************************************/
-/*......................................................................
-
-
-                            LEGAL NOTICES
-
-
-1. NOTICE
-
-This work was performed at the University of California, Lawrence
-Livermore National Laboratory (UC LLNL) under contract no.
-W-7405-ENG-48 (Contract 48) between the U.S. Department of Energy
-(DOE) and The Regents of the University of California (the University)
-for the operation of UC LLNL.  The rights of the Federal Government are 
-reserved under Contract 48 subject to the restrictions agreed upon by the
-DOE and University as allowed under DOE Acquisition Letter 97-1.
-
-
-2. DISCLAIMER
-
-This work was prepared as an account of work sponsored by an agency of
-the United States Government.  Neither the United States Government
-nor the University of California nor any of their empolyees makes any
-warranty, express or implied, or assumes any liability or
-responsibility for the accuracy, completeness, or usefulness of any
-information, apparatus, product, or process disclosed, or represents
-that its use would not infringe privately owned rights.  Reference
-herein to any specific commercial products, process, or service by
-trade name, trademark, manufacturer, or otherwise, does not
-necessarily constitute or imply its endorsement, recommendation, or
-favoring by the United States Government or the University of
-California.  The views and opinions of authors expressed herein do not
-necessarily state or reflect those of the United States Government or
-the University of California, and shall not be used for advertising or
-product endorsement purposes.
-
-*/
-
-
 
 /************************************************************/
 /******************* BEGIN Imports **************************/
