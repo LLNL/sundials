e7196e06ea4296e130676f8379dfea04ff300723
radu <serban1@llnl.gov>
2002-03-29 16:52:26 +0000

    The global var. F2C_machEnv is declared external here and should be defined by a Fortran interface implementation of the NVECTOR module.

--- shared/include/fcmixpar.h
+++ shared/include/fcmixpar.h
@@ -20,17 +20,32 @@
  *                 Some examples:                                            *
  *                   1 for Sun, DEC Alpha, SGI, IBM with -q extname, Meiko   *
  *                   0 for HP, IBM default                                   *
+ *                                                                           *
+ * Declarations:                                                             *
+ *                                                                           *
+ *   F2C_machEnv  A global variable used to communicate the machine          *
+ *                environment structure among the various routine in a       *
+ *                Fortran - C interface. It is declared extern here and must *
+ *                defined in the Fortran - C interface implementation file   *
+ *                of the NVECTOR module.                                     *  
  *****************************************************************************/  
+
+#ifdef __cplusplus     /* wrapper to enable C++ usage */
+extern "C" {
+#endif
 
 #ifndef _fcmixpar_h
 #define _fcmixpar_h
-
+  
 #define CRAY        0
 #define UNDERSCORE  1
 
 #include "nvector.h"
 
-M_Env F2C_machEnv;
+extern M_Env F2C_machEnv;
 
 #endif
 
+#ifdef __cplusplus
+}
+#endif
