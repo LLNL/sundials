a0130a114aac7b0cb34d18639dbfd7f6d1884356
alanh <alanh@llnl.gov>
2002-07-30 22:35:15 +0000

    General code cleanup; added error return tests; fixed overlong lines.

--- kinsol/fcmix/kindiagpf.f
+++ kinsol/fcmix/kindiagpf.f
@@ -1,37 +1,37 @@
-      program diagpf
+      program kindiagpf
 c   ***************************************************************************
-c   *                                                                         *
-c   * File: kindiagpf.f                                                       *
-c   * Programmers: Allan G. Taylor and Alan C. Hindmarsh @ LLNL               *
-c   * Version of 25 Mar 1999                                                  *
-c   *   simple diagonal test with Fortran interface, using user-supplied      *
-c   *   preconditioner setup and solve routines (supplied in Fortran, below)  *
-c   *   This example does a basic test of the code suite by solving the system*
-c   *                        f(u) = 0.  for                                   *
+c   * File        : kindiagpf.f                                               *
+c   * Programmers : Allan G. Taylor and Alan C. Hindmarsh @ LLNL              *
+c   * Version of  : 30 July 2002                                              *
+c   *   Simple diagonal test with Fortran interface, using user-supplied      *
+c   *   preconditioner setup and solve routines (supplied in Fortran, below). *
+c   *   This example does a basic test of the solver by solving the system    *
+c   *                        f(u) = 0   for                                   *
 c   *                        f(u) = u(i)^2 - i^2 .                            *
 c   *   No scaling is done.                                                   *
-c   *   execute line:  mpirun -np 4 kindiagpf                                 *
+c   *   An approximate diagonal preconditioner is used.                       *
+c   *   Execute line:  mpirun -np 4 kindiagpf                                 *
 c   *--------------------------------------------------------------------------
-c     the following include is required
+c   * Modified by Radu Serban to work with new serial NVECTOR, 12 March 2002. *
+c   *-------------------------------------------------------------------------*
+c     The following include is required
       include "mpif.h"
 
       parameter(localsize=32)
       integer neq, ier, size, globalstrat, rank
-      integer mype, npes, baseadd, nlocal, comm
+      integer mype, npes, baseadd, nlocal
       integer iopt(40)
       
       double precision fnormtol, scsteptol
       double precision ropt(40)
       double precision uu(localsize), scale(localsize)
       double precision constr(localsize)
-      double precision ri
       double precision pp
       
       common /pcom/ pp(localsize), mype, npes, baseadd, nlocal
 
-      comm = 0
-      neq = 4*localsize
-      nlocal = 32
+      nlocal = localsize
+      neq = 4*nlocal
       globalstrat = 0
       fnormtol = 0.00001
       scsteptol = 0.0001
@@ -39,33 +39,33 @@ c     the following include is required
       maxl = 10
       maxlrst = 2
       msbpre  = 5
-      mu = 0
-      ml = 0
 
-c     the user MUST call mpi_init , Fortran binding, for the fkinsol package
+c     The user MUST call mpi_init, Fortran binding, for the fkinsol package
 c     to work. The communicator, MPI_COMM_WORLD, is the only one common 
-c     between the Fortran and C bindings. So, in the following, the communicator
+c     between the Fortran and C bindings. So in the following, the communicator
 c     MPI_COMM_WORLD is used in calls to mpi_comm_size and mpi_comm_rank
 c     to determine the total number of processors and the rank (0 ... size-1) 
 c     number of this process.
       
       call mpi_init(ierr)
-c     * * * * * * * * * * * * * * * * * * * * * * *
       
       call fmenvinitp(nlocal, neq, ier)
-      if(ier .ne. 0) then
+      if (ier .ne. 0) then
+         write(6,1220),ier
+ 1220    format('fmenvinitp failed, ier =',i2)
          stop
       endif
       
       call mpi_comm_size(MPI_COMM_WORLD,size,ier)
       
-      if(size .ne. 4) then
-         write(6,1234)
- 1234    format(1x,' # of processors not 4. set to 4 and try again')
+      if (size .ne. 4) then
+         write(6,1230)
+ 1230    format(/'Number of processors not 4. Set to 4 and try again.')
          call fmenvfreep
          stop
       endif
       npes = size
+
       call mpi_comm_rank(MPI_COMM_WORLD, rank, ier)
       
       mype = rank
@@ -73,151 +73,132 @@ c     * * * * * * * * * * * * * * * * * * * * * * *
       
       call fkinmalloc(neq, ier)
       
-      if(ier .ne. 0)then
-         write(6,1230)ier
- 1230    format('fpkinmalloc failed',i2)
+      if (ier .ne. 0) then
+         write(6,1231)ier
+ 1231    format('fkinmalloc failed, ier =',i2)
          stop
       endif
       
-      call fkinspgmr20(maxl, maxlrst, msbpre)
+      call fkinspgmr20(maxl, maxlrst, msbpre, ier)
+
+      if (ier .ne. 0) then
+         write(6,1232)ier
+ 1232    format('fkinspgmr20 failed, ier =',i2)
+         stop
+      endif
       
       if(mype .eq. 0)write(6,1240)
- 1240 format(//,'kindiagpf demo case. This kinsol example code does a')
-      if(mype .eq. 0) write(6,1241)
- 1241 format('128 eqn diagonal algebraic system. Its purpose is to ')
-      if(mype .eq. 0)write(6,1242)
- 1242 format('demonstrate the use of the Fortran interface in a')
-      if(mype .eq. 0)write(6,1243)
- 1243 format('parallel environment.',//)
-      if(mype .eq. 0)write(6,1244)
- 1244 format('globalstrategy = INEXACT_NEWTON' ,//)
-      
-      do 20 ii = 1 , 32
+ 1240 format('Example program kindiagpf'/' This fkinsol example code',
+     1       ' solves a 128 eqn diagonal algebraic system.'/
+     2       ' Its purpose is to demonstrate the use of the Fortran',
+     3       ' interface'/' in a parallel environment.'/
+     4       ' globalstrategy = INEXACT_NEWTON'/)
+
+      do 20 ii = 1,nlocal
          i = ii + baseadd
-         ri = i
-         uu(ii) = 2*ri
-         scale(ii) = 1.
-         constr(ii) = 0.
+         uu(ii) = 2.0*i
+         scale(ii) = 1.0
+         constr(ii) = 0.0
  20   continue
       
       call fkinsol(neq, uu, 0, scale, scale, fnormtol, 
-     1     scsteptol, constr, inopt, iopt, ropt, ier)
+     1             scsteptol, constr, inopt, iopt, ropt, ier)
+
+      if (mype .eq. 0) write(6,1245)ier
+ 1245 format(/' fkinsol return code is ',i5)
+
+      if (mype .eq. 0) write(6,1246)
+ 1246 format(/' The resultant values of uu (processor 0) are:'//)
       
-      
-      if(mype .eq. 0) write(6,1245)ier
- 1245 format(1x,' return code is ',i5)
-      
-      
-      if(mype .eq. 0)write(6,1246)
- 1246 format(1x,'The resultant values of uu (processor 0) are:',///)
-      
-      do 30 i = 1, 32, 4
+      do 30 i = 1,nlocal,4
          if(mype .eq. 0) write(6,1256)i+baseadd, uu(i), uu(i+1),
      1        uu(i+2), uu(i+3)
  1256    format(i4,4(1x,f10.6))
  30   continue
-      
-      
-      if(mype .eq. 0) write(6,1267) iopt(4), iopt(11), iopt(5), 
-     1     iopt(12), iopt(13),  iopt(14)
- 1267 format(1x,'nni=',i4,' nli=',i4,' nfe=',i4,' npe=',i4,
-     1     ' nps=',i4,' ncfl=',i4)
-      
+
+      if (mype .eq. 0) write(6,1267)iopt(4),iopt(11),iopt(5),iopt(12),
+     1                 iopt(13),iopt(14)
+ 1267 format(//' nni=',i4,',  nli=',i4,',  nfe=',i4,',  npe=',i4,
+     1       ',  nps=',i4,',  ncfl=',i4)
+
       call fkinfree
-      
       call fmenvfreep
       
-c     an explicit call to mpi_finalize (Fortran binding) is required by 
+c     An explicit call to mpi_finalize (Fortran binding) is required by 
 c     the constructs used in fkinsol. 
-      
       call mpi_finalize(ier)
       
       stop
-      
       end
       
-c     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-c     The function defining the system f(u) = 0. must be defined by a Fortran
-c     function of the following form. 
+
+c * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+c     The function defining the system f(u) = 0 must be defined by a Fortran
+c     function with the following name and form. 
       
       subroutine KFUN(neq, uu, fval)
-c     
+
       double precision fval(*), uu(*)
       
-c     common pcom * * * * * * * * * * * * * * * * * * * * * * * * * * *
       parameter(localsize=32)
       double precision pp
       integer mype, npes, baseadd, nlocal
       common /pcom/ pp(localsize), mype, npes, baseadd, nlocal
-c     common pcom * * * * * * * * * * * * * * * * * * * * * * * * * * *
       
-      do 10 i = 1 , nlocal
-         fval(i) = uu(i)*uu(i) - (i+baseadd)*(i+baseadd)
- 10   continue
+      do 10 i = 1,nlocal
+ 10      fval(i) = uu(i)*uu(i) - (i+baseadd)*(i+baseadd)
       
       return
       end
       
       
-c     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-c     the routine kpreco is the preconditioner setup routine. It is required that
-c     that specific name be used in order that the c code can find and link to it
-c     the argument list must also be as illustrated below:
+c * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+c     The routine kpreco is the preconditioner setup routine. It must have
+c     that specific name be used in order that the c code can find and link
+c     to it.  The argument list must also be as illustrated below:
       
       subroutine kpreco(neq, udata, uscale, fdata, fscale, 
-     1     vtemp1, vtemp2, uround, nfe, ier)
+     1                  vtemp1, vtemp2, uround, nfe, ier)
       
       integer nfe, ier
-      
       double precision udata(*), uscale(*), fdata(*), fscale(*)
-      double precision vtemp1(*), vtemp2(*) 
+      double precision vtemp1(*), vtemp2(*), uround
       
-      double precision uround
-      
-c     common pcom * * * * * * * * * * * * * * * * * * * * * * * * * * *
       parameter(localsize=32)
       double precision pp
       integer mype, npes, baseadd, nlocal
       common /pcom/ pp(localsize), mype, npes, baseadd, nlocal
-c     common pcom * * * * * * * * * * * * * * * * * * * * * * * * * * *
-      
+            
+      do 10 i = 1,nlocal
+ 10      pp(i) = 0.5/(udata(i)+5.0)
+
       ier = 0
-      
-      do 10 i = 1 , nlocal
-         pp(i) = 0.5/(udata(i)+5.)
- 10   continue
       
       return
       end
       
       
-c     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-c     the routine kpsol is the preconditioner solve routine. It is required that
-c     that specific name be used in order that the c code can find and link to it
-c     the argument list must also be as illustrated below:
+c * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+c     The routine kpsol is the preconditioner solve routine. It must have
+c     that specific name be used in order that the c code can find and link
+c     to it.  The argument list must also be as illustrated below:
       
       subroutine kpsol(neq, udata, uscale, fdata, fscale, 
-     1     vtem, ftem, uround, nfe, ier)
+     1                 vv, ftem, uround, nfe, ier)
       
       integer nfe, ier
-      
       double precision udata(*), uscale(*), fdata(*), fscale(*)
-      double precision vtem(*), ftem(*) 
+      double precision vv(*), ftem(*), uround
       
-      double precision uround
-      
-c     common pcom * * * * * * * * * * * * * * * * * * * * * * * * * * *
       parameter(localsize=32)
       double precision pp
       integer mype, npes, baseadd, nlocal
       common /pcom/ pp(localsize), mype, npes, baseadd, nlocal
-c     common pcom * * * * * * * * * * * * * * * * * * * * * * * * * * *
       
+      do 10 i = 1,nlocal
+ 10      vv(i) = vv(i) * pp(i)
+
       ier = 0
-      
-      do 10 i = 1 , nlocal
-         vtem(i) = vtem(i) * pp(i)
- 10   continue
-      
+
       return
       end
--- kinsol/fcmix/kindiagsf.f
+++ kinsol/fcmix/kindiagsf.f
@@ -1,18 +1,18 @@
-      program diagsf
+      program kindiagsf
 c   ***************************************************************************
-c   *                                                                         *
 c   * File        : kindiagsf.f                                               *
 c   * Programmers : Allan G. Taylor and Alan C. Hindmarsh @ LLNL              *
-c   * Version of  :12 March 2002                                              *
-c   *   simple diagonal test with Fortran interface, using user-supplied      *
-c   *   preconditioner setup and solve routines (supplied in Fortran, below)  *
-c   *   This example does a basic test of the code suite by solving the system*
-c   *                        f(u) = 0.  for                                   *
+c   * Version of  : 30 July 2002                                              *
+c   *   Simple diagonal test with Fortran interface, using user-supplied      *
+c   *   preconditioner setup and solve routines (supplied in Fortran, below). *
+c   *   This example does a basic test of the solver by solving the system    *
+c   *                        f(u) = 0   for                                   *
 c   *                        f(u) = u(i)^2 - i^2 .                            *
 c   *   No scaling is done.                                                   *
-c   *   execute line: kindiagsf                                               *
+c   *   An approximate diagonal preconditioner is used.                       *
+c   *   Execute line: kindiagsf                                               *
 c   *-------------------------------------------------------------------------*
-c   * Modified by Radu Serban to work with new serial nvector (12/3/2002)     *
+c   * Modified by Radu Serban to work with new serial NVECTOR, 12 March 2002. *
 c   *-------------------------------------------------------------------------*
 
       integer PROBSIZE
@@ -36,146 +36,126 @@ c   *-------------------------------------------------------------------------*
       maxl = 10
       maxlrst = 2
       msbpre  = 5
-      mu = 0
-      ml = 0
 
 c * * * * * * * * * * * * * * * * * * * * * *
 
       call fmenvinits(neq, ier)
-      if(ier .ne. 0) then
+      if (ier .ne. 0) then
+         write(6,1220),ier
+ 1220    format('fmenvinits failed, ier =',i2)
          stop
       endif
 
       call fkinmalloc(neq, ier)
-      if(ier .ne. 0)then
+      if (ier .ne. 0) then
+         write(6,1230),ier
+ 1230    format('fkinmalloc failed, ier =',i2)
          stop
       endif
 
-      call fkinspgmr20(maxl, maxlrst, msbpre)
+      call fkinspgmr20(maxl, maxlrst, msbpre, ier)
 
-      do 20 i = 1 , PROBSIZE
-         uu(i) = 2.*i
-         scale(i) = 1.
-         constr(i) = 0.
- 20   continue
+      if (ier .ne. 0) then
+         write(6,1232)ier
+ 1232    format('fkinspgmr20 failed, ier =',i2)
+         stop
+      endif
+
+      do 20 i = 1,neq
+         uu(i) = 2.0*i
+         scale(i) = 1.0
+         constr(i) = 0.0
+  20  continue
+
+      write(6,1240)
+ 1240 format('Example program kindiagsf'/' This fkinsol example code',
+     1       ' solves a 128 eqn diagonal algebraic system.'/
+     2       ' Its purpose is to demonstrate the use of the Fortran',
+     3       ' interface'/' in a serial environment.'/
+     4       ' globalstrategy = INEXACT_NEWTON'/)
+
       call fkinsol(neq, uu, 0, scale, scale, fnormtol, 
-     1     scsteptol, constr, inopt, iopt, ropt, ier)
+     1             scsteptol, constr, inopt, iopt, ropt, ier)
 
-       write(6,1240)
- 1240  format('kindiagsf example case. This kinsol example code does a')
-       write(6,1241)
- 1241  format('128 eqn diagonal algebraic system. Its purpose is to ')
-       write(6,1242)
- 1242  format('demonstrate the use of the Fortran interface in a')
-       write(6,1243)
- 1243  format('serial environment.')
-       write(6,1244)
- 1244  format('globalstrategy = INEXACT_NEWTON')
-       
-       write(6,1245)ier
- 1245  format(1x,' return code is ',i5)
+      write(6,1245)ier
+ 1245 format(/' fkinsol return code is ',i5)
 
+      write(6,1246)
+ 1246 format(/' The resultant values of uu are:'/)
 
-       write(6,1246)
- 1246  format(1x,'The resultant values of uu are:',///)
-       
-       do 30 i = 1, PROBSIZE, 4
-          write(6,1256)i, uu(i), uu(i+1), uu(i+2), uu(i+3)
- 1256     format(i4,4(1x,f10.6))
- 30    continue
-       
-       
-       write(6,1267) iopt(4), iopt(11), iopt(5), 
-     1      iopt(12), iopt(13),  iopt(14)
- 1267  format(1x,'nni=',i4,' nli=',i4,' nfe=',i4,' npe=',i4,
-     1      ' nps=',i4,' ncfl=',i4)
-       
-       call fkinfree
-       call fmenvfrees
-       
-       stop
-       
-       end
+      do 30 i = 1,neq,4
+         write(6,1256)i, uu(i), uu(i+1), uu(i+2), uu(i+3)
+ 1256    format(i4,4(1x,f10.6))
+ 30   continue
+
+      write(6,1267)iopt(4),iopt(11),iopt(5),iopt(12),iopt(13),iopt(14)
+ 1267 format(//' nni=',i4,',  nli=',i4,',  nfe=',i4,',  npe=',i4,
+     1       ',  nps=',i4,',  ncfl=',i4)
+
+      call fkinfree
+      call fmenvfrees
+
+      stop
+      end
       
-c     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-c     The function defining the system f(u) = 0. must be defined by a Fortran
-c     function of the following form. 
+c * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+c     The function defining the system f(u) = 0 must be defined by a Fortran
+c     function of the following form.
       
       subroutine KFUN(neq, uu, fval)
-c     
+
       double precision fval(*), uu(*)
-      
-      parameter(PROBSIZE=128)
-      
-      
-      do 10 i = 1 , PROBSIZE
-         fval(i) = uu(i)*uu(i) - i*i
- 10   continue
+
+      do 10 i = 1,neq
+ 10      fval(i) = uu(i)*uu(i) - i*i
       
       return
       end
       
       
-c     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-c     the routine kpreco is the preconditioner setup routine. It is required that
-c     that specific name be used in order that the c code can find and link to it
-c     the argument list must also be as illustrated below:
+c * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+c     The routine kpreco is the preconditioner setup routine. It must have
+c     that specific name be used in order that the c code can find and link
+c     to it.  The argument list must also be as illustrated below:
       
       subroutine kpreco(neq, udata, uscale, fdata, fscale, 
-     1     vtemp1, vtemp2, uround, nfe, ier)
+     1                  vtemp1, vtemp2, uround, nfe, ier)
       
       integer nfe, ier
-      
       double precision udata(*), uscale(*), fdata(*), fscale(*)
-      double precision vtemp1(*), vtemp2(*) 
+      double precision vtemp1(*), vtemp2(*), uround
       
-      double precision uround
-      
-c     common pcom * * * * * * * * * * * * * * * * * * * * * * * * * * *
-      integer PROBSIZE
-      parameter(PROBSIZE=128)
       double precision pp
-      common /pcom/ pp(PROBSIZE)
-c     common pcom * * * * * * * * * * * * * * * * * * * * * * * * * * *
+      common /pcom/ pp(128)
+      
+      do 10 i = 1,neq
+ 10      pp(i) = 0.5/(udata(i)+5.)
       
       ier = 0
-      
-      do 10 i = 1 , PROBSIZE
-         pp(i) = 0.5/(udata(i)+5.)
- 10   continue
       
       return
       end
       
       
-c     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-c     the routine kpsol is the preconditioner solve routine. It is required that
-c     that specific name be used in order that the c code can find and link to it
-c     the argument list must also be as illustrated below:
+c * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+c     The routine kpsol is the preconditioner solve routine. It must have
+c     that specific name be used in order that the c code can find and link
+c     to it.  The argument list must also be as illustrated below:
       
       subroutine kpsol(neq, udata, uscale, fdata, fscale, 
-     1     vtem, ftem, uround, nfe, ier)
+     1                 vv, ftem, uround, nfe, ier)
       
       integer nfe, ier
-      
       double precision udata(*), uscale(*), fdata(*), fscale(*)
-      double precision vtem(*), ftem(*) 
+      double precision vv(*), ftem(*), uround
       
-      double precision uround
-      
-c     common pcom * * * * * * * * * * * * * * * * * * * * * * * * * * *
-      integer PROBSIZE
-      parameter(PROBSIZE=128)
       double precision pp
-      common /pcom/ pp(PROBSIZE)
-c     common pcom * * * * * * * * * * * * * * * * * * * * * * * * * * *
+      common /pcom/ pp(128)
+      
+      do 10 i = 1,neq
+ 10      vv(i) = vv(i) * pp(i)
       
       ier = 0
       
-      do 10 i = 1 , PROBSIZE
-         vtem(i) = vtem(i) * pp(i)
- 10   continue
-      
       return
       end
-      
