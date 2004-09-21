d18f9bfb6ea3df9b6ca1a56ea8b77217ba20ca90
alanh <alanh@llnl.gov>
2004-09-21 22:58:28 +0000

    Added routine FCVROOTINFO to return rootsfound.  HAS BUG!!!

--- cvode/fcmix/fcvroot.c
+++ cvode/fcmix/fcvroot.c
@@ -1,7 +1,7 @@
 /*
  * -----------------------------------------------------------------
- * $Revision: 1.5 $
- * $Date: 2004-07-22 22:54:43 $
+ * $Revision: 1.6 $
+ * $Date: 2004-09-21 22:58:28 $
  * ----------------------------------------------------------------- 
  * Programmer(s): Aaron Collier @ LLNL
  * -----------------------------------------------------------------
@@ -32,6 +32,16 @@ void FCV_ROOTINIT(int *nrtfn, int *ier)
 {
   *ier = CVodeRootInit(CV_cvodemem, (RootFn) FCVrootfunc, *nrtfn);
   CV_nrtfn = *nrtfn;
+  return; 
+}
+
+/***************************************************************************/
+
+void FCV_ROOTINFO(int *nrtfn, int **info, int *ier)
+{
+  int *rootsfound, i;
+  *ier = CVodeGetRootInfo(CV_cvodemem, &rootsfound);
+  for (i = 0; i < nrtfn; i++) info[i] = rootsfound[i];
   return; 
 }
 
