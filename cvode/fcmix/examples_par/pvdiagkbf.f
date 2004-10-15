e775922b590de8963547a89c7ab79d3def47142a
collier6 <collier6@llnl.gov>
2004-10-15 20:20:17 +0000

    Made the following changes:
    (1) added "implicit none" directive where possible and explicitly declared all variables
    (2) changed "integer" to "integer*4" for variables corresponding to 'long int' C-type
    (3) explicitly declared all floating-point values as #d# (forces use of double precision)
    (4) modified to use appopriate intrinsic functions/procedures
    (5) updated sample outputs

--- cvode/fcmix/examples_par/pvdiagkbf.f
+++ cvode/fcmix/examples_par/pvdiagkbf.f
@@ -1,44 +1,51 @@
 C     ----------------------------------------------------------------
-C     $Revision: 1.15 $
-C     $Date: 2004-10-12 21:37:39 $
+C     $Revision: 1.16 $
+C     $Date: 2004-10-15 20:20:16 $
 C     ----------------------------------------------------------------
 C     Diagonal ODE example.  Stiff case, with diagonal preconditioner.
 C     Uses FCVODE interfaces and FCVBBD interfaces.
 C     Solves problem twice -- with left and right preconditioning.
 C     ----------------------------------------------------------------
 C     
-      IMPLICIT DOUBLE PRECISION (A-H, O-Z)
-C     
 C     Include MPI-Fortran header file for MPI_COMM_WORLD, MPI types.
       
       INCLUDE "mpif.h"
 C     
+      INTEGER NOUT, LNST, LNFE, LNSETUP, LNNI, LNCF, LNETF, LNPE
+      INTEGER LNLI, LNPS, LNCFL, MYPE, IER, NPES, METH, ITMETH
+      INTEGER IATOL, INOPT, ITASK, IPRE, IGS, IOUT
       INTEGER*4 IOPT(40)
+      INTEGER*4 NEQ, NLOCAL, I, MUDQ, MLDQ, MU, ML, NETF
+      INTEGER*4 NST, NFE, NPSET, NPE, NPS, NNI, NLI, NCFN, NCFL
+      INTEGER*4 LENRPW, LENIPW, NGE
+      DOUBLE PRECISION ALPHA, TOUT, ERMAX, AVDIM
+      DOUBLE PRECISION ATOL, ERRI, RTOL, GERMAX, DTOUT, Y, ROPT, T
       DIMENSION Y(1024), ROPT(40)
-      DATA ATOL/1.0E-10/, RTOL/1.0E-5/, DTOUT/0.1D0/, NOUT/10/
-      DATA LNST/4/, LNFE/5/, LNSETUP/6/, LNNI/7/, LNCF/8/, LNETF/9/,
-     &     LNPE/18/, LNLI/19/, LNPS/20/, LNCFL/21/
 C     
-      COMMON /PCOM/ ALPHA, MYPE, NLOCAL
+      DATA ATOL/1.0D-10/, RTOL/1.0D-5/, DTOUT/0.1D0/, NOUT/10/
+      DATA LNST/4/, LNFE/5/, LNSETUP/6/, LNNI/7/, LNCF/8/, LNETF/9/,
+     1     LNPE/18/, LNLI/19/, LNPS/20/, LNCFL/21/
+C
+      COMMON /PCOM/ ALPHA, NLOCAL, MYPE
 C     
 C     Get NPES and MYPE.  Requires initialization of MPI.
       CALL MPI_INIT(IER)
       IF (IER .NE. 0) THEN
          WRITE(6,5) IER
- 5       FORMAT(///' MPI_ERROR: MPI_INIT returned IER = ',I5)
+ 5       FORMAT(///' MPI_ERROR: MPI_INIT returned IER = ', I5)
          STOP
       ENDIF
       CALL MPI_COMM_SIZE(MPI_COMM_WORLD, NPES, IER)
       IF (IER .NE. 0) THEN
          WRITE(6,6) IER
- 6       FORMAT(///' MPI_ERROR: MPI_COMM_SIZE returned IER = ',I5)
+ 6       FORMAT(///' MPI_ERROR: MPI_COMM_SIZE returned IER = ', I5)
          CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
          STOP
       ENDIF
       CALL MPI_COMM_RANK(MPI_COMM_WORLD, MYPE, IER)
       IF (IER .NE. 0) THEN
          WRITE(6,7) IER
- 7       FORMAT(///' MPI_ERROR: MPI_COMM_RANK returned IER = ',I5)
+ 7       FORMAT(///' MPI_ERROR: MPI_COMM_RANK returned IER = ', I5)
          CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
          STOP
       ENDIF
@@ -46,7 +53,7 @@ C     Get NPES and MYPE.  Requires initialization of MPI.
 C     
 C     Set input arguments.
       NLOCAL = 10
-      NEQ = NPES*NLOCAL
+      NEQ = NPES * NLOCAL
       T = 0.0D0
       METH = 2
       ITMETH = 2
@@ -58,35 +65,36 @@ C     Set input arguments.
 C     Set parameter alpha
       ALPHA  = 10.0D0
 C     
-      DO I = 1,NLOCAL
+      DO I = 1, NLOCAL
          Y(I) = 1.0D0
       ENDDO
 C     
       IF (MYPE .EQ. 0) THEN
          WRITE(6,15) NEQ, ALPHA, RTOL, ATOL, NPES
- 15      FORMAT(/'Diagonal test problem, size NEQ =',I5,
-     &        '  parameter alpha = ',F8.3/
-     &        '  ydot_i = -alpha*i * y_i (i = 1,...,NEQ)'/
-     &        'RTOL, ATOL = ',2E10.1/'Method is BDF/NEWTON/SPGMR'/
-     &        'Preconditioner is band-block-diagonal, using CVBBDPRE'/
-     &        'Number of processors = ',I3)
+ 15      FORMAT('Diagonal test problem:'//' NEQ = ', I3, /
+     &          ' parameter alpha = ', F8.3/
+     &          ' ydot_i = -alpha*i * y_i (i = 1,...,NEQ)'/
+     &          ' RTOL, ATOL = ', 2E10.1/
+     &          ' Method is BDF/NEWTON/SPGMR'/
+     &          ' Preconditioner is band-block-diagonal, using CVBBDPRE'
+     &          /' Number of processors = ', I3/)
       ENDIF
 C     
-      CALL FNVINITP (NLOCAL, NEQ, IER)
+      CALL FNVINITP(NLOCAL, NEQ, IER)
 C     
       IF (IER .NE. 0) THEN
          WRITE(6,20) IER
- 20      FORMAT(///' SUNDIALS_ERROR: FNVINITP returned IER = ',I5)
+ 20      FORMAT(///' SUNDIALS_ERROR: FNVINITP returned IER = ', I5)
          CALL MPI_FINALIZE(IER)
          STOP
       ENDIF
 C     
-      CALL FCVMALLOC (T, Y, METH, ITMETH, IATOL, RTOL, ATOL,
-     &     INOPT, IOPT, ROPT, IER)
+      CALL FCVMALLOC(T, Y, METH, ITMETH, IATOL, RTOL, ATOL,
+     &               INOPT, IOPT, ROPT, IER)
 C     
       IF (IER .NE. 0) THEN
          WRITE(6,30) IER
- 30      FORMAT(///' SUNDIALS_ERROR: FCVMALLOC returned IER = ',I5)
+ 30      FORMAT(///' SUNDIALS_ERROR: FCVMALLOC returned IER = ', I5)
          CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
          STOP
       ENDIF
@@ -95,23 +103,23 @@ C
       MLDQ = 0
       MU = 0
       ML = 0
-      CALL FCVBBDINIT (NLOCAL, MUDQ, MLDQ, MU, ML, 0.0D0, IER) 
+      CALL FCVBBDINIT(NLOCAL, MUDQ, MLDQ, MU, ML, 0.0D0, IER)
       IF (IER .NE. 0) THEN
          WRITE(6,35) IER
- 35      FORMAT(///' SUNDIALS_ERROR: FCVBBDINIT returned IER = ',I5)
+ 35      FORMAT(///' SUNDIALS_ERROR: FCVBBDINIT returned IER = ', I5)
          CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
          STOP
       ENDIF
-
+C
       CALL FCVBBDSPGMR(IPRE, IGS, 0, 0.0D0, IER)
       IF (IER .NE. 0) THEN
          WRITE(6,36) IER
- 36      FORMAT(///' SUNDIALS_ERROR: FCVBBDSPGMR returned IER = ',I5)
+ 36      FORMAT(///' SUNDIALS_ERROR: FCVBBDSPGMR returned IER = ', I5)
          CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
          STOP
       ENDIF
-
-      IF (MYPE.EQ. 0) WRITE(6,38)
+C
+      IF (MYPE .EQ. 0) WRITE(6,38)
  38   FORMAT(/'Preconditioning on left'/)
 C     
 C     Looping point for cases IPRE = 1 and 2.
@@ -120,17 +128,18 @@ C
 C     
 C     Loop through tout values, call solver, print output, test for failure.
       TOUT = DTOUT
-      DO 60 IOUT = 1,NOUT
+      DO 60 IOUT = 1, NOUT
 C     
-         CALL FCVODE (TOUT, T, Y, ITASK, IER)
+         CALL FCVODE(TOUT, T, Y, ITASK, IER)
 C     
-         IF (MYPE .EQ. 0) WRITE(6,45) T,IOPT(LNST),IOPT(LNFE)
- 45      FORMAT(' t =',E10.2,5X,'no. steps =',I5,'   no. f-s =',I5)
+         IF (MYPE .EQ. 0) WRITE(6,45) T, IOPT(LNST), IOPT(LNFE)
+ 45      FORMAT(' t = ', E10.2, 5X, 'no. steps = ', I5,
+     &          '   no. f-s = ', I5)
 C     
          IF (IER .NE. 0) THEN
             WRITE(6,50) IER, IOPT(26)
- 50         FORMAT(///' SUNDIALS_ERROR: FCVODE returned IER = ',I5,/,
-     &              '                 Linear Solver returned IER = ',I5)
+ 50         FORMAT(///' SUNDIALS_ERROR: FCVODE returned IER = ', I5, /,
+     &             '                 Linear Solver returned IER = ', I5)
             CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
             STOP
          ENDIF
@@ -140,21 +149,21 @@ C
 C     
 C     Get max. absolute error in the local vector.
       ERMAX = 0.0D0
-      DO 65 I = 1,NLOCAL
-         ERRI  = Y(I) - EXP(-ALPHA*(MYPE*NLOCAL + I)*T)
-         ERMAX = MAX(ERMAX,ABS(ERRI))
+      DO 65 I = 1, NLOCAL
+         ERRI  = Y(I) - DEXP(-ALPHA * (MYPE * NLOCAL + I) * T)
+         ERMAX = DMAX1(ERMAX, DABS(ERRI))
  65   CONTINUE
 C     Get global max. error from MPI_REDUCE call.
-      CALL MPI_REDUCE (ERMAX, GERMAX, 1, MPI_DOUBLE_PRECISION, MPI_MAX,
-     &     0, MPI_COMM_WORLD, IER)
+      CALL MPI_REDUCE(ERMAX, GERMAX, 1, MPI_DOUBLE_PRECISION, MPI_MAX,
+     &                0, MPI_COMM_WORLD, IER)
       IF (IER .NE. 0) THEN
          WRITE(6,70) IER
- 70      FORMAT(///' MPI_ERROR: MPI_REDUCE returned IER = ',I5)
+ 70      FORMAT(///' MPI_ERROR: MPI_REDUCE returned IER = ', I5)
          CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
          STOP
       ENDIF
       IF (MYPE .EQ. 0) WRITE(6,75) GERMAX
- 75   FORMAT(/'Max. absolute error is',E10.2)
+ 75   FORMAT(/'Max. absolute error is', E10.2/)
 C     
 C     Print final statistics.
       IF (MYPE .EQ. 0) THEN
@@ -165,56 +174,58 @@ C     Print final statistics.
          NPS = IOPT(LNPS)
          NNI = IOPT(LNNI)
          NLI = IOPT(LNLI)
-         AVDIM = REAL(NLI)/REAL(NNI)
+         AVDIM = DBLE(NLI) / DBLE(NNI)
          NCFN = IOPT(LNCF)
          NCFL = IOPT(LNCFL)
          NETF = IOPT(LNETF)
-         WRITE (6,80) NST,NFE,NPSET,NPE,NPS,NNI,NLI,AVDIM,NCFN,NCFL,NETF
- 80      FORMAT(/'Final statistics..'/
-     &        ' number of steps        =',I5,4X,
-     &        ' number of f evals.     =',I5/
-     &        ' number of prec. setups =',I5/
-     &        ' number of prec. evals. =',I5,4X,
-     &        ' number of prec. solves =',I5/
-     &        ' number of nonl. iters. =',I5,4X,
-     &        ' number of lin. iters.  =',I5/
-     &        ' average Krylov subspace dimension (NLI/NNI)  =',F8.4/
-     &        ' number of conv. failures..  nonlinear =',I3,
-     &        ' linear =',I3/
-     &        ' number of error test failures =',I3)
-         CALL FCVBBDOPT (LENRPW, LENIPW, NGE)
-         WRITE (6,82) LENRPW, LENIPW, NGE
- 82      FORMAT('In CVBBDPRE:'/'real/integer local work space =',2I5/
-     &        ' number of g evals.     =',I5)
+         WRITE(6,80) NST, NFE, NPSET, NPE, NPS, NNI, NLI, AVDIM, NCFN,
+     &               NCFL, NETF
+ 80      FORMAT(/'Final statistics:'//
+     &          ' number of steps        = ', I5, 4X,
+     &          ' number of f evals.     = ', I5/
+     &          ' number of prec. setups = ', I5/
+     &          ' number of prec. evals. = ', I5, 4X,
+     &          ' number of prec. solves = ', I5/
+     &          ' number of nonl. iters. = ', I5, 4X,
+     &          ' number of lin. iters.  = ', I5/
+     &          ' average Krylov subspace dimension (NLI/NNI) = ', F8.4/
+     &          ' number of conv. failures.. nonlinear = ', I3,
+     &          '  linear = ', I3/
+     &          ' number of error test failures = ', I3/)
+         CALL FCVBBDOPT(LENRPW, LENIPW, NGE)
+         WRITE(6,82) LENRPW, LENIPW, NGE
+ 82      FORMAT('In CVBBDPRE:'//
+     &          ' real/integer local work space = ', 2I5/
+     &          ' number of g evals. = ', I5)
       ENDIF
 C     
 C     If IPRE = 1, re-initialize T, Y, and the solver, and loop for case IPRE = 2.
 C     Otherwise jump to final block.
       IF (IPRE .EQ. 2) GO TO 99
-      
+C
       T = 0.0D0
-      DO I = 1,NLOCAL
+      DO I = 1, NLOCAL
          Y(I) = 1.0D0
       ENDDO         
-      
-      CALL FCVREINIT (T, Y, IATOL, RTOL, ATOL,INOPT, IOPT, ROPT, IER)
+C
+      CALL FCVREINIT(T, Y, IATOL, RTOL, ATOL,INOPT, IOPT, ROPT, IER)
       IF (IER .NE. 0) THEN
          WRITE(6,91) IER
- 91      FORMAT(///' SUNDIALS_ERROR: FCVREINIT returned IER = ',I5)
+ 91      FORMAT(///' SUNDIALS_ERROR: FCVREINIT returned IER = ', I5)
          CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
          STOP
       ENDIF
-      
+C
       IPRE = 2
-      
-      CALL FCVBBDREINIT (NLOCAL, MUDQ, MLDQ, 0D0, IER) 
+C
+      CALL FCVBBDREINIT(NLOCAL, MUDQ, MLDQ, 0.0D0, IER) 
       IF (IER .NE. 0) THEN
          WRITE(6,92) IER
- 92      FORMAT(///' SUNDIALS_ERROR: FCVBBDREINIT returned IER = ',I5)
+ 92      FORMAT(///' SUNDIALS_ERROR: FCVBBDREINIT returned IER = ', I5)
          CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
          STOP
       ENDIF
-
+C
       CALL FCVSPGMRREINIT(IPRE, IGS, 0.0D0, IER)
       IF (IER .NE. 0) THEN
          WRITE(6,93) IER
@@ -222,9 +233,9 @@ C     Otherwise jump to final block.
          CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
          STOP
       ENDIF
-
-      IF (MYPE .EQ. 0) WRITE (6,95)
- 95   FORMAT(/60('-')//'Preconditioning on right'/)
+C
+      IF (MYPE .EQ. 0) WRITE(6,95)
+ 95   FORMAT(//60('-')///'Preconditioning on right'/)
       GO TO 40
 C     
 C     Free the memory and finalize MPI.
@@ -235,23 +246,32 @@ C     Free the memory and finalize MPI.
 C     
       STOP
       END
-      
+C
       SUBROUTINE FCVFUN(T, Y, YDOT)
 C     Routine for right-hand side function f
-      IMPLICIT DOUBLE PRECISION (A-H, O-Z)
+C
+      IMPLICIT NONE
+C
+      INTEGER MYPE
+      INTEGER*4 I, NLOCAL
+      DOUBLE PRECISION Y, YDOT, ALPHA, T
       DIMENSION Y(*), YDOT(*)
-      COMMON /PCOM/ ALPHA, MYPE, NLOCAL
+C
+      COMMON /PCOM/ ALPHA, NLOCAL, MYPE
 C     
-      DO I = 1,NLOCAL
-         YDOT(I) = -ALPHA*(MYPE*NLOCAL + I)*Y(I)
+      DO I = 1, NLOCAL
+         YDOT(I) = -ALPHA * (MYPE * NLOCAL + I) * Y(I)
       ENDDO
 C     
       RETURN
       END
-      
+C
       SUBROUTINE FCVLOCFN(NLOC, T, YLOC, GLOC)
 C     Routine to define local approximate function g, here the same as f. 
-      IMPLICIT DOUBLE PRECISION (A-H, O-Z)
+      IMPLICIT NONE
+C
+      INTEGER*4 NLOC
+      DOUBLE PRECISION YLOC, GLOC, T
       DIMENSION YLOC(*), GLOC(*)
 C     
       CALL FCVFUN(T, YLOC, GLOC)
--- cvode/fcmix/examples_par/pvdiagnf.f
+++ cvode/fcmix/examples_par/pvdiagnf.f
@@ -1,47 +1,52 @@
 C     ----------------------------------------------------------------
-C     $Revision: 1.9 $
-C     $Date: 2004-10-12 21:37:39 $
+C     $Revision: 1.10 $
+C     $Date: 2004-10-15 20:20:17 $
 C     ----------------------------------------------------------------
 C     Diagonal ODE example. Nonstiff case: alpha = 10/NEQ.
 C     ----------------------------------------------------------------
 C
-      IMPLICIT DOUBLE PRECISION (A-H, O-Z)
-C
 C Include MPI-Fortran header file for MPI_COMM_WORLD, MPI types.
+C
       INCLUDE "mpif.h"
 C
+      INTEGER IER, MYPE, NPES, NOUT, LNST, LNFE, LNNI, LNCF, LNETF
+      INTEGER METH, ITMETH, IATOL, INOPT, ITASK, IOUT
+      INTEGER*4 NEQ, NLOCAL, I, NST, NFE, NNI, NCFN, NETF
       INTEGER*4 IOPT(40)
+      DOUBLE PRECISION Y, ROPT, ATOL, RTOL, DTOUT, T, ALPHA, TOUT
+      DOUBLE PRECISION ERMAX, ERRI, GERMAX
       DIMENSION Y(128), ROPT(40)
-      DATA ATOL/1.0E-10/, RTOL/1.0E-5/, DTOUT/0.1D0/, NOUT/10/
+C
+      DATA ATOL/1.0D-10/, RTOL/1.0D-5/, DTOUT/0.1D0/, NOUT/10/
       DATA LNST/4/, LNFE/5/, LNNI/7/, LNCF/8/, LNETF/9/
 C
-      COMMON /PCOM/ ALPHA, MYPE, NLOCAL
+      COMMON /PCOM/ ALPHA, NLOCAL, MYPE
 C
 C Get NPES and MYPE.  Requires initialization of MPI.
-      CALL MPI_INIT (IER)
+      CALL MPI_INIT(IER)
       IF (IER .NE. 0) THEN
         WRITE(6,5) IER
- 5      FORMAT(///' MPI_ERROR: MPI_INIT returned IER = ',I5)
+ 5      FORMAT(///' MPI_ERROR: MPI_INIT returned IER = ', I5)
         STOP
         ENDIF
-      CALL MPI_COMM_SIZE (MPI_COMM_WORLD, NPES, IER)
+      CALL MPI_COMM_SIZE(MPI_COMM_WORLD, NPES, IER)
       IF (IER .NE. 0) THEN
         WRITE(6,6) IER
- 6      FORMAT(///' MPI_ERROR: MPI_COMM_SIZE returned IER = ',I5)
+ 6      FORMAT(///' MPI_ERROR: MPI_COMM_SIZE returned IER = ', I5)
         CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
         STOP
         ENDIF
-      CALL MPI_COMM_RANK (MPI_COMM_WORLD, MYPE, IER)
+      CALL MPI_COMM_RANK(MPI_COMM_WORLD, MYPE, IER)
       IF (IER .NE. 0) THEN
         WRITE(6,7) IER
- 7      FORMAT(///' MPI_ERROR: MPI_COMM_RANK returned IER = ',I5)
+ 7      FORMAT(///' MPI_ERROR: MPI_COMM_RANK returned IER = ', I5)
         CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
         STOP
         ENDIF
 C
 C Set input arguments.
       NLOCAL = 2
-      NEQ = NPES*NLOCAL
+      NEQ = NPES * NLOCAL
       T = 0.0D0
       METH = 1
       ITMETH = 1
@@ -49,57 +54,58 @@ C Set input arguments.
       INOPT = 0
       ITASK = 1
 c Set parameter ALPHA
-      ALPHA  = 10.0D0/NEQ
+      ALPHA  = 10.0D0 / NEQ
 C
-      DO 10 I = 1,NLOCAL
+      DO 10 I = 1, NLOCAL
   10    Y(I) = 1.0D0
 C
       IF (MYPE .EQ. 0) THEN
         WRITE(6,11) NEQ, ALPHA
-  11    FORMAT('Diagonal test problem, size NEQ =',I3,
-     1         '  parameter alpha =',F8.3)
+  11    FORMAT('Diagonal test problem:'//' NEQ = ', I3, /
+     1         ' parameter alpha = ', F8.3)
         WRITE(6,12)
-  12    FORMAT('  ydot_i = -alpha*i * y_i (i = 1,...,NEQ)')
-        WRITE(6,13)RTOL,ATOL
-  13    FORMAT('RTOL, ATOL =',2E10.1)
+  12    FORMAT(' ydot_i = -alpha*i * y_i (i = 1,...,NEQ)')
+        WRITE(6,13) RTOL, ATOL
+  13    FORMAT(' RTOL, ATOL = ', 2E10.1)
         WRITE(6,14)
-  14    FORMAT('Method is ADAMS/FUNCTIONAL')
-        WRITE(6,15)NPES
-  15    FORMAT('Number of processors =',i3)
+  14    FORMAT(' Method is ADAMS/FUNCTIONAL')
+        WRITE(6,15) NPES
+  15    FORMAT(' Number of processors = ', I3//)
         ENDIF
 C
-      CALL FNVINITP (NLOCAL, NEQ, IER)
+      CALL FNVINITP(NLOCAL, NEQ, IER)
 C
       IF (IER .NE. 0) THEN
         WRITE(6,20) IER
-  20    FORMAT(///' SUNDIALS_ERROR: FNVINITP returned IER = ',I5)
+  20    FORMAT(///' SUNDIALS_ERROR: FNVINITP returned IER = ', I5)
         CALL MPI_FINALIZE(IER)
         STOP
         ENDIF
 C
-      CALL FCVMALLOC (T, Y, METH, ITMETH, IATOL, RTOL, ATOL,
-     1                INOPT, IOPT, ROPT, IER)
+      CALL FCVMALLOC(T, Y, METH, ITMETH, IATOL, RTOL, ATOL,
+     1               INOPT, IOPT, ROPT, IER)
 C
       IF (IER .NE. 0) THEN
         WRITE(6,30) IER
-  30    FORMAT(///' SUNDIALS_ERROR: FCVMALLOC returned IER = ',I5)
+  30    FORMAT(///' SUNDIALS_ERROR: FCVMALLOC returned IER = ', I5)
         CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
         STOP
         ENDIF
 C
 C Loop through tout values, call solver, print output, test for failure.
       TOUT = DTOUT
-      DO 70 IOUT = 1,NOUT
+      DO 70 IOUT = 1, NOUT
 C
-        CALL FCVODE (TOUT, T, Y, ITASK, IER)
+        CALL FCVODE(TOUT, T, Y, ITASK, IER)
 C
-        IF (MYPE .EQ. 0) WRITE(6,40) T,IOPT(LNST),IOPT(LNFE)
-  40    FORMAT(/' t =',D10.2,5X,'no. steps =',I5,'   no. f-s =',I5)
+        IF (MYPE .EQ. 0) WRITE(6,40) T, IOPT(LNST), IOPT(LNFE)
+  40    FORMAT(' t = ', D10.2, 5X, 'no. steps = ', I5,
+     &         '   no. f-s = ', I5)
 C
         IF (IER .NE. 0) THEN
           WRITE(6,60) IER, IOPT(26)
-  60      FORMAT(///' SUNDIALS_ERROR: FCVODE returned IER = ',I5,/,
-     &              '                 Linear Solver returned IER = ',I5)
+  60      FORMAT(///' SUNDIALS_ERROR: FCVODE returned IER = ', I5, /,
+     &           '                 Linear Solver returned IER = ', I5)
           CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
           STOP
           ENDIF
@@ -109,21 +115,21 @@ C
 C
 C Get max. absolute error in the local vector.
       ERMAX = 0.0D0
-      DO 75 I = 1,NLOCAL
-        ERRI  = Y(I) - EXP(-ALPHA*(MYPE*NLOCAL + I)*T)
-        ERMAX = MAX(ERMAX,ABS(ERRI))
+      DO 75 I = 1, NLOCAL
+        ERRI  = Y(I) - DEXP(-ALPHA * (MYPE * NLOCAL + I) * T)
+        ERMAX = DMAX1(ERMAX, DABS(ERRI))
   75    CONTINUE
 C Get global max. error from MPI_REDUCE call.
-      CALL MPI_REDUCE (ERMAX, GERMAX, 1, MPI_DOUBLE_PRECISION, MPI_MAX,
-     1                 0, MPI_COMM_WORLD, IER)
+      CALL MPI_REDUCE(ERMAX, GERMAX, 1, MPI_DOUBLE_PRECISION, MPI_MAX,
+     1                0, MPI_COMM_WORLD, IER)
       IF (IER .NE. 0) THEN
         WRITE(6,80) IER
-  80    FORMAT(///' MPI_ERROR: MPI_REDUCE returned IER = ',I5)
+  80    FORMAT(///' MPI_ERROR: MPI_REDUCE returned IER = ', I5)
         CALL MPI_ABORT(MPI_COMM_WORLD, 1, IER)
         STOP
         ENDIF
       IF (MYPE .EQ. 0) WRITE(6,85) GERMAX
-  85  FORMAT(//'Max. absolute error is',E10.2)
+  85  FORMAT(/'Max. absolute error is ', E10.2/)
 C
 C Print final statistics.
       NST = IOPT(LNST)
@@ -131,12 +137,13 @@ C Print final statistics.
       NNI = IOPT(LNNI)
       NCFN = IOPT(LNCF)
       NETF = IOPT(LNETF)
-      IF (MYPE .EQ. 0) WRITE (6,90) NST,NFE,NNI,NCFN,NETF
-  90  FORMAT(//' final statistics..'/
-     2 ' number of steps        =',I5,5X,'number of f evals.     =',I5/
-     3 ' number of nonlinear iters. =',I5/
-     4 ' number of nonlinear conv. failures =',I3/
-     5 ' number of error test failures =',I3)
+      IF (MYPE .EQ. 0) WRITE (6,90) NST, NFE, NNI, NCFN, NETF
+  90  FORMAT(/'Final statistics:'//
+     &       ' number of steps = ', I5, 5X, /,
+     &       ' number of f evals. = ', I5/
+     &       ' number of nonlinear iters. = ', I5/
+     &       ' number of nonlinear conv. failures = ', I3/
+     &       ' number of error test failures = ', I3)
 C
 C Free the memory and finalize MPI.
       CALL FCVFREE
@@ -144,21 +151,26 @@ C Free the memory and finalize MPI.
       CALL MPI_FINALIZE(IER)
       IF (IER .NE. 0) THEN
         WRITE(6,95) IER
- 95     FORMAT(///' MPI_ERROR: MPI_FINALIZE returned IER = ',I5)
+ 95     FORMAT(///' MPI_ERROR: MPI_FINALIZE returned IER = ', I5)
         STOP
         ENDIF
 C
       STOP
       END
-
+C
       SUBROUTINE FCVFUN(T, Y, YDOT)
 C Routine for right-hand side function f
-      IMPLICIT DOUBLE PRECISION (A-H, O-Z)
-      DIMENSION Y(*), YDOT(*)
-      COMMON /PCOM/ ALPHA, MYPE, NLOCAL
+      IMPLICIT NONE
 C
-      DO 10 I = 1,NLOCAL
-  10    YDOT(I) = -ALPHA*(MYPE*NLOCAL + I)*Y(I)
+      INTEGER MYPE
+      INTEGER*4 NLOCAL, I
+      DOUBLE PRECISION Y, YDOT, ALPHA, T
+      DIMENSION Y(*), YDOT(*)
+C
+      COMMON /PCOM/ ALPHA, NLOCAL, MYPE
+C
+      DO 10 I = 1, NLOCAL
+  10    YDOT(I) = -ALPHA * (MYPE * NLOCAL + I) * Y(I)
 C
       RETURN
       END
