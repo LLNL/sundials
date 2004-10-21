42ddb4af86325fe065f084e89950059e558a642c
collier6 <collier6@llnl.gov>
2004-10-21 20:41:04 +0000

    Made the following changes:
    (1) updated to use config.h header file (automatically generated by configure script) to determine options given by user
    (2) added support for extended precision option (uses long double C-type)

--- shared/include/sundialstypes.h
+++ shared/include/sundialstypes.h
@@ -1,46 +1,55 @@
-/*******************************************************************
- *                                                                 *
- * File          : sundialstypes.h                                 *
- * Programmers   : Scott D. Cohen, Alan C. Hindmarsh, and          *
- *                 Radu Serban @ LLNL                              *
- * Version of    : 07 February 2004                                *
- *-----------------------------------------------------------------*
- * Copyright (c) 2002, The Regents of the University of California *
- * Produced at the Lawrence Livermore National Laboratory          *
- * All rights reserved                                             *
- * For details, see sundials/shared/LICENSE                        *
- *-----------------------------------------------------------------*
- * This header file exports two types: realtype and booleantype,   *
- *as well as the constants TRUE and FALSE.                         *
- *                                                                 *
- * Users should #include "sundialstypes.h" in any file that        *
- * shhould be easily modifiable to work with a different real      *
- * type and use the exported name realtype within such a file.     *
- *                                                                 *
- * The constant SUNDIALS_SINGLE_PRECISION indicates the underlying *
- * type for realtype. It is set at the configuration stage.        *
- *                                                                 *
- * The legal types for realtype are float and double.              *
- *                                                                 *
- * The macro RCONST gives a user a convenient way to define real   *
- * constants. To use the real constant 1.0, for example, the       *
- * user should write                                               *
- *                                                                 *
- * #define ONE RCONST(1.0)                                         *
- *                                                                 *
- * If realtype is double, then RCONST(1.0) expands to 1.0.         *
- * If realtype is float, then RCONST(1.0) expands to 1.0F.         *
- * There is never a need to explicitly cast 1.0 to (realtype).     *
- *                                                                 *
- *******************************************************************/
+/*
+ * -----------------------------------------------------------------
+ * $Revision: 1.8 $
+ * $Date: 2004-10-21 20:41:04 $
+ * ----------------------------------------------------------------- 
+ * Programmer(s): Scott Cohen, Alan Hindmarsh, Radu Serban, and
+ *                Aaron Collier @ LLNL
+ * -----------------------------------------------------------------
+ * Copyright (c) 2002, The Regents of the University of California.
+ * Produced at the Lawrence Livermore National Laboratory.
+ * All rights reserved.
+ * For details, see sundials/shared/LICENSE.
+ *------------------------------------------------------------------
+ * This header file exports two types: realtype and booleantype,
+ * as well as the constants TRUE and FALSE.
+ *
+ * Users should include the header file sundialstypes.h in every
+ * program file and use the exported name realtype instead of
+ * float, double or long double.
+ *
+ * The constants SUNDIALS_SINGLE_PRECISION, SUNDIALS_DOUBLE_PRECISION
+ * and SUNDIALS_LONG_DOUBLE_PRECISION indicate the underlying data
+ * type of realtype. It is set at the configuration stage.
+ *
+ * The legal types for realtype are float, double and long double.
+ *
+ * The macro RCONST gives the user a convenient way to define
+ * real-valued constants. To use the constant 1.0, for example,
+ * the user should write the following:
+ *
+ *   #define ONE RCONST(1.0)
+ *
+ * If realtype is defined as a double, then RCONST(1.0) expands
+ * to 1.0. If realtype is defined as a float, then RCONST(1.0)
+ * expands to 1.0F. If realtype is defined as a long double,
+ * then RCONST(1.0) expands to 1.0L. There is never a need to
+ * explicitly cast 1.0 to (realtype).
+ *------------------------------------------------------------------
+ */
 
-#ifdef __cplusplus     /* wrapper to enable C++ usage */
+#ifdef __cplusplus  /* wrapper to enable C++ usage */
 extern "C" {
 #endif
   
-#ifndef _sundialstypes_h
-#define _sundialstypes_h
-  
+#ifndef _SUNDIALSTYPES_H
+#define _SUNDIALSTYPES_H
+
+#ifndef _CONFIG_H
+#define _CONFIG_H
+#include <config.h>
+#endif
+
 #include <float.h>
   
 #if defined(SUNDIALS_SINGLE_PRECISION)
@@ -48,32 +57,43 @@ extern "C" {
 typedef float realtype;
 #define RCONST(x) x##F
 #define BIG_REAL FLT_MAX
+#define SMALL_REAL FLT_MIN
 #define UNIT_ROUNDOFF FLT_EPSILON
 
-#else
+#elif defined(SUNDIALS_DOUBLE_PRECISION)
 
 typedef double realtype;
 #define RCONST(x) x
 #define BIG_REAL DBL_MAX
+#define SMALL_REAL DBL_MIN
 #define UNIT_ROUNDOFF DBL_EPSILON
+
+#elif defined(SUNDIALS_EXTENDED_PRECISION)
+
+typedef long double realtype;
+#define RCONST(x) x##L
+#define BIG_REAL LDBL_MAX
+#define SMALL_REAL LDBL_MIN
+#define UNIT_ROUNDOFF LDBL_EPSILON
 
 #endif
 
-/******************************************************************
- *                                                                *
- * Type : booleantype                                             *
- * Constants : FALSE, TRUE                                        *
- *----------------------------------------------------------------*
- * ANSI C does not have a built-in boolean type. Below is the     *
- * definition for a new type booleantype. The advantage of using  *
- * the name booleantype (instead of int) is an increase in code   *
- * readability.                                                   *
- * It allows the programmer to make a distinction between int and *
- * boolean data. Variables of type booleantype are intended to    *
- * have only the two values FALSE and TRUE which are defined      *
- * below to be equal to 0 and 1, respectively.                    *
- *                                                                *
- ******************************************************************/
+/*
+ *------------------------------------------------------------------
+ * Type : booleantype
+ *------------------------------------------------------------------
+ * Constants : FALSE and TRUE
+ *------------------------------------------------------------------
+ * ANSI C does not have a built-in boolean data type. Below is the
+ * definition for a new type called booleantype. The advantage of
+ * using the name booleantype (instead of int) is an increase in
+ * code readability. It also allows the programmer to make a
+ * distinction between int and boolean data. Variables of type
+ * booleantype are intended to have only the two values FALSE and
+ * TRUE which are defined below to be equal to 0 and 1,
+ * respectively.
+ *------------------------------------------------------------------
+ */
 
 #ifndef booleantype
 #define booleantype int
