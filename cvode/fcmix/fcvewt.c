7af2ada3f83176f8d94d82a39065971a72093867
radu <serban1@llnl.gov>
2005-04-01 21:53:21 +0000

    Newfile with wrapper for user-provided routine to compute ewt.

---
+++ cvode/fcmix/fcvewt.c
@@ -0,0 +1,60 @@
+/*
+ * -----------------------------------------------------------------
+ * $Revision: 1.1.2.1 $
+ * $Date: 2005-04-01 21:53:21 $
+ * ----------------------------------------------------------------- 
+ * Programmer: Radu Serban @ LLNL
+ * -----------------------------------------------------------------
+ * Copyright (c) 2002, The Regents of the University of California.
+ * Produced at the Lawrence Livermore National Laboratory.
+ * All rights reserved.
+ * For details, see sundials/cvode/LICENSE.
+ * -----------------------------------------------------------------
+ * Fortran/C interface routines for CVODE, for the case of a 
+ * user-supplied error weight calculation routine.
+ * -----------------------------------------------------------------
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "fcvode.h"         /* actual fn. names, prototypes and global vars.  */
+#include "nvector.h"        /* definitions of type N_Vector and vector macros */
+#include "sundialstypes.h"  /* definition of type realtype                    */
+
+/***************************************************************************/
+
+/* Prototype of the Fortran routine */
+
+#ifdef __cplusplus  /* wrapper to enable C++ usage */
+extern "C" {
+#endif
+  extern void FCV_EWT(realtype*, realtype*);
+#ifdef __cplusplus
+}
+#endif
+
+/***************************************************************************/
+
+void FCV_EWTSET(int *flag, int *ier)
+{
+  if (*flag == 1)
+    *ier = CVodeSetTolerances(CV_cvodemem, CV_WF, 0.0, FCVEwtSet);
+}
+
+/***************************************************************************/
+
+/* 
+ * C function to interface between CVODE and a Fortran subroutine FCVEWT.
+ */
+
+int FCVEwtSet(N_Vector y, N_Vector ewt, void *e_data)
+{
+  
+  realtype *ydata, *ewtdata;
+
+  ydata  = N_VGetArrayPointer(y);
+  ewtdata = N_VGetArrayPointer(ewt);
+
+  FCV_EWT(ydata, ewtdata);
+}
