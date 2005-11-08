22a5284a2b2f1336f2718b706843d2c0d3c82fb4
alanh <alanh@llnl.gov>
2005-11-08 23:57:33 +0000

    Corrected names lenrwLS, leniwLS, nfeLS.

--- cvodes/examples_par/pvkx.c
+++ cvodes/examples_par/pvkx.c
@@ -1,7 +1,7 @@
 /*
  * -----------------------------------------------------------------
- * $Revision: 1.17 $
- * $Date: 2005-09-23 19:00:01 $
+ * $Revision: 1.18 $
+ * $Date: 2005-11-08 23:57:32 $
  * -----------------------------------------------------------------
  * Programmer(s): S. D. Cohen, A. C. Hindmarsh, M. R. Wittman, and
  *                Radu Serban  @ LLNL
@@ -465,9 +465,9 @@ static void PrintOutput(void *cvode_mem, int my_pe, MPI_Comm comm,
 static void PrintFinalStats(void *cvode_mem)
 {
   long int lenrw, leniw ;
-  long int lenrwSPGMR, leniwSPGMR;
+  long int lenrwLS, leniwLS;
   long int nst, nfe, nsetups, nni, ncfn, netf;
-  long int nli, npe, nps, ncfl, nfeSPGMR;
+  long int nli, npe, nps, ncfl, nfeLS;
   int flag;
 
   flag = CVodeGetWorkSpace(cvode_mem, &lenrw, &leniw);
@@ -485,7 +485,7 @@ static void PrintFinalStats(void *cvode_mem)
   flag = CVodeGetNumNonlinSolvConvFails(cvode_mem, &ncfn);
   check_flag(&flag, "CVodeGetNumNonlinSolvConvFails", 1, 0);
 
-  flag = CVSpgmrGetWorkSpace(cvode_mem, &lenrwSPGMR, &leniwSPGMR);
+  flag = CVSpgmrGetWorkSpace(cvode_mem, &lenrwLS, &leniwLS);
   check_flag(&flag, "CVSpgmrGetWorkSpace", 1, 0);
   flag = CVSpgmrGetNumLinIters(cvode_mem, &nli);
   check_flag(&flag, "CVSpgmrGetNumLinIters", 1, 0);
@@ -495,18 +495,18 @@ static void PrintFinalStats(void *cvode_mem)
   check_flag(&flag, "CVSpgmrGetNumPrecSolves", 1, 0);
   flag = CVSpgmrGetNumConvFails(cvode_mem, &ncfl);
   check_flag(&flag, "CVSpgmrGetNumConvFails", 1, 0);
-  flag = CVSpgmrGetNumRhsEvals(cvode_mem, &nfeSPGMR);
+  flag = CVSpgmrGetNumRhsEvals(cvode_mem, &nfeLS);
   check_flag(&flag, "CVSpgmrGetNumRhsEvals", 1, 0);
 
   printf("\nFinal Statistics: \n\n");
-  printf("lenrw   = %5ld     leniw = %5ld\n", lenrw, leniw);
-  printf("llrw    = %5ld     lliw  = %5ld\n", lenrwSPGMR, leniwSPGMR);
+  printf("lenrw   = %5ld     leniw   = %5ld\n", lenrw, leniw);
+  printf("lenrwLS = %5ld     leniwLS = %5ld\n", lenrwLS, leniwLS);
   printf("nst     = %5ld\n"                  , nst);
-  printf("nfe     = %5ld     nfel  = %5ld\n"  , nfe, nfeSPGMR);
-  printf("nni     = %5ld     nli   = %5ld\n"  , nni, nli);
-  printf("nsetups = %5ld     netf  = %5ld\n"  , nsetups, netf);
-  printf("npe     = %5ld     nps   = %5ld\n"  , npe, nps);
-  printf("ncfn    = %5ld     ncfl  = %5ld\n\n", ncfn, ncfl); 
+  printf("nfe     = %5ld     nfeLS   = %5ld\n"  , nfe, nfeLS);
+  printf("nni     = %5ld     nli     = %5ld\n"  , nni, nli);
+  printf("nsetups = %5ld     netf    = %5ld\n"  , nsetups, netf);
+  printf("npe     = %5ld     nps     = %5ld\n"  , npe, nps);
+  printf("ncfn    = %5ld     ncfl    = %5ld\n\n", ncfn, ncfl); 
 }
  
 /* Routine to send boundary data to neighboring PEs */
