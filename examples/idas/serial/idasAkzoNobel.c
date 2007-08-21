463af542d365ed8882d106a27e5c5ff41162c7bb
petra1 <petra1@llnl.gov>
2007-08-21 19:16:56 +0000

    - Added Akzo Nobel chemical kinetics simulation example (idasAkzoNobel) and updated build/instalation files.  - Added a missing call to IDAFree in idasAkzoNobelASA.c

--- examples/idas/serial/idasAkzoNobel.c
+++ examples/idas/serial/idasAkzoNobel.c
@@ -1,6 +1,6 @@
 /*
  * -----------------------------------------------------------------
- * $Revision: 1.2 $
+ * $Revision: 1.1 $
  * $Date: 2007-08-21 19:16:56 $
  * -----------------------------------------------------------------
  * Programmer(s): Radu Serban and Cosmin Petra @ LLNL
@@ -18,10 +18,6 @@
  * species are mixed, while carbon dioxide is continuously added.
  * See http://pitagora.dm.uniba.it/~testset/report/chemakzo.pdf  
  * 
- * IDAS also computes the sensitivities with respect to initial
- * conditions of the following quantity:
- *   G = int_t0^t1 y1 dt
- * The sensitivity of G is the solution of the adjoint system at t0. 
  * -----------------------------------------------------------------
  */
  
@@ -38,24 +34,25 @@
 #define Ith(v,i)    NV_Ith_S(v,i-1)       /* i-th vector component */
 
 /* Problem Constants */
-#define NEQ 6
-#define T0 RCONST(0.0)
-#define TF RCONST(180.0)
+#define NEQ  6
+
+#define T0      RCONST(0.0)
+
+#define T1      RCONST(1e-8)  /* first time for output */
+
+#define TF      RCONST(180.0) /* Final time. */
+#define NF      25            /* Total number of outputs. */ 
 
 #define RTOL  RCONST(1.0e-08)
 #define ATOL  RCONST(1.0e-10)
-#define RTOLB RCONST(1.0e-06)
-#define ATOLB RCONST(1.0e-08)
 #define RTOLQ RCONST(1.0e-10)
 #define ATOLQ RCONST(1.0e-12)
-
 
 #define ZERO  RCONST(0.0)
 #define HALF  RCONST(0.5)
 #define ONE   RCONST(1.0)
 #define TWO   RCONST(2.0)
 
-#define STEPS 150
 
 typedef struct {
   realtype k1, k2, k3, k4;
@@ -64,17 +61,13 @@ typedef struct {
 
 static int res(realtype t, N_Vector yy, N_Vector yd, N_Vector res, void *userdata);
 
-static int resB(realtype tt, 
-                N_Vector yy, N_Vector yp,
-                N_Vector yyB, N_Vector ypB, N_Vector rrB,
-                void *user_dataB);
-
 static int rhsQ(realtype t, N_Vector yy, N_Vector yp, 
               N_Vector qdot, void *user_data);
 
-static void PrintOutput(realtype tfinal, N_Vector yB, N_Vector ypB);
+static void PrintHeader(realtype rtol, realtype avtol, N_Vector y);
+static void PrintOutput(void *mem, realtype t, N_Vector y);
+static void PrintFinalStats(void *mem);
 static int check_flag(void *flagvalue, char *funcname, int opt);
-
 
 /* Main program */
 int main()
@@ -82,20 +75,13 @@ int main()
   UserData data;
   void *mem;
   N_Vector yy, yp, rr, q;
-  N_Vector yB, ypB;
-  int ncheck, flag;
-  realtype time;
-  long int nst, nstB;
-  int indexB;
-    
+  int flag;
+  realtype time, tout, incr;
+  int nout;
 
   mem = NULL;
   yy = yp = NULL;
 
-  printf("\nAdjoint Sensitivity Example for Akzo-Nobel Chemical Kinetics\n");
-  printf("-------------------------------------------------------------\n");
-  printf("Sensitivity of G = int_t0^tf (y1) dt with respect to IC.\n");
-  printf("-------------------------------------------------------------\n\n");
   /* Allocate user data. */
   data = (UserData) malloc(sizeof(*data));
 
@@ -156,7 +142,7 @@ int main()
 
   /* Attach user data. */
   flag = IDASetUserData(mem, data);
-  if(check_flag(&flag, "IDASetUser", 1)) return(1);
+  if(check_flag(&flag, "IDASetUserData", 1)) return(1);
   
   /* Attach linear solver. */
   flag = IDADense(mem, NEQ);
@@ -171,76 +157,41 @@ int main()
 
   flag = IDASetQuadErrCon(mem, TRUE);
   if (check_flag(&flag, "IDASetQuadErrCon", 1)) return(1);
+
+  PrintHeader(RTOL, ATOL, yy);
+  /* Print initial states */
+  PrintOutput(mem,0.0,yy);
+
+  tout = T1; nout = 0;
+  incr = RPowerR(TF/T1,ONE/NF);
  
-  /* Prepare ADJOINT. */
-  flag = IDAAdjInit(mem, STEPS, IDA_HERMITE);
-  if (check_flag(&flag, "IDAAdjInit", 1)) return(1);
-
   /* FORWARD run. */
-  printf("Forward integration ... ");
-  flag = IDASolveF(mem, TF, &time, yy, yp, IDA_NORMAL, &ncheck);
-  if (check_flag(&flag, "IDASolveF", 1)) return(1);
+  while (1) {
 
-  flag = IDAGetNumSteps(mem, &nst);
-  if (check_flag(&flag, "IDAGetNumSteps", 1)) return(1);
+    flag = IDASolve(mem, tout, &time, yy, yp, IDA_NORMAL);
+    if (check_flag(&flag, "IDASolve", 1)) return(1);
 
-  printf("done ( nst = %ld )\n",nst);
+    PrintOutput(mem, time, yy);
+
+    nout++;
+    tout *= incr;
+
+    if (nout>NF) break;
+  }
 
   flag = IDAGetQuad(mem, &time, q);
   if (check_flag(&flag, "IDAGetQuad", 1)) return(1);
 
+  printf("\n--------------------------------------------------------\n");
   printf("G:          %24.16f \n",Ith(q,1));
   printf("--------------------------------------------------------\n\n");
 
-
-  /* BACKWARD run */
-
-  /* Initialize yB */
-  yB = N_VNew_Serial(NEQ);
-  if (check_flag((void *)yB, "N_VNew_Serial", 0)) return(1);
-  N_VConst(ZERO, yB);
-  
-  ypB = N_VNew_Serial(NEQ);
-  if (check_flag((void *)ypB, "N_VNew_Serial", 0)) return(1);
-  N_VConst(ZERO, ypB);
-  Ith(ypB,1) = - ONE;
-
-  flag = IDACreateB(mem, &indexB);
-  if (check_flag(&flag, "IDACreateB", 1)) return(1);
-
-  flag = IDAInitB(mem, indexB, resB, TF, yB, ypB);
-  if (check_flag(&flag, "IDAInitB", 1)) return(1);
-
-  flag = IDASStolerancesB(mem, indexB, RTOLB, ATOLB);
-  if (check_flag(&flag, "IDASStolerancesB", 1)) return(1);
-
-  flag = IDASetUserDataB(mem, indexB, data);
-  if (check_flag(&flag, "IDASetUserDataB", 1)) return(1);
-
-  flag = IDASetMaxNumStepsB(mem, indexB, 1000);
-
-  flag = IDADenseB(mem, indexB, NEQ);
-  if (check_flag(&flag, "IDADenseB", 1)) return(1);
-
-  printf("Backward integration ... ");
-
-  flag = IDASolveB(mem, T0, IDA_NORMAL);
-  if (check_flag(&flag, "IDASolveB", 1)) return(1);
-
-  IDAGetNumSteps(IDAGetAdjIDABmem(mem, indexB), &nstB);
-  printf("done ( nst = %ld )\n", nstB);
-
-  flag = IDAGetB(mem, indexB, &time, yB, ypB);
-  if (check_flag(&flag, "IDAGetB", 1)) return(1);
-
-  PrintOutput(time, yB, ypB);
+  PrintFinalStats(mem);
 
   IDAFree(&mem);
 
   N_VDestroy_Serial(yy);
   N_VDestroy_Serial(yp);
-  N_VDestroy_Serial(yB);
-  N_VDestroy_Serial(ypB);
   N_VDestroy_Serial(q);
 
   return(0);
@@ -310,94 +261,77 @@ static int rhsQ(realtype t, N_Vector yy, N_Vector yp, N_Vector qdot, void *user_
   return(0);
 }
 
-#define QUARTER   RCONST(0.25)
-#define FOUR      RCONST(4.0)
-#define EIGHT     RCONST(8.0)
-
-/* 
- * resB routine. Residual for adjoint system. 
- */
-static int resB(realtype tt, 
-                N_Vector yy, N_Vector yp,
-                N_Vector yyB, N_Vector ypB, N_Vector rrB,
-                void *user_dataB)
+static void PrintHeader(realtype rtol, realtype avtol, N_Vector y)
 {
-  UserData data;
-
-  realtype y1, y2, y3, y4, y5, y6;
-
-  realtype yB1, yB2, yB3, yB4, yB5, yB6;
-  realtype ypB1, ypB2, ypB3, ypB4, ypB5;
-
-  realtype k1, k2, k3, k4;
-  realtype K, klA, Ks, pCO2, H;
-
-  realtype y2tohalf, y1to3, k2overK, tmp1, tmp2;
-
-  data = (UserData) user_dataB;
-  k1 = data->k1;
-  k2 = data->k2;
-  k3 = data->k3;
-  k4 = data->k4;
-  K = data->K;
-  klA = data->klA;
-  Ks = data->Ks;
-  pCO2 = data->pCO2;
-  H = data->H;
-
-  y1 = Ith(yy,1);
-  y2 = Ith(yy,2);
-  y3 = Ith(yy,3);
-  y4 = Ith(yy,4);
-  y5 = Ith(yy,5);
-  y6 = Ith(yy,6);
-
-  yB1 = Ith(yyB,1);
-  yB2 = Ith(yyB,2);
-  yB3 = Ith(yyB,3);
-  yB4 = Ith(yyB,4);
-  yB5 = Ith(yyB,5);
-  yB6 = Ith(yyB,6);
-  
-  ypB1 = Ith(ypB,1);
-  ypB2 = Ith(ypB,2);
-  ypB3 = Ith(ypB,3);
-  ypB4 = Ith(ypB,4);
-  ypB5 = Ith(ypB,5);
-
-  y2tohalf = sqrt(y2);
-  y1to3 = y1*y1*y1;
-  k2overK = k2/K;
-
-  tmp1 = k1* y1to3 * y2tohalf; tmp2 = k3*y4*y4;
-  Ith(rrB,1) = 1 +  ypB1 - (EIGHT*tmp1 + k2overK*y5 + tmp2)*yB1 - (TWO*tmp1+tmp2)*yB2 + (FOUR*tmp1+k2overK*y5)*yB3 + k2overK*y5*(yB4-yB5) - TWO*tmp2*yB4 + Ks*y4*yB6;
-
-  tmp1 = k1 * y1*y1to3 * (y2tohalf/y2); tmp2 = k4 * y6*y6 * (y2tohalf/y2);
-  Ith(rrB,2) =     ypB2 - tmp1*yB1 - (QUARTER*tmp1 + QUARTER*tmp2 + klA)*yB2 + HALF*tmp1*yB3 + HALF*tmp2*yB5;
-
-  Ith(rrB,3) =     ypB3 + k2*y4*(yB1-yB3-yB4+yB5);
-
-  tmp1 = k3*y1*y4; tmp2 = k2*y3;
-  Ith(rrB,4) =     ypB4 + (tmp2-TWO*tmp1)*yB1 - TWO*tmp1*yB2 - tmp2*yB3 - (tmp2+FOUR*tmp1)*yB4 + tmp2*yB5 + Ks*y1*yB6;
-
-  Ith(rrB,5) =     ypB5 - k2overK*y1*(yB1-yB3-yB4+yB5);
-
-  Ith(rrB,6) =           k4*y6*y2tohalf*(2*yB5-yB2) - yB6;
-
-
-  return 0;
+  printf("\nidasAkzoNobel: Akzo Nobel chemical kinetics DAE serial example problem for IDAS\n");
+  printf("Linear solver: IDADENSE, Jacobian is computed by IDAS.\n");
+#if defined(SUNDIALS_EXTENDED_PRECISION)
+  printf("Tolerance parameters:  rtol = %Lg   atol = %Lg\n",
+         rtol, avtol);
+#elif defined(SUNDIALS_DOUBLE_PRECISION)
+  printf("Tolerance parameters:  rtol = %lg   atol = %lg\n",
+         rtol, avtol);
+#else
+  printf("Tolerance parameters:  rtol = %g   atol = %g\n",
+         rtol, avtol);
+#endif
+  printf("---------------------------------------------------------------------------------\n");
+  printf("   t        y1        y2       y3       y4       y5");
+  printf("      y6    | nst  k      h\n");
+  printf("---------------------------------------------------------------------------------\n");
 }
 
 
-/*
- * Print results after backward integration
- */
-static void PrintOutput(realtype tfinal, N_Vector yB, N_Vector ypB)
+static void PrintOutput(void *mem, realtype t, N_Vector y)
 {
-  printf("dG/dy0: \t%12.4e\n\t\t%12.4e\n\t\t%12.4e\n\t\t%12.4e\n\t\t%12.4e\n\t\t%12.4e\n",
-         Ith(yB,1), Ith(yB,2), Ith(yB,3), Ith(yB,4), Ith(yB,5), Ith(yB,6));
-  printf("--------------------------------------------------------\n\n");
+  realtype *yval;
+  int retval, kused;
+  long int nst;
+  realtype hused;
+
+  yval  = NV_DATA_S(y);
+
+  retval = IDAGetLastOrder(mem, &kused);
+  check_flag(&retval, "IDAGetLastOrder", 1);
+  retval = IDAGetNumSteps(mem, &nst);
+  check_flag(&retval, "IDAGetNumSteps", 1);
+  retval = IDAGetLastStep(mem, &hused);
+  check_flag(&retval, "IDAGetLastStep", 1);
+#if defined(SUNDIALS_EXTENDED_PRECISION)
+  printf("%8.2Le %8.2Le %8.2Le %8.2Le %8.2Le %8.2Le %8.2Le | %3ld  %1d %8.2Le\n", 
+         t, yval[0], yval[1], yval[2], yval[3], yval[4], yval[5], nst, kused, hused);
+#elif defined(SUNDIALS_DOUBLE_PRECISION)
+  printf("%8.2le %8.2le %8.2le %8.2le %8.2le %8.2le %8.2le | %3ld  %1d %8.2le\n", 
+         t, yval[0], yval[1], yval[2], yval[3], yval[4], yval[5], nst, kused, hused);
+#else
+  printf("%8.2e %8.2e %8.2e %8.2e %8.2e %8.2e %8.2e | %3ld  %1d %8.2e\n", 
+         t, yval[0], yval[1], yval[2], yval[3], yval[4], yval[5], nst, kused, hused);
+#endif
 }
+
+
+static void PrintFinalStats(void *mem)
+{
+  int flag;
+  long int nst, nni, nje, nre, nreLS, netf, ncfn;
+
+  flag = IDAGetNumSteps(mem, &nst);
+  flag = IDAGetNumResEvals(mem, &nre);
+  flag = IDADlsGetNumJacEvals(mem, &nje);
+  flag = IDAGetNumNonlinSolvIters(mem, &nni);
+  flag = IDAGetNumErrTestFails(mem, &netf);
+  flag = IDAGetNumNonlinSolvConvFails(mem, &ncfn);
+  flag = IDADlsGetNumResEvals(mem, &nreLS);
+
+  printf("\nFinal Run Statistics: \n\n");
+  printf("Number of steps                    = %ld\n", nst);
+  printf("Number of residual evaluations     = %ld\n", nre+nreLS);
+  printf("Number of Jacobian evaluations     = %ld\n", nje);
+  printf("Number of nonlinear iterations     = %ld\n", nni);
+  printf("Number of error test failures      = %ld\n", netf);
+  printf("Number of nonlinear conv. failures = %ld\n", ncfn);
+}
+
 
 /* 
  * Check function return value.
