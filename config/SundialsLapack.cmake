143aa57f7e69989f35bc8995bb7802a1674119ea
radu <serban1@llnl.gov>
2009-02-17 02:58:48 +0000

    autotools-based build - removed sundials_lapack.c references from the Makefile.in in each   solver's src/ directory. - in every single Makefile.in template in the example directories, added   the variable BLAS_LAPACK_LIBS on the link line for the examples that   DO NOT use the Lapack module. This is required in order to resolve all   symbols when building shared libraries. This is not a requirement   when building static libraries, but it would be much too complicated to   check and act conditionally on the build type. Moreover, this does no harm   and these makefiles are never seen by the user.
    CMake-based build
    - top-level CMakeLists.txt file:
      - add compiler directive to inhibit warnings re. unsecure functions (Windows only)
      - if examples are enabled, replacement variables are set if examples will be
        installed (not only if generating makefiles)
    - removed references to sundials_lapack.c from the CMakeLists.txt files in each
      solver's src/ directory.
    - in every single CMakeLists.txt file in the example directories, added
      the LAPACK_LIBRARIES to all examples, whether they use Lapack or not.
      This is required in order to resolve all symbols when building shared libraries.
      This is not a requirement when building static libraries, but it would be too
      complicated to check and act conditionally on the build type. Moreover, this
      does no harm and these cmake files are never seen by the user.
    - general overhaul of the entire build system to address issues that were
      apparent only on Windows machines (e.g. gratiously dealing with the case
      in which Lapack is enabled but no working Fortran compiler exists, building
      and using shared libraries, etc.)
    - reorganization of all CMakeLists.txt configuration files to improve readibility
      and maintainability. Greatly reduced the number of variables moved around.
    
    Makefile and CMakeLists templates (for exported examples)
    - modified so that they always link the Blas/Lapack libraries.
      This is done to address the case in which shared libraries were built for the
      SUNDIALS solvers. Indeed, in that case, to prevent "undefined symbol" errors, we
      must make sure that all referenced symbols are resolved, whether the example
      uses the Lapack module or not. We note this in the comment section (these
      configuration files are exported and therefore exposed to the user).

---
+++ config/SundialsLapack.cmake
@@ -0,0 +1,85 @@
+# ---------------------------------------------------------------
+# $Revision: 1.1 $
+# $Date: 2009-02-17 02:58:46 $
+# ---------------------------------------------------------------
+# Programmer:  Radu Serban @ LLNL
+# ---------------------------------------------------------------
+# Copyright (c) 2008, The Regents of the University of California.
+# Produced at the Lawrence Livermore National Laboratory.
+# All rights reserved.
+# For details, see the LICENSE file.
+# ---------------------------------------------------------------
+# BLAS/LAPACK tests for SUNDIALS CMake-based configuration.
+#
+# 
+
+SET(LAPACK_FOUND FALSE)
+
+# If LAPACK libraries are undefined, try to find them (if we have
+# a working Fortran compiler) or look for them in the most
+# obvious place...
+if(NOT LAPACK_LIBRARIES)
+  if(F77_FOUND)
+    include(FindLAPACK)
+  else(F77_FOUND)
+    find_library(LAPACK_LIBRARIES
+      NAMES lapack
+      PATHS /usr/lib /usr/local/lib
+      "$ENV{ProgramFiles}/LAPACK/Lib"
+      )
+  endif(F77_FOUND)
+endif(NOT LAPACK_LIBRARIES)
+# If using a GNU C compiler, it is quite likely we'll want LAPACK_LINKER_FLAGS
+# to include -lg2c (if not already present)
+if(CMAKE_COMPILER_IS_GNUCC AND NOT LAPACK_LINKER_FLAGS MATCHES "g2c")
+  set(LAPACK_LINKER_FLAGS "${LAPACK_LINKER_FLAGS} -lg2c")
+endif(CMAKE_COMPILER_IS_GNUCC AND NOT LAPACK_LINKER_FLAGS MATCHES "g2c")
+# If we have the LAPACK libraries, test them
+if(LAPACK_LIBRARIES)
+  message(STATUS "Looking for LAPACK libraries... OK")
+  # Create the LapackTest directory
+  set(LapackTest_DIR ${PROJECT_BINARY_DIR}/LapackTest)
+  file(MAKE_DIRECTORY ${LapackTest_DIR})
+  # Create a CMakeLists.txt file 
+  file(WRITE ${LapackTest_DIR}/CMakeLists.txt
+    "PROJECT(ltest C)\n"
+    "SET(CMAKE_VERBOSE_MAKEFILE ON)\n"
+    "SET(CMAKE_BUILD_TYPE \"${CMAKE_BUILD_TYPE}\")\n"
+    "SET(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS}\")\n"
+    "SET(CMAKE_C_FLAGS_RELEASE \"${CMAKE_C_FLAGS_RELEASE}\")\n"
+    "SET(CMAKE_C_FLAGS_DEBUG \"${CMAKE_C_FLAGS_DEBUG}\")\n"
+    "SET(CMAKE_C_FLAGS_RELWITHDEBUGINFO \"${CMAKE_C_FLAGS_RELWITHDEBUGINFO}\")\n"
+    "SET(CMAKE_C_FLAGS_MINSIZE \"${CMAKE_C_FLAGS_MINSIZE}\")\n"
+    "SET(CMAKE_EXE_LINKER_FLAGS \"\${CMAKE_EXE_LINKER_FLAGS} ${LAPACK_LINKER_FLAGS}\")\n"
+    "ADD_EXECUTABLE(ltest ltest.c)\n"
+    "TARGET_LINK_LIBRARIES(ltest ${LAPACK_LIBRARIES})\n")    
+  # Create a C source file which calls a Blas function (dcopy) and an Lapack function (dgetrf)
+  file(WRITE ${LapackTest_DIR}/ltest.c
+    "${F77_MANGLE_MACRO1}\n"
+    "#define dcopy_f77 SUNDIALS_F77_FUNC(dcopy, DCOPY)\n"
+    "#define dgetrf_f77 SUNDIALS_F77_FUNC(dgetrf, DGETRF)\n"
+    "extern void dcopy_f77(int *n, const double *x, const int *inc_x, double *y, const int *inc_y);\n"
+    "extern void dgetrf_f77(const int *m, const int *n, double *a, int *lda, int *ipiv, int *info);\n"
+    "int main(){\n"
+    "int n=1;\n"
+    "double x, y;\n"
+    "dcopy_f77(&n, &x, &n, &y, &n);\n"
+    "dgetrf_f77(&n, &n, &x, &n, &n, &n);\n"
+    "return(0);\n"
+    "}\n")
+  # Attempt to link the "ltest" executable
+  try_compile(LTEST_OK ${LapackTest_DIR} ${LapackTest_DIR}
+    ltest OUTPUT_VARIABLE MY_OUTPUT)    
+  # To ensure we do not use stuff from the previous attempts, 
+  # we must remove the CMakeFiles directory.
+  file(REMOVE_RECURSE ${LapackTest_DIR}/CMakeFiles)
+  # Process test result
+  if(LTEST_OK)
+    message(STATUS "Checking if Lapack works... OK")
+    set(LAPACK_FOUND TRUE)
+  else(LTEST_OK)
+    message(STATUS "Checking if Lapack works... FAILED")
+  endif(LTEST_OK)
+else(LAPACK_LIBRARIES)
+  message(STATUS "Looking for LAPACK libraries... FAILED")
+endif(LAPACK_LIBRARIES)
