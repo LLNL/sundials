a64a442cd0ff393cab694537de8b10cb99c0ae1c
radu <serban1@llnl.gov>
2009-03-29 19:56:49 +0000

    Fixed sundialsTB version number

--- sundialsTB/Contents.m
+++ sundialsTB/Contents.m
@@ -1,4 +1,4 @@
-% sundialsTB v.2.5.0 - Matlab interfaces to SUNDIALS solvers
+% sundialsTB v.2.4.0 - Matlab interfaces to SUNDIALS solvers
 %
 % sundialsTB provides interfaces to the CVODES, IDAS, and KINSOL
 %   solvers in SUNDIALS.
