ec26870822c8e528a90a03991a70875e569fb657
alanh <alanh@llnl.gov>
2011-05-18 23:55:40 +0000

    Removed JOK/JCUR paragraph; corrected later references to IDAMalloc and JCUR.

--- sundialsTB/idas/function_types/IDAPrecSetupFn.m
+++ sundialsTB/idas/function_types/IDAPrecSetupFn.m
@@ -20,12 +20,6 @@
 %   Newton iteration.  If the PSOLFUN function needs no
 %   preparation, the PSETFUN function need not be provided.
 %
-%   For greater efficiency, the PSETFUN function may save
-%   Jacobian-related data and reuse it, rather than generating it
-%   from scratch.  In this case, it should use the input flag JOK
-%   to decide whether to recompute the data, and set the output
-%   flag JCUR accordingly.
-%
 %   Each call to the PSETFUN function is preceded by a call to
 %   DAEFUN with the same (t,yy,yp) arguments.  Thus the PSETFUN
 %   function can use any auxiliary data that is computed and
@@ -39,12 +33,12 @@
 %   to a negative value, in which case the integration will be halted.
 %   The input argument RR contains the current value of f(t,yy,yp).
 %
-%   If a user data structure DATA was specified in IDAMalloc, then
+%   If a user data structure DATA was specified in IDASetUserData, then
 %   PSETFUN must be defined as
 %        FUNCTION [FLAG,NEW_DATA] = PSETFUN(T,YY,YP,RR,CJ,DATA)
 %   If the local modifications to the user data structure are needed in
-%   other user-provided functions then, besides setting the flags JCUR
-%   and FLAG, the PSETFUN function must also set NEW_DATA. Otherwise, it 
+%   other user-provided functions then, besides setting the flag
+%   FLAG, the PSETFUN function must also set NEW_DATA. Otherwise, it 
 %   should set NEW_DATA=[] (do not set NEW_DATA = DATA as it would lead
 %   to unnecessary copying).
 %
@@ -56,4 +50,4 @@
 
 % Radu Serban <radu@llnl.gov>
 % Copyright (c) 2007, The Regents of the University of California.
-% $Revision: 1.2 $Date: 2007/05/11 18:48:45 $
+% $Revision: 1.3 $Date: 2007/08/21 17:38:44 $
