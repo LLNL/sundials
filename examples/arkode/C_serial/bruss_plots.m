21e592d3c3dd6b3d9c87fc76d6cb5fd9845fce55
smithsg <smithsg@llnl.gov>
2013-11-07 00:57:28 +0000

    Revert Copyright statement to correct one

--- examples/arkode/C_serial/bruss_plots.m
+++ examples/arkode/C_serial/bruss_plots.m
@@ -1,15 +1,7 @@
 % ----------------------------------------------------------------
 % Programmer(s): Daniel R. Reynolds @ SMU
 % ----------------------------------------------------------------
-% LLNS Start Copyright
-% Copyright (c) 2013, Lawrence Livermore National Security
-% This work was performed under the auspices of the U.S. Department 
-% of Energy by Lawrence Livermore National Laboratory in part under 
-% Contract W-7405-Eng-48 and in part under Contract DE-AC52-07NA27344.
-% Produced at the Lawrence Livermore National Laboratory.
-% All rights reserved.
-% For details, see the LICENSE file.
-% LLNS End Copyright
+% Copyright (c) 2012, Southern Methodist University.
 % All rights reserved.
 % For details, see the LICENSE file.
 % ----------------------------------------------------------------
