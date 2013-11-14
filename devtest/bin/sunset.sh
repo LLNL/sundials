e1e299bd6a0c53984045afd0b2734c3354f13653
banks12 <banks12@llnl.gov>
2013-11-14 22:57:29 +0000

    Make sure to snag current python for testRunner

--- devtest/bin/sunset.sh
+++ devtest/bin/sunset.sh
@@ -3,6 +3,9 @@
 # set path for SUNDIALS build
 export PATH=/usr/casc/sundials/nightly/bin:$PATH
 
+# Python
+source /usr/casc/apps/python/2.7.3/setup.sh
+
 # cmake
 export CMAKE_HOME=/usr/casc/sundials/nightly/bin/cmake-2.8.10.2
 export PATH=$CMAKE_HOME/bin:$PATH
