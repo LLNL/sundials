dee8b20e8c72ab6c1e8c78e5839dd66c674fc194
banks12 <banks12@llnl.gov>
2013-12-04 08:17:20 +0000

    Improved Build: nightly checkout directory structure has revNNNN as parent and today's date as name of subdirectory for actualy build. Currently set to keep 5 revision directories, 5 daily build directories, and 10 daily build directories in current revision. Also: more meaningful email subject.

--- devtest/bin/sunbuild.py
+++ devtest/bin/sunbuild.py
@@ -9,151 +9,194 @@ from optparse import OptionParser
 
 
 class Logger(object):
-	def __init__(self, logFile):
-		self.terminal = sys.stdout
-		self.log = open(logFile, "w")
+    def __init__(self, logFile):
+        self.terminal = sys.stdout
+        self.log = open(logFile, "w")
 
-	def write(self, message):
-		self.terminal.write(message)
-		self.log.write(message)
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
         
-	def flush(self):
-		self.log.flush()
+    def flush(self):
+        self.log.flush()
 		
-	def close(self):
-		self.log.close()
-        
+    def close(self):
+        self.log.close()
+
 def main():
-	svnRepo = "file:///usr/casc/sundials/svnrepo/trunk"
-	sunBaseDir = "/usr/casc/sundials/devtest"
-	sunBinDir = os.path.join(sunBaseDir, "bin")
-	sunNightlyDir = os.path.join(sunBaseDir, "nightly")
-	logFileName = "build.log"
-	tmpLogFile = os.path.join(sunNightlyDir, logFileName)
-	sys.stdout = Logger(tmpLogFile)
+    maxRevDirs = 5                  # maximum revision directories to keep
+    maxBuildsPerPreviousRev = 3     # maximum number of builds to keep per previous revisions
+    maxBuildsPerCurrentRev = 10     # maximum number of builds to keep for current revision
+    
+    svnRepo = "file:///usr/casc/sundials/svnrepo/trunk"
+    sunBaseDir = "/usr/casc/sundials/devtest"
+    sunBinDir = os.path.join(sunBaseDir, "bin")
+    sunNightlyDir = os.path.join(sunBaseDir, "nightly")
+    todayDate = time.strftime("%Y-%m-%d")
+    logFileName = "build.log"
+    tmpLogFile = os.path.join(sunNightlyDir, logFileName)
+    sys.stdout = Logger(tmpLogFile)
 	
-	print "SUNDIALS Automated Build/Test"
-	startTime = datetime.datetime.now()
-	print startTime.ctime()
+    print "*** SUNDIALS Automated Build/Test ***"
+    startTime = datetime.datetime.now()
+    print startTime.ctime()
 
-	# parse command line options
-	parser = OptionParser()
-	parser.add_option("-r", "--rev", dest="rev", help="svn revision to checkout", metavar="number", default=0) # 0 -> latest
+    # parse command line options
+    parser = OptionParser()
+    parser.add_option("-r", "--rev", dest="rev", help="svn revision to checkout", metavar="number", default=0) # 0 -> latest
 
-	(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 	
+    # check for required options (none required at this time...could easily take specific revision# to build)
+    try:
+        try:	
+            # determine revision to checkout
+            svnrev = options.rev
+            if (svnrev == 0):
+                # get revision number from "Revision :  nnnn"
+                cmd = "svn info " + svnRepo + " | grep Revision"
+                svnrev = runCommandPopen(cmd).replace(' ','').split(':')[1].strip()
+                print svnrev
+            print "svn revision number: " + svnrev
+    		
+            # create directory for "today's" checkout
+            sunRevDir = os.path.join(sunNightlyDir, "rev"+svnrev)
+            sunCheckoutDir = os.path.join(sunRevDir, todayDate)
+            print "\n*** Creating checkout directory: " + sunCheckoutDir + " ..."
+            if not os.path.exists(sunCheckoutDir):
+                os.makedirs(sunCheckoutDir)
+    
+            # if directory is not empty - exit
+            if os.listdir(sunCheckoutDir) != []:
+                msg = "Revision 'rev"+svnrev+"/"+todayDate + "' already present."
+                print msg
+                raise Exception(msg)
+    
+            # checkout desired revision
+            sunSrcDir = os.path.join(sunCheckoutDir,"trunk")
+            cmd = "svn checkout -r " + svnrev + " " + svnRepo + " " + sunSrcDir
+            print "\n*** Checkout revision : " + cmd + " ..."
+            cmdout = runCommandPopen(cmd)
+            print cmdout
+    			
+            # create build directory
+            sunBuildDir = os.path.join(sunCheckoutDir, "build")
+            print "\n*** Creating build directory: " + sunBuildDir
+            if not os.path.exists(sunBuildDir):
+                os.makedirs(sunBuildDir)
+    
+            # if directory is not empty - exit
+            if os.listdir(sunBuildDir) != []:
+                msg = "Build directory for: 'rev" + svnrev + "/" + todayDate + "' is not empty."
+                print msg
+                raise Exception(msg)
+    				
+            # change working directory to build dir
+            os.chdir(sunBuildDir)
+    			
+            # run CMake to configure
+            cmd = "cmake -DMPI_ENABLE=ON " + sunSrcDir
+            print "\n*** Configuring with:  " + cmd + " ..."
+            cmdout = runCommandPopen(cmd)
+            print cmdout
+    			
+            # run make to build libs and executables
+            cmd = "make"
+            print "\n*** Building with:  " + cmd + " ..."
+            cmdout = runCommandPopen(cmd)
+            print cmdout
+    
+            # run 'make test' to test examples
+            cmd = "make test"
+            print "\n*** Testing with:  " + cmd + " ..."
+            (msg, cmdout) = runTestCommandPopen(cmd)
+            print cmdout
+    			
+            # purge nightly build directory (keep maxRevDirs number of revisions)
+            print "\n*** Purging nightly build directory..."
+            revDirList = purgeNightlyDir(sunNightlyDir, maxRevDirs)
+            
+            # now purge each revision directory (except most current)
+            for revDir in revDirList[:-1]:
+                purgeNightlyDir(revDir, maxBuildsPerPreviousRev)
+            
+            # now purge current revision directory
+            purgeNightlyDir(revDirList[-1], maxBuildsPerCurrentRev) # most current rev is last on list
+            
+        except Exception, e:
+            msg = "Failed: " + str(e)
+            cleanup(msg, startTime, sunCheckoutDir, tmpLogFile, logFileName)
+            return 2
+    
+        # build successful - cleanup
+        cleanup("Success: Rev: " + svnrev + " | " + msg, startTime, sunCheckoutDir, tmpLogFile, logFileName)
+    
+    # catch any other exception
+    except:
+        print "Build Aborted!"
+        # send log file in progress...
+        #print "Sending email from file: ", tmpLogFile
+        msg = "ABORT: Build failed for rev: " + str(svnrev)
+        cleanup(msg, startTime, sunCheckoutDir, tmpLogFile, logFileName)
+        raise Exception("ABORT")
 	
-	# check for required options (none required at this time)
 
-	try:	
-		try:
-			# determine revision to checkout
-			svnrev = options.rev
-			if (svnrev == 0):
-				# get revision number from "Revision :  nnnn"
-				cmd = "svn info " + svnRepo + " | grep Revision"
-				svnrev = runCommandPopen(cmd).replace(' ','').split(':')[1].strip()
-			#svnrev = 'rev' + svnrev
-		
-			print "svn revision number: " + svnrev
-		
-			# create directory for checkout
-			sunCheckoutDir = os.path.join(sunNightlyDir, "rev"+svnrev)
-			print "Creating checkout directory: " + sunCheckoutDir
-			if not os.path.exists(sunCheckoutDir):
-				os.makedirs(sunCheckoutDir)
-			
-			# if directory is not empty - exit
-			if os.listdir(sunCheckoutDir) != []:
-				msg = "Checkout revision: '" + sunCheckoutDir + "' already present."
-				print msg
-				raise msg
-				
-			# checkout desired revision
-			sunSrcDir = os.path.join(sunCheckoutDir,"trunk")
-			cmd = "svn checkout " + svnRepo + " " + sunSrcDir
-			print "running: " + cmd + "..."
-			cmdout = runCommandPopen(cmd)
-			print cmdout
-			
-			# create build directory
-			sunBuildDir = os.path.join(sunCheckoutDir, "build")
-			print "Creating build directory: " + sunBuildDir
-			if not os.path.exists(sunBuildDir):
-				os.makedirs(sunBuildDir)
-			
-			# if directory is not empty - exit
-			if os.listdir(sunBuildDir) != []:
-				msg = "Error: build directory: '" + sunBuildDir + "' is not empty."
-				print msg
-				raise msg
-				
-			# change working directory to build dir
-			os.chdir(sunBuildDir)
-			
-			# run CMake to configure
-			cmd = "cmake -DMPI_ENABLE=ON " + sunSrcDir
-			print "Configuring with:  " + cmd + " ..."
-			cmdout = runCommandPopen(cmd)
-			print cmdout
-			
-			# run make to build libs and executables
-			cmd = "make"
-			print "Building with:  " + cmd + " ..."
-			cmdout = runCommandPopen(cmd)
-			print cmdout
-			
-			# run 'make test' to test examples
-			cmd = "make test"
-			print "Testing with:  " + cmd + " ..."
-			cmdout = runTestCommandPopen(cmd)
-			print cmdout
-			
-			# cleanup nightly build directory - keep last 7
-			
-		
-		# catch any exception
-		except Exception, e:
-			print "Execption: ", e
-			# send log file in progress...
-			print "Build log file: ", tmpLogFile
-			sys.stdout.flush()
-			sys.stdout.close()
-			sendEmail(tmpLogFile)
-			return 2
-
-	# catch any exception
-	except:
-		print "Build Aborted!"
-		# send log file in progress...
-		#print "Sending email from file: ", tmpLogFile
-		sys.stdout.flush()
-		sys.stdout.close()
-		sendEmail(tmpLogFile, "SUNDIALS build aborted for rev: " + svnrev)
-		raise "ABORT"
-	
-	# Build Done - move and mail log file
-	endTime = datetime.datetime.now()
-	print "End time: ", endTime.ctime()
-	elapsedTime = endTime - startTime
-	print "Elapsed Time:", elapsedTime
-	
-	sys.stdout.flush()
-	sys.stdout.close()
-
-	# move the log file to the revision checkout directory
-	revLogFile = os.path.join(sunCheckoutDir, logFileName)
-	os.rename(tmpLogFile, revLogFile)
-	
-	# send email
-	sendEmail(revLogFile, "New SUNDIALS build for svn rev: " + svnrev)
-
-	# nothing else to do
-	return 0
+    # nothing else to do
+    return 0
 
 ############################
 # functions
 ############################
+#
+# purge the nightly directory of older builds
+#
+def purgeNightlyDir(rootDir, maxKeepDirs):
+    dirList = []
+    for tmpName in sorted(os.listdir(rootDir)):
+        tmpSpec = os.path.join(rootDir, tmpName)
+        if os.path.isdir(tmpSpec):
+            dirExtension = os.path.splitext(tmpSpec)[1]
+            # only purge directories without extensions (e.g. allows user to add '.save' to specific build) 
+            if dirExtension == '':
+                dirList.append(tmpSpec)
+                #print tmpSpec
+    
+    # if more than maxRevDirs - remove them
+    numDirs = len(dirList)
+    removeDirCount = numDirs - maxKeepDirs
+    for  i in xrange(removeDirCount):
+        shutil.rmtree(dirList[i])
+    
+    # return list of remaining directories
+    return dirList[removeDirCount:numDirs]
+
+#
+# cleanup
+#
+def cleanup(msg, startTime, sunCheckoutDir, tmpLogFile, logFileName):
+    # move and mail log file
+    endTime = datetime.datetime.now()
+    print "End time: ", endTime.ctime()
+    elapsedTime = endTime - startTime
+    print "Elapsed Time:", elapsedTime
+
+    sys.stdout.flush()
+    sys.stdout.close()
+
+    # move the log file to the revision checkout directory
+    revLogFile = os.path.join(sunCheckoutDir, logFileName)
+    
+    # don't overwrite a previous log file (since this is daily - this should never happen. But...)
+    if fileExists(revLogFile):
+        revLogFile = tmpLogFile
+    else:
+        os.rename(tmpLogFile, revLogFile)
+
+    # send email
+    sendEmail(revLogFile, msg)
+    return
+
+
 #
 # check if file exists
 #
@@ -166,34 +209,17 @@ def fileExists(fname):
 # run external command 
 #
 def runCommandPopen(cmd):
-	p = subprocess.Popen(cmd, shell=True, \
-	stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	(stdout, stderr) = p.communicate()
-	errcode = p.returncode
-	if p.returncode != 0:
-		print ("Error: Command did not complete successfully. (error code: %s)" % p.returncode)
-		print "Error/failure details:\n", stderr
-		raise Exception(p.returncode)
-	
-	# return last line of output
-	return ''.join(stdout.splitlines()[-1:])
-
+    cmdout = subprocess.check_output(cmd, shell=True)
+    # return last line of output
+    return ''.join(cmdout.splitlines()[-1:])
 #
 # run external command 
 #
 def runTestCommandPopen(cmd):
-	p = subprocess.Popen(cmd, shell=True, \
-	stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	(stdout, stderr) = p.communicate()
-	errcode = p.returncode
-	if p.returncode != 0:
-		print ("Error: Command did not complete successfully. (error code: %s)" % p.returncode)
-		#print "Error/failure details:\n", stderr
-		#raise Exception(p.returncode)
-	
-	# return last line of output  
-	# TODO: capture only 'meaningful' last lines (may need to add a 'Completion Line' in testRunner to look for)
-	return '\n'.join(stdout.splitlines()[-12:])
+    cmdout = subprocess.check_output(cmd, shell=True)
+    cmdoutList = cmdout.splitlines()
+    # return 3rd from last line (% passed line), and full output.
+    return (cmdoutList[-3], '\n'.join(cmdoutList))
 
 
 #
@@ -211,6 +237,7 @@ def sendEmail(logFile, subject):
 	me = "SUNDIALS.sunbuild@llnl.gov"
 	# you == the recipient's email address
 	you = "sundials-devs@llnl.gov"
+	#you = "banks12@llnl.gov" # Eddy
 	msg['Subject'] = subject
 	msg['From'] = me
 	msg['To'] = you
