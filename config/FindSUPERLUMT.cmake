08349a1f57a6c74f36518cbced6287222fc51e1f
banks12 <banks12@llnl.gov>
2014-02-05 19:16:31 +0000

    Renamed all occurrences of 'SuperLU' to 'SUPERLUMT' to specifically refer to the Multi-Threaded version of SuperLU.

---
+++ config/FindSUPERLUMT.cmake
@@ -0,0 +1,93 @@
+# ---------------------------------------------------------------
+# $Revision: 1.0 $
+# $Date: 2013-02-14 $
+# ---------------------------------------------------------------
+# Programmer:  Eddy Banks @ LLNL
+# ---------------------------------------------------------------
+# Copyright (c) 2013, The Regents of the University of California.
+# Produced at the Lawrence Livermore National Laboratory.
+# All rights reserved.
+# For details, see the LICENSE file.
+# ---------------------------------------------------------------
+# SUPERLUMT tests for SUNDIALS CMake-based configuration.
+# 
+
+# make sure valid thread type - if not, then warn and return
+STRING(TOUPPER "${SUPERLUMT_THREAD_TYPE}" SUPERLUMT_THREAD_TYPE_UPPER)
+If(SUPERLUMT_THREAD_TYPE AND NOT SUPERLUMT_THREAD_TYPE_UPPER STREQUAL "OPENMP" AND NOT SUPERLUMT_THREAD_TYPE_UPPER STREQUAL "PTHREAD")
+    PRINT_WARNING("Unknown thread type: ${SUPERLUMT_THREAD_TYPE}" "Please enter Pthread or OpenMP")
+ENDIF(SUPERLUMT_THREAD_TYPE AND NOT SUPERLUMT_THREAD_TYPE_UPPER STREQUAL "OPENMP" AND NOT SUPERLUMT_THREAD_TYPE_UPPER STREQUAL "PTHREAD")
+
+# find the SUPERLUMT include directory path
+IF(SUPERLUMT_THREAD_TYPE)
+    # if have user input for thread type - set postfix of library name
+    set(POST ${SUPERLUMT_THREAD_TYPE_UPPER})
+    set(temp_SUPERLUMT_INCLUDE_DIR ${SUPERLUMT_INCLUDE_DIR})
+    unset(SUPERLUMT_INCLUDE_DIR CACHE)  
+    find_path(SUPERLUMT_INCLUDE_DIR pdsp_defs.h ${temp_SUPERLUMT_INCLUDE_DIR})
+ELSE(SUPERLUMT_THREAD_TYPE)
+    FIND_PATH(SUPERLUMT_INCLUDE_DIR slu_defs.h ${temp_SUPERLUMT_INCLUDE_DIR})
+ENDIF(SUPERLUMT_THREAD_TYPE)
+
+IF(MSVC)
+  SET(PRE lib)
+ENDIF(MSVC)
+
+if (SUPERLUMT_LIBRARIES)
+    #print_warning("FindSUPERLUMT.cmake SUPERLUMT_LIBRARIES" "${SUPERLUMT_LIBRARIES}")
+    get_filename_component(SUPERLUMT_LIBRARY_DIR ${SUPERLUMT_LIBRARIES} PATH)
+    #print_warning("FindSUPERLUMT.cmake SUPERLUMT_LIBRARY_DIR" "${SUPERLUMT_LIBRARY_DIR}")
+    
+else (SUPERLUMT_LIBRARIES)
+
+    set(SUPERLUMT_LIBRARY_NAME superlu_mt_${POST})
+    
+    # find library path using potential names for static and/or shared libs
+    set(temp_SUPERLUMT_LIBRARY_DIR ${SUPERLUMT_LIBRARY_DIR})
+    unset(SUPERLUMT_LIBRARY_DIR CACHE)  
+    find_path(SUPERLUMT_LIBRARY_DIR
+        NAMES lib${SUPERLUMT_LIBRARY_NAME}.so lib${SUPERLUMT_LIBRARY_NAME}.a
+        PATHS ${temp_SUPERLUMT_LIBRARY_DIR}
+        )
+    
+    #unset(SUPERLUMT_LIBRARIES CACHE)
+    FIND_LIBRARY( SUPERLUMT_LIBRARIES ${PRE}superlu_mt_${POST} ${SUPERLUMT_LIBRARY_DIR} NO_DEFAULT_PATH)
+    mark_as_advanced(SUPERLUMT_LIBRARIES)
+    
+    #print_warning("FindSUPERLUMT.cmake SUPERLUMT_LIBRARIES" "${SUPERLUMT_LIBRARIES}")
+endif (SUPERLUMT_LIBRARIES)
+#print_warning("FindSUPERLUMT.cmake SUPERLUMT_LIBRARIES" "${SUPERLUMT_LIBRARIES}")
+
+# add threading library (pthread or gomp)
+unset(SUPERLUMT_THREAD_LIBRARY CACHE)
+If(SUPERLUMT_THREAD_TYPE_UPPER STREQUAL "PTHREAD")
+  # add pthread to libraries
+  find_library(SUPERLUMT_THREAD_LIBRARY
+      NAMES pthread
+      PATHS /usr/lib /usr/local/lib
+      "$ENV{ProgramFiles}/SUPERLUMT/Lib"
+      )
+  # add to SUPERLUMT_LIBRARIES
+ELSE(SUPERLUMT_THREAD_TYPE_UPPER STREQUAL "PTHREAD")
+  # add pthread to libraries
+  find_library(SUPERLUMT_THREAD_LIBRARY
+      NAMES gomp
+      PATHS /usr/lib /usr/local/lib
+      "$ENV{ProgramFiles}/SUPERLUMT/Lib"
+      )
+ENDIF(SUPERLUMT_THREAD_TYPE_UPPER STREQUAL "PTHREAD")
+mark_as_advanced(SUPERLUMT_THREAD_LIBRARY)
+
+# add to SUPERLUMT_LIBRARIES
+set(SUPERLUMT_LIBRARIES ${SUPERLUMT_LIBRARIES} ${SUPERLUMT_THREAD_LIBRARY})
+
+
+# If LAPACK/BLAS not enabled - find BLAS with SUPERLUMT
+if(NOT LAPACK_ENABLE)
+    set(SUPERLUMT_BLAS_LIBRARY_NAME blas_${POST})
+    
+    #unset(SUPERLUMT_BLAS_LIBRARIES CACHE)
+    FIND_LIBRARY( SUPERLUMT_BLAS_LIBRARIES ${PRE}blas_${POST} ${SUPERLUMT_LIBRARY_DIR} NO_DEFAULT_PATH)
+    set(SUPERLUMT_LIBRARIES ${SUPERLUMT_LIBRARIES} ${SUPERLUMT_BLAS_LIBRARIES})
+    mark_as_advanced(SUPERLUMT_BLAS_LIBRARIES)
+endif(NOT LAPACK_ENABLE)
---
+++ config/SundialsSuperLUMT.cmake
@@ -0,0 +1,72 @@
+# ---------------------------------------------------------------
+# $Revision: 1.1 $
+# $Date: 2013-02-14 $
+# ---------------------------------------------------------------
+# Programmer:  Eddy Banks @ LLNL
+# ---------------------------------------------------------------
+# Copyright (c) 2013, The Regents of the University of California.
+# Produced at the Lawrence Livermore National Laboratory.
+# All rights reserved.
+# For details, see the LICENSE file.
+# ---------------------------------------------------------------
+# SUPERLUMT tests for SUNDIALS CMake-based configuration.
+#    - loosely based on SundialsLapack.cmake
+# 
+
+#print_warning("SundialsSUPERLUMT.cmake 1 SUPERLUMT_FOUND" "${SUPERLUMT_FOUND}")
+SET(SUPERLUMT_FOUND FALSE)
+#print_warning("SundialsSUPERLUMT.cmake 2 SUPERLUMT_FOUND" "${SUPERLUMT_FOUND}")
+
+# set SUPERLUMT_LIBRARIES
+include(FindSUPERLUMT)
+# If we have the SUPERLUMT libraries, test them
+#print_warning("SundialsSUPERLUMT.cmake 3: SUPERLUMT_LIBRARIES" "${SUPERLUMT_LIBRARIES}")
+#print_warning("SundialsSUPERLUMT.cmake 4: SUPERLUMT_BLAS_LIBRARIES" "${SUPERLUMT_BLAS_LIBRARIES}")
+if(SUPERLUMT_LIBRARIES)
+  #print_warning("SundialsSUPERLUMT.cmake 5 SUPERLUMT_FOUND" "${SUPERLUMT_FOUND}")
+  #PRINT_WARNING("SundialsSUPERLUMT.cmake 6 SUPERLUMT_LIBRARIES" "${SUPERLUMT_LIBRARIES}")
+  message(STATUS "Looking for SUPERLUMT libraries... OK")
+  # Create the SUPERLUMT_TEST directory
+  set(SUPERLUMT_TEST_DIR ${PROJECT_BINARY_DIR}/SUPERLUMT_TEST)
+  file(MAKE_DIRECTORY ${SUPERLUMT_TEST_DIR})
+  # Create a CMakeLists.txt file 
+  file(WRITE ${SUPERLUMT_TEST_DIR}/CMakeLists.txt
+    "CMAKE_MINIMUM_REQUIRED(VERSION 2.2)\n"
+    "PROJECT(ltest C)\n"
+    "SET(CMAKE_VERBOSE_MAKEFILE ON)\n"
+    "SET(CMAKE_BUILD_TYPE \"${CMAKE_BUILD_TYPE}\")\n"
+    "SET(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS}\")\n"
+    "SET(CMAKE_C_FLAGS_RELEASE \"${CMAKE_C_FLAGS_RELEASE}\")\n"
+    "SET(CMAKE_C_FLAGS_DEBUG \"${CMAKE_C_FLAGS_DEBUG}\")\n"
+    "SET(CMAKE_C_FLAGS_RELWITHDEBUGINFO \"${CMAKE_C_FLAGS_RELWITHDEBUGINFO}\")\n"
+    "SET(CMAKE_C_FLAGS_MINSIZE \"${CMAKE_C_FLAGS_MINSIZE}\")\n"
+    "ADD_EXECUTABLE(ltest ltest.c)\n"
+    "TARGET_LINK_LIBRARIES(ltest ${SUPERLUMT_LIBRARIES})\n")    
+# TODO: Eddy - fix this test
+# Create a C source file which calls a SUPERLUMT function
+  file(WRITE ${SUPERLUMT_TEST_DIR}/ltest.c
+    "int main(){\n"
+    "int n=1;\n"
+    "double x, y;\n"
+    "return(0);\n"
+    "}\n")
+  # Attempt to link the "ltest" executable
+  try_compile(LTEST_OK ${SUPERLUMT_TEST_DIR} ${SUPERLUMT_TEST_DIR} ltest OUTPUT_VARIABLE MY_OUTPUT)
+      
+  # To ensure we do not use stuff from the previous attempts, 
+  # we must remove the CMakeFiles directory.
+  file(REMOVE_RECURSE ${SUPERLUMT_TEST_DIR}/CMakeFiles)
+  # Process test result
+#PRINT_WARNING("LTEST_OK" "${LTEST_OK}")
+  if(LTEST_OK)
+#PRINT_WARNING("x SundialsSUPERLUMT.cmake SUPERLUMT_LIBRARIES" "${SUPERLUMT_LIBRARIES}")
+    message(STATUS "Checking if SUPERLUMT works... OK")
+    set(SUPERLUMT_FOUND TRUE)
+    #print_warning("SUPERLUMT_FOUND" "${SUPERLUMT_FOUND}")
+  else(LTEST_OK)
+    message(STATUS "Checking if SUPERLUMT works... FAILED")
+  endif(LTEST_OK)
+else(SUPERLUMT_LIBRARIES)
+#PRINT_WARNING("y SundialsSUPERLUMT.cmake SUPERLUMT_LIBRARIES" "${SUPERLUMT_LIBRARIES}")
+  message(STATUS "Looking for SUPERLUMT libraries... FAILED")
+endif(SUPERLUMT_LIBRARIES)
