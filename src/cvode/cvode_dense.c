3248988b233f642577e63c3c71186a7e56147d9b
woodward6 <woodward6@llnl.gov>
2014-03-17 21:59:20 +0000

    Fixed typo.

--- src/cvode/cvode_dense.c
+++ src/cvode/cvode_dense.c
@@ -246,7 +246,7 @@ static int cvDenseSetup(CVodeMem cv_mem, int convfail, N_Vector ypred,
   /* Use nst, gamma/gammap, and convfail to set J eval. flag jok */
  
   dgamma = ABS((gamma/gammap) - ONE);
-  jbad = (nst == 0) || (nst > nstlj + CVS_MSBJ) ||
+  jbad = (nst == 0) || (nst > nstlj + CVD_MSBJ) ||
          ((convfail == CV_FAIL_BAD_J) && (dgamma < CVD_DGMAX)) ||
          (convfail == CV_FAIL_OTHER);
   jok = !jbad;
