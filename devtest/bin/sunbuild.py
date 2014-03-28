6e244a67ae1f890d3198e53c18bce629e1d12774
banks12 <banks12@llnl.gov>
2014-03-28 22:59:16 +0000

    Nightly build script now has full output from 'make' and corresponding message line in email. Also adding new answer file for cvRoberts_klu

--- devtest/bin/sunbuild.py
+++ devtest/bin/sunbuild.py
@@ -19,7 +19,7 @@ class Logger(object):
         
     def flush(self):
         self.log.flush()
-		
+        
     def close(self):
         self.log.close()
 
@@ -29,14 +29,17 @@ def main():
     maxBuildsPerCurrentRev = 10     # maximum number of builds to keep for current revision
     
     svnRepo = "file:///usr/casc/sundials/svnrepo/trunk"
+    #sunBaseDir = "/usr/casc/sundials/etest/devtest"
     sunBaseDir = "/usr/casc/sundials/devtest"
     sunBinDir = os.path.join(sunBaseDir, "bin")
     sunNightlyDir = os.path.join(sunBaseDir, "nightly")
     todayDate = time.strftime("%Y-%m-%d")
     logFileName = "build.log"
+    makeLogFileName = "make.log"
+    makeFileName = ""
     tmpLogFile = os.path.join(sunNightlyDir, logFileName)
     sys.stdout = Logger(tmpLogFile)
-	
+    
     print "*** SUNDIALS Automated Build/Test ***"
     startTime = datetime.datetime.now()
     print startTime.ctime()
@@ -46,10 +49,10 @@ def main():
     parser.add_option("-r", "--rev", dest="rev", help="svn revision to checkout", metavar="number", default=0) # 0 -> latest
 
     (options, args) = parser.parse_args()
-	
+    
     # check for required options (none required at this time...could easily take specific revision# to build)
     try:
-        try:	
+        try:    
             # determine revision to checkout
             svnrev = options.rev
             if (svnrev == 0):
@@ -58,7 +61,7 @@ def main():
                 svnrev = runCommandPopen(cmd).replace(' ','').split(':')[1].strip()
                 print svnrev
             print "svn revision number: " + svnrev
-    		
+            
             # create directory for "today's" checkout
             sunRevDir = os.path.join(sunNightlyDir, "rev"+svnrev)
             sunCheckoutDir = os.path.join(sunRevDir, todayDate)
@@ -78,7 +81,7 @@ def main():
             print "\n*** Checkout revision : " + cmd + " ..."
             cmdout = runCommandPopen(cmd)
             print cmdout
-    			
+                
             # create build directory
             sunBuildDir = os.path.join(sunCheckoutDir, "build")
             print "\n*** Creating build directory: " + sunBuildDir
@@ -90,10 +93,10 @@ def main():
                 msg = "Build directory for: 'rev" + svnrev + "/" + todayDate + "' is not empty."
                 print msg
                 raise Exception(msg)
-    				
+                    
             # change working directory to build dir
             os.chdir(sunBuildDir)
-    			
+                
             # run CMake to configure
             cmd = "cmake "
             # enable mpi
@@ -107,9 +110,10 @@ def main():
             print "\n*** Configuring with:  " + cmd + " ..."
             cmdout = runCommandPopen(cmd)
             print cmdout
-    			
+                
             # run make to build libs and executables
-            cmd = "make -j4"
+            makeLogFile = os.path.join(sunBuildDir, makeLogFileName)
+            cmd = "make -j4 &> " + makeLogFile
             print "\n*** Building with:  " + cmd + " ..."
             cmdout = runCommandPopen(cmd)
             print cmdout
@@ -119,7 +123,7 @@ def main():
             print "\n*** Testing with:  " + cmd + " ..."
             (msg, cmdout) = runTestCommandPopen(cmd)
             print cmdout
-    			
+                
             # purge nightly build directory (keep maxRevDirs number of revisions)
             print "\n*** Purging nightly build directory..."
             revDirList = purgeNightlyDir(sunNightlyDir, maxRevDirs)
@@ -133,21 +137,21 @@ def main():
             
         except Exception, e:
             msg = "FAILED: " + str(e)
-            cleanup(msg, startTime, sunCheckoutDir, tmpLogFile, logFileName)
+            cleanup(msg, startTime, sunCheckoutDir, tmpLogFile, logFileName, makeLogFile)
             return 2
     
         # build successful - cleanup
         msg = "Finished: Rev: " + svnrev + " | " + msg
-        cleanup(msg, startTime, sunCheckoutDir, tmpLogFile, logFileName)
+        cleanup(msg, startTime, sunCheckoutDir, tmpLogFile, logFileName, makeLogFile)
     
     # catch any other exception
     except:
         # send log file in progress...
         #print "Sending email from file: ", tmpLogFile
         msg = "ABORT: Build failed for rev: " + str(svnrev)
-        cleanup(msg, startTime, sunCheckoutDir, tmpLogFile, logFileName)
+        cleanup(msg, startTime, sunCheckoutDir, tmpLogFile, logFileName, makeLogFile)
         raise Exception("ABORT")
-	
+    
     # nothing else to do
     return 0
 
@@ -180,7 +184,7 @@ def purgeNightlyDir(rootDir, maxKeepDirs):
 #
 # cleanup
 #
-def cleanup(msg, startTime, sunCheckoutDir, tmpLogFile, logFileName):
+def cleanup(msg, startTime, sunCheckoutDir, tmpLogFile, logFileName, makeLogFile):
 
     # file spec of final log file in revision checkout directory
     revLogFile = os.path.join(sunCheckoutDir, logFileName)
@@ -188,6 +192,7 @@ def cleanup(msg, startTime, sunCheckoutDir, tmpLogFile, logFileName):
     # print closing info
     print "\n" + msg
     print "For build details see:  " + revLogFile
+    print "For make specific details see:  " + makeLogFile
     print "For test details see: " + sunCheckoutDir + "/build/Testing/Temporary/LastTest.log"
     
     endTime = datetime.datetime.now()
@@ -213,9 +218,9 @@ def cleanup(msg, startTime, sunCheckoutDir, tmpLogFile, logFileName):
 # check if file exists
 #
 def fileExists(fname):
-	if os.path.exists(fname):
-		return True
-	return False
+    if os.path.exists(fname):
+        return True
+    return False
 
 #
 # run external command 
@@ -255,31 +260,30 @@ def findPercentSuccessfulLine(cmdout):
 # send email to SUNDIALS dev list 
 #
 def sendEmail(logFile, subject):
-	# Open a plain text file for reading.  For this example, assume that
-	# the text file contains only ASCII characters.
-	fp = open(logFile, 'rb')
-	# Create a text/plain message
-	msg = MIMEText(fp.read())
-	fp.close()
+    # Open a plain text file for reading.  For this example, assume that
+    # the text file contains only ASCII characters.
+    fp = open(logFile, 'rb')
+    # Create a text/plain message
+    msg = MIMEText(fp.read())
+    fp.close()
 
-	# me == the sender's email address
-	me = "SUNDIALS.sunbuild@llnl.gov"
-	# you == the recipient's email address
-	you = "sundials-devs@llnl.gov"
-	#you = "banks12@llnl.gov" # Eddy
-	msg['Subject'] = subject
-	msg['From'] = me
-	msg['To'] = you
+    # me == the sender's email address
+    me = "SUNDIALS.sunbuild@llnl.gov"
+    # you == the recipient's email address
+    you = "sundials-devs@llnl.gov"
+    #you = "banks12@llnl.gov" # Eddy
+    msg['Subject'] = subject
+    msg['From'] = me
+    msg['To'] = you
 
-	# Send the message via our own SMTP server, but don't include the
-	# envelope header.
-	s = smtplib.SMTP('smtp.llnl.gov')
-	s.sendmail(me, [you], msg.as_string())
-	s.quit()
+    # Send the message via our own SMTP server, but don't include the
+    # envelope header.
+    s = smtplib.SMTP('smtp.llnl.gov')
+    s.sendmail(me, [you], msg.as_string())
+    s.quit()
 
 ############################
 
 if __name__ == "__main__":
-	sys.exit(main())
-
+    sys.exit(main())
 
