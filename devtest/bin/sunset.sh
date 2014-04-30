b1619c822d8db99b2cbe0769cadb58ab59bf087d
banks12 <banks12@llnl.gov>
2014-04-30 16:25:21 +0000

    Point to libraries for RedHat 6.x

--- devtest/bin/sunset.sh
+++ devtest/bin/sunset.sh
@@ -7,10 +7,10 @@ export PATH=/usr/casc/sundials/devtest/bin:$PATH
 source /usr/apps/python/2.7.3/setup.sh
 
 # cmake
-export CMAKE_HOME=/usr/casc/sundials/apps/rh5/cmake-2.8.10.2
+export CMAKE_HOME=/usr/casc/sundials/apps/rh6/cmake-2.8.10.2
 export PATH=$CMAKE_HOME/bin:$PATH
 
 # openmpi
-source /usr/casc/sundials/apps/rh5/openmpi/1.4.5/setup.sh
-#export PATH=/usr/casc/sundials/apps/rh5/openmpi/1.4.5/bin:$PATH
+source /usr/casc/sundials/apps/rh6/openmpi/1.4.5/setup.sh
+#export PATH=/usr/casc/sundials/apps/rh6/openmpi/1.4.5/bin:$PATH
 umask 002
