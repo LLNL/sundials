57da7ded3c78ccfdec316ec1fef4c3853e0d60fa
banks12 <banks12@llnl.gov>
2016-04-20 15:39:15 +0000

    Updated to newer libraries

--- devtest/bin/sunset.sh
+++ devtest/bin/sunset.sh
@@ -10,13 +10,22 @@ export PATH=/usr/apps/python/latest/bin:$PATH
 source /usr/casc/sundials/apps/rh6/cmake/cmake-2.8.10.2/setup.sh
 
 # lapack
-source /usr/casc/sundials/apps/rh6/lapack/3.5.0/setup.sh
+source /usr/casc/sundials/apps/rh6/lapack/3.6.0/setup.sh
 
-# openmpi
-source /usr/casc/sundials/apps/rh6/openmpi/1.4.5/setup.sh
+# suitesparse klu
+source /usr/casc/sundials/apps/rh6/suitesparse/4.2.1/setup.sh
 
 # SuperLU_MT
-source /usr/casc/sundials/apps/rh6/superlu_mt/SuperLU_MT_2.4/setup.sh
+source /usr/casc/sundials/apps/rh6/superlu_mt/SuperLU_MT_3.0/setup.sh
+
+# openmpi
+source /usr/casc/sundials/apps/rh6/openmpi/1.8.8/setup.sh
+
+# hypre
+source /usr/casc/sundials/apps/rh6/hypre/2.10.0b/setup.sh
+
+# petsc
+source /usr/casc/sundials/apps/rh6/petsc/3.6.3/setup.sh
 
 # variable used by Arkode OpenMP examples
 export OMP_NUM_THREADS=4
