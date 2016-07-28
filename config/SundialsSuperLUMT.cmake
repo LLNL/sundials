4fa497bc13afe2326d258712175c5de7beefd142
banks12 <banks12@llnl.gov>
2016-07-28 04:51:30 +0000

    Remove debug prints

--- config/SundialsSuperLUMT.cmake
+++ config/SundialsSuperLUMT.cmake
@@ -13,18 +13,12 @@
 #    - loosely based on SundialsLapack.cmake
 # 
 
-#print_warning("SundialsSUPERLUMT.cmake 1 SUPERLUMT_FOUND" "${SUPERLUMT_FOUND}")
 SET(SUPERLUMT_FOUND FALSE)
-#print_warning("SundialsSUPERLUMT.cmake 2 SUPERLUMT_FOUND" "${SUPERLUMT_FOUND}")
 
 # set SUPERLUMT_LIBRARIES
 include(FindSUPERLUMT)
 # If we have the SUPERLUMT libraries, test them
-#print_warning("SundialsSUPERLUMT.cmake 3: SUPERLUMT_LIBRARIES" "${SUPERLUMT_LIBRARIES}")
-#print_warning("SundialsSUPERLUMT.cmake 4: SUPERLUMT_BLAS_LIBRARIES" "${SUPERLUMT_BLAS_LIBRARIES}")
 if(SUPERLUMT_LIBRARIES)
-  #print_warning("SundialsSUPERLUMT.cmake 5 SUPERLUMT_FOUND" "${SUPERLUMT_FOUND}")
-  #print_WARNING("SundialsSUPERLUMT.cmake 6 SUPERLUMT_LIBRARIES" "${SUPERLUMT_LIBRARIES}")
   message(STATUS "Looking for SUPERLUMT libraries... OK")
   # Create the SUPERLUMT_TEST directory
   set(SUPERLUMT_TEST_DIR ${PROJECT_BINARY_DIR}/SUPERLUMT_TEST)
@@ -59,16 +53,12 @@ if(SUPERLUMT_LIBRARIES)
   # we must remove the CMakeFiles directory.
   file(REMOVE_RECURSE ${SUPERLUMT_TEST_DIR}/CMakeFiles)
   # Process test result
-#PRINT_WARNING("LTEST_OK" "${LTEST_OK}")
   if(LTEST_OK)
-#PRINT_WARNING("x SundialsSUPERLUMT.cmake SUPERLUMT_LIBRARIES" "${SUPERLUMT_LIBRARIES}")
     message(STATUS "Checking if SUPERLUMT works... OK")
     set(SUPERLUMT_FOUND TRUE)
-    #print_warning("SUPERLUMT_FOUND" "${SUPERLUMT_FOUND}")
   else(LTEST_OK)
     message(STATUS "Checking if SUPERLUMT works... FAILED")
   endif(LTEST_OK)
 else(SUPERLUMT_LIBRARIES)
-  PRINT_WARNING("Eddy -  SundialsSUPERLUMT.cmake SUPERLUMT_LIBRARIES" "${SUPERLUMT_LIBRARIES}")
   message(STATUS "Looking for SUPERLUMT libraries... FAILED")
 endif(SUPERLUMT_LIBRARIES)
