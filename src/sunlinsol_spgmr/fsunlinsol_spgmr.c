7cfef0ff824161402b82abe88b57ae2eb5d48492
Daniel R. Reynolds <reynolds@smu.edu>
2017-06-26 19:25:54 -0500

    Added stubs for generic SPGMR SUNLinearSolver object (to be filled in during code sprint).

---
+++ src/sunlinsol_spgmr/fsunlinsol_spgmr.c
@@ -0,0 +1,80 @@
+/*
+ * -----------------------------------------------------------------
+ * Programmer(s): Daniel Reynolds @ SMU
+ *                David Gardner, Carol Woodward, Slaven Peles @ LLNL
+ * -----------------------------------------------------------------
+ * LLNS/SMU Copyright Start
+ * Copyright (c) 2017, Southern Methodist University and 
+ * Lawrence Livermore National Security
+ *
+ * This work was performed under the auspices of the U.S. Department 
+ * of Energy by Southern Methodist University and Lawrence Livermore 
+ * National Laboratory under Contract DE-AC52-07NA27344.
+ * Produced at Southern Methodist University and the Lawrence 
+ * Livermore National Laboratory.
+ *
+ * All rights reserved.
+ * For details, see the LICENSE file.
+ * LLNS/SMU Copyright End
+ * -----------------------------------------------------------------
+ * This file (companion of fsunlinsol_spgmr.h) contains the
+ * implementation needed for the Fortran initialization of SPGMR
+ * linear solver operations.
+ * -----------------------------------------------------------------
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "fsunlinsol_spgmr.h"
+
+/* Define global linsol variables */
+
+SUNLinearSolver F2C_CVODE_linsol;
+SUNLinearSolver F2C_IDA_linsol;
+SUNLinearSolver F2C_KINSOL_linsol;
+SUNLinearSolver F2C_ARKODE_linsol;
+
+/* Declarations of external global variables */
+
+extern SUNMatrix F2C_CVODE_matrix;
+extern SUNMatrix F2C_IDA_matrix;
+extern SUNMatrix F2C_KINSOL_matrix;
+extern SUNMatrix F2C_ARKODE_matrix;
+
+extern N_Vector F2C_CVODE_vec;
+extern N_Vector F2C_IDA_vec;
+extern N_Vector F2C_KINSOL_vec;
+extern N_Vector F2C_ARKODE_vec;
+
+/* Fortran callable interfaces */
+
+void FSUNSPGMR_INIT(int *code, int *pretype, int *maxl, int *ier)
+{
+  *ier = 0;
+
+  switch(*code) {
+  case FCMIX_CVODE:
+    F2C_CVODE_linsol = NULL;
+    F2C_CVODE_linsol = SUNSPGMR(F2C_CVODE_vec, *pretype, *maxl);
+    if (F2C_CVODE_linsol == NULL) *ier = -1;
+    break;
+  case FCMIX_IDA:
+    F2C_IDA_linsol = NULL;
+    F2C_IDA_linsol = SUNSPGMR(F2C_IDA_vec, *pretype, *maxl);
+    if (F2C_IDA_linsol == NULL) *ier = -1;
+    break;
+  case FCMIX_KINSOL:
+    F2C_KINSOL_linsol = NULL;
+    F2C_KINSOL_linsol = SUNSPGMR(F2C_KINSOL_vec, *pretype, *maxl);
+    if (F2C_KINSOL_linsol == NULL) *ier = -1;
+    break;
+  case FCMIX_ARKODE:
+    F2C_ARKODE_linsol = NULL;
+    F2C_ARKODE_linsol = SUNSPGMR(F2C_ARKODE_vec, *pretype, *maxl);
+    if (F2C_ARKODE_linsol == NULL) *ier = -1;
+    break;
+  default:
+    *ier = -1;
+  }
+}
---
+++ src/sunlinsol_spgmr/sunlinsol_spgmr.c
@@ -0,0 +1,185 @@
+/*
+ * -----------------------------------------------------------------
+ * Programmer(s): Daniel Reynolds @ SMU
+ *                David Gardner, Carol Woodward, Slaven Peles @ LLNL
+ * -----------------------------------------------------------------
+ * LLNS/SMU Copyright Start
+ * Copyright (c) 2017, Southern Methodist University and 
+ * Lawrence Livermore National Security
+ *
+ * This work was performed under the auspices of the U.S. Department 
+ * of Energy by Southern Methodist University and Lawrence Livermore 
+ * National Laboratory under Contract DE-AC52-07NA27344.
+ * Produced at Southern Methodist University and the Lawrence 
+ * Livermore National Laboratory.
+ *
+ * All rights reserved.
+ * For details, see the LICENSE file.
+ * LLNS/SMU Copyright End
+ * -----------------------------------------------------------------
+ * This is the implementation file for the SPGMR implementation of 
+ * the SUNLINSOL package.
+ * -----------------------------------------------------------------
+ */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sunlinsol/sunlinsol_spgmr.h>
+#include <sundials/sundials_math.h>
+
+#define ONE  RCONST(1.0)
+
+/*
+ * -----------------------------------------------------------------
+ * exported functions
+ * -----------------------------------------------------------------
+ */
+
+/* ----------------------------------------------------------------------------
+ * Function to create a new SPGMR linear solver
+ */
+
+SUNLinearSolver SUNSPGMR(N_Vector y, int pretype, int maxl)
+{
+  SUNLinearSolver S;
+  SUNLinearSolver_Ops ops;
+  SUNLinearSolverContent_SPGMR content;
+  
+  /* Create linear solver */
+  S = NULL;
+  S = (SUNLinearSolver) malloc(sizeof *S);
+  if (S == NULL) return(NULL);
+  
+  /* Create linear solver operation structure */
+  ops = NULL;
+  ops = (SUNLinearSolver_Ops) malloc(sizeof(struct _generic_SUNLinearSolver_Ops));
+  if (ops == NULL) { free(S); return(NULL); }
+
+  /* Attach operations */
+  ops->gettype           = SUNLinSolGetType_SPGMR;
+  ops->setatimes         = SUNLinSolSetATimes_SPGMR;
+  ops->setpreconditioner = SUNLinSolSetPreconditioner_SPGMR;
+  ops->initialize        = SUNLinSolInitialize_SPGMR;
+  ops->setup             = SUNLinSolSetup_SPGMR;
+  ops->solve             = SUNLinSolSolve_SPGMR;
+  ops->performance       = SUNLinSolPerformance_SPGMR;
+  ops->free              = SUNLinSolFree_SPGMR;
+
+  /* Create content */
+  content = NULL;
+  content = (SUNLinearSolverContent_SPGMR) malloc(sizeof(struct _SUNLinearSolverContent_SPGMR));
+  if (content == NULL) { free(ops); free(S); return(NULL); }
+
+  /* Fill content */
+  content->last_flag = 0;
+  content->maxl = maxl;
+  content->pretype = pretype;
+  content->ATSetup = NULL;
+  content->ATimes = NULL;
+  content->ATData = NULL;
+  content->Psetup = NULL;
+  content->Psolve = NULL;
+  content->PData = NULL;
+  
+  /* Attach content and ops */
+  S->content = content;
+  S->ops     = ops;
+
+  return(S);
+}
+
+/*
+ * -----------------------------------------------------------------
+ * implementation of linear solver operations
+ * -----------------------------------------------------------------
+ */
+
+SUNLinearSolver_Type SUNLinSolGetType_SPGMR(SUNLinearSolver S)
+{
+  return SUNLINEARSOLVER_ITERATIVE;
+}
+
+int SUNLinSolInitialize_SPGMR(SUNLinearSolver S)
+{
+  /* allocate solver-specific memory here (Krylov subspace vectors) */
+
+
+  /* return with success */
+  SLS_CONTENT_SPGMR(S)->last_flag = 0;
+  return 0;
+}
+
+int SUNLinSolSetATimes_SPGMR(SUNLinearSolver S, void* ATData, 
+                            ATSetupFn ATSetup, ATimesFn ATimes)
+{
+  /* set function pointers to integrator-supplied ATSetup 
+     and ATimes routines and data, and return with success */
+  SLS_CONTENT_SPGMR(S)->ATSetup = ATSetup;
+  SLS_CONTENT_SPGMR(S)->ATimes = ATimes;
+  SLS_CONTENT_SPGMR(S)->ATData = ATData;
+  SLS_CONTENT_SPGMR(S)->last_flag = 0;
+  return 0;
+}
+
+int SUNLinSolSetPreconditioner_SPGMR(SUNLinearSolver S, void* PData,
+                                    PSetupFn PSetup, PSolveFn PSolve)
+{
+  /* set function pointers to integrator-supplied PSetup and PSolve
+     routines and data, and return with success */
+  SLS_CONTENT_SPGMR(S)->PSetup = PSetup;
+  SLS_CONTENT_SPGMR(S)->PSolve = PSolve;
+  SLS_CONTENT_SPGMR(S)->PData = PData;
+  SLS_CONTENT_SPGMR(S)->last_flag = 0;
+  return 0;
+}
+
+int SUNLinSolSetup_SPGMR(SUNLinearSolver S, SUNMatrix A, N_Vector tmp1, 
+                         N_Vector tmp2, N_Vector tmp3)
+{
+  int ierr;
+  ATSetupFn ATSetup = SLS_CONTENT_SPGMR(S)->ATSetup;
+  PSetupFn PSetup = SLS_CONTENT_SPGMR(S)->PSetup;
+  void* ATData = SLS_CONTENT_SPGMR(S)->ATData;
+  void* PData = SLS_CONTENT_SPGMR(S)->PData;;
+  
+  /* no solver-specific setup is required, but if user-supplied 
+     ATSetup or PSetup routines exist, call those here */
+  if (ATSetup != NULL) {
+    SLS_CONTENT_SPGMR(S)->last_flag = ATSetup(ATData);
+    if (SLS_CONTENT_SPGMR(S)->last_flag != 0)  return 1;
+  }
+  if (PSetup != NULL) {
+    SLS_CONTENT_SPGMR(S)->last_flag = PSetup(PData);
+    if (SLS_CONTENT_SPGMR(S)->last_flag != 0)  return 1;
+  }
+  
+  /* return with success */ 
+  return 0;
+}
+
+int SUNLinSolSolve_SPGMR(SUNLinearSolver S, SUNMatrix A, N_Vector x, 
+                         N_Vector b, N_Vector w, realtype tol)
+{
+
+}
+
+int SUNLinSolPerformance_SPGMR(SUNLinearSolver S, int perftask)
+{
+
+}
+
+long int SUNLinSolLastFlag_SPGMR(SUNLinearSolver S)
+{
+  /* return the stored 'last_flag' value */
+  return (SLS_CONTENT_SPGMR(S)->last_flag);
+}
+
+int SUNLinSolFree_SPGMR(SUNLinearSolver S)
+{
+  /* delete items from the contents structure, then delete generic structures */
+  free(S->content);  S->content = NULL;
+  free(S->ops);  S->ops = NULL;
+  free(S); S = NULL;
+  return 0;
+}
