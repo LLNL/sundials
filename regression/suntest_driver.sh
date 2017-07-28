651794f5dc082594d6ea0d3474e607f800f42a8e
David J. Gardner <gardner48@llnl.gov>
2017-07-28 16:00:00 -0700

    Update Testing Scripts
    Rename testing driver and remove minimal test from driver
    Update testing scripts to explicit set cmake options
    Clean up testing scripts

--- regression/suntest_driver.sh
+++ regression/suntest_driver.sh
@@ -24,63 +24,39 @@ indextype=('signed_64bit')
 # remove old test directories and logs
 \rm -rf suntest*/ *.log
 
-# output to stdout and log file
+# ------------------------------------------------------------------------------
+# Run regression tests
 echo "--------------------------------------------------" | tee -a suntest.log
 echo "SUNDIALS Regression Tests" | tee -a suntest.log
 date | tee -a suntest.log
 echo "--------------------------------------------------" | tee -a suntest.log
 
-# ------------------------------------------------------------------------------
-# Minimal setup: no external solvers enabled
+# loop over build options
 for ((i=0; i<${#realtype[@]}; i++)); do
     for ((j=0; j<${#indextype[@]}; j++)); do
 
         # run tests
-        ./suntest_minimal.sh ${realtype[i]} ${indextype[j]} $nbt
+        ./suntest.sh ${realtype[i]} ${indextype[j]} $nbt
 
-        # check number of errors
-        nerr=$?
-        if [ $nerr -ne 0 ]; then
-            let nfail+=nerr
-            echo "FAILED: $nerr failures, suntest_minimal.sh ${realtype[i]} ${indextype[j]}" \
-                | tee -a suntest.log
+        # check return flag
+        if [ $? -ne 0 ]; then
+            let nfail+=1
+            echo "FAILED: ${realtype[i]} ${indextype[j]}" | tee -a suntest.log
         else
-            echo "PASSED: $nerr failures, suntest_minimal.sh ${realtype[i]} ${indextype[j]}" \
-                | tee -a suntest.log
+            echo "PASSED: ${realtype[i]} ${indextype[j]}" | tee -a suntest.log
         fi
     done
 done
-echo "--------------------------------------------------" | tee -a suntest.log
 
 # ------------------------------------------------------------------------------
-# Maximal setup: all external solvers enabled
-for ((i=0; i<${#realtype[@]}; i++)); do
-    for ((j=0; j<${#indextype[@]}; j++)); do
-
-        # run tests
-        ./suntest_maximal.sh ${realtype[i]} ${indextype[j]} $nbt
-
-        # check number of errors
-        nerr=$?
-        if [ $nerr -ne 0 ]; then
-            let nfail+=nerr
-            echo "FAILED: $nerr failures, suntest_maximal.sh ${realtype[i]} ${indextype[j]}" \
-                | tee -a suntest.log
-        else
-            echo "PASSED: $nerr failures, suntest_maximal.sh ${realtype[i]} ${indextype[j]}" \
-                | tee -a suntest.log
-        fi
-    done
-done
+# Return overall pass/fail
 echo "--------------------------------------------------" | tee -a suntest.log
-
-# ------------------------------------------------------------------------------
-# Return pass/fail
-echo "--------------------------------------------------" | tee -a suntest.log
+echo "Regression Tests Completed" | tee -a suntest.log
+date | tee -a suntest.log
 if [ $nfail -ne 0 ]; then
     echo "FAILED: $nfail failures." | tee -a suntest.log
 else
-    echo "PASSED: $nfail failures." | tee -a suntest.log
+    echo "PASSED" | tee -a suntest.log
 fi
 echo "--------------------------------------------------" | tee -a suntest.log
 
--- regression/suntest_minimal.sh
+++ regression/suntest_minimal.sh
@@ -22,25 +22,25 @@ if [ "$#" -lt 2 ]; then
     echo "ERROR: Illegal number of parameters, real and index type required"
     exit 1
 fi
-realtype=$1
-indextype=$2
+realtype=$1   # required, precision for realtypes
+indextype=$2  # required, integer type for indices
+nbt=$3        # optional, number of build threads
 
-# optional input, number of build threads
-nbt=$3
-
-# Python
+# add newer python install to path
 export PATH=/usr/apps/python/latest/bin:$PATH
-
-# cmake
-source /usr/casc/sundials/apps/rh6/cmake/cmake-2.8.10.2/setup.sh
-
-# openmpi
-source /usr/casc/sundials/apps/rh6/openmpi/1.8.8/setup.sh
 
 # number of threads in OpenMP examples
 export OMP_NUM_THREADS=4
 
+# set file permissions (rwxrwxr-x)
 umask 002
+
+# path to cmake
+MYCMAKE=/usr/casc/sundials/apps/rh6/cmake/cmake-2.8.10.2/bin/cmake
+
+# path to installed libraries
+APPDIR=/usr/casc/sundials/apps/rh6
+MPIDIR=${APPDIR}/openmpi/1.8.8/bin
 
 # create build directory
 \rm -rf suntest_minimal_${realtype}_${indextype}
@@ -51,24 +51,34 @@ cd suntest_minimal_${realtype}_${indextype}
 # Note the -LAH flag lists the non-advanced cached variables (L), 
 # the dvanced variables (A), and help for each variable (H). This
 # will not print any system variables.
-cmake \
+$MYCMAKE \
     -D SUNDIALS_PRECISION=$realtype \
     -D SUNDIALS_INDEX_TYPE=$indextype \
+    \
+    -D CXX_ENABLE=ON \
+    -D FCMIX_ENABLE=ON \
+    -D F90_ENABLE=ON \
+    \
+    -D OPENMP_ENABLE=ON \
+    -D PTHREAD_ENABLE=ON \
+    \
+    -D CMAKE_C_COMPILER="/usr/bin/cc" \
+    -D CMAKE_CXX_COMPILER="/usr/bin/c++" \
+    -D CMAKE_Fortran_COMPILER="/usr/bin/gfortran" \
     \
     -D CMAKE_C_FLAGS='-Wall -std=c99 -pedantic' \
     \
     -D MPI_ENABLE=ON \
-    -D OPENMP_ENABLE=ON \
-    -D PTHREAD_ENABLE=ON \
-    \
-    -D FCMIX_ENABLE=TRUE \
-    -D F90_ENABLE=TRUE \
-    -D CXX_ENABLE=TRUE \
+    -D MPI_MPICC="${MPIDIR}/mpicc" \
+    -D MPI_MPICXX="${MPIDIR}/mpicxx" \
+    -D MPI_MPIF77="${MPIDIR}/mpif77" \
+    -D MPI_MPIF90="${MPIDIR}/mpif90" \
+    -D MPI_RUN_COMMAND="${MPIDIR}/mpirun" \
     \
     -LAH \
     ../../. 2>&1 | tee configure.log
 
-# check return code
+# check cmake return code
 rc=${PIPESTATUS[0]}
 echo -e "\ncmake returned $rc\n" | tee -a configure.log
 if [ $rc -ne 0 ]; then
@@ -78,7 +88,7 @@ fi
 # build sundials
 make -j $nbt 2>&1 | tee make.log
 
-# check return code
+# check make return code
 rc=${PIPESTATUS[0]}
 echo -e "\nmake returned $rc\n" | tee -a make.log
 if [ $rc -ne 0 ]; then
@@ -88,7 +98,7 @@ fi
 # test sundials
 make test 2>&1 | tee test.log
 
-# check return code
+# check make test return code
 rc=${PIPESTATUS[0]}
 echo -e "\nmake test returned $rc\n" | tee -a test.log
 if [ $rc -ne 0 ]; then
