d2e14def77be2cd263cbaad4de3be4bb1957df42
David J. Gardner <gardner48@llnl.gov>
2017-09-19 10:54:57 -0700

    update copyright in CMake files

--- config/SundialsFortran.cmake
+++ config/SundialsFortran.cmake
@@ -1,13 +1,15 @@
 # ---------------------------------------------------------------
-# $Revision$
-# $Date$
-# ---------------------------------------------------------------
 # Programmer:  Radu Serban @ LLNL
 # ---------------------------------------------------------------
-# Copyright (c) 2008, The Regents of the University of California.
+# LLNS Copyright Start
+# Copyright (c) 2014, Lawrence Livermore National Security
+# This work was performed under the auspices of the U.S. Department 
+# of Energy by Lawrence Livermore National Laboratory in part under 
+# Contract W-7405-Eng-48 and in part under Contract DE-AC52-07NA27344.
 # Produced at the Lawrence Livermore National Laboratory.
 # All rights reserved.
 # For details, see the LICENSE file.
+# LLNS Copyright End
 # ---------------------------------------------------------------
 # Fortran-related tests for SUNDIALS CMake-based configuration.
 #
