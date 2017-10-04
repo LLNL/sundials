396ea8d7d8af56c0f689e41c154cb577b6115f0f
David J. Gardner <gardner48@llnl.gov>
2017-10-04 13:00:27 -0700

    Fix some ANSI C errors/warnings

--- examples/nvector/test_nvector.c
+++ examples/nvector/test_nvector.c
@@ -808,14 +808,14 @@ int Test_N_VLinearSum(N_Vector X, N_Vector Y, N_Vector Z, sunindextype local_len
  * --------------------------------------------------------------------*/
 int Test_N_VConst(N_Vector X, sunindextype local_length, int myid)
 {
-  int      fails = 0, failure = 0;
+  int      i, fails = 0, failure = 0;
   double   start_time, stop_time;
 
-  /* fill vector data */
-  /* unclear what is the purpose of this -- commenting out */
-//  for(i=0; i < local_length; i++){
-//    set_element(X, i, ZERO);
-//  }
+  /* fill vector data with zeros to prevent passing in the case where
+     the input vector is a vector of ones */
+  for(i=0; i < local_length; i++){
+    set_element(X, i, ZERO);
+  }
 
   start_time = get_time();
   N_VConst(ONE,X);
--- examples/sunlinsol/spbcgs/parallel/test_sunlinsol_spbcgs_parallel.c
+++ examples/sunlinsol/spbcgs/parallel/test_sunlinsol_spbcgs_parallel.c
@@ -32,6 +32,16 @@
 #include "test_sunlinsol.h"
 #include "mpi.h"
 
+#if defined(SUNDIALS_EXTENDED_PRECISION)
+#define GSYM "Lg"
+#define ESYM "Le"
+#define FSYM "Lf"
+#else
+#define GSYM "g"
+#define ESYM "e"
+#define FSYM "f"
+#endif
+
 /* constants */
 #define FIVE      RCONST(5.0)
 #define THOUSAND  RCONST(1000.0)
@@ -148,7 +158,7 @@ int main(int argc, char *argv[])
            (long int) (ProbData.nprocs * ProbData.Nloc));
     printf("  Preconditioning type = %i\n", pretype);
     printf("  Maximum Krylov subspace dimension = %i\n", maxl);
-    printf("  Solver Tolerance = %lg\n", tol);
+    printf("  Solver Tolerance = %"GSYM"\n", tol);
     printf("  timing output flag = %i\n\n", print_timing);
   }
   
@@ -422,7 +432,7 @@ int ATimes(void* Data, N_Vector v_vec, N_Vector z_vec)
   if (check_flag(s2, "N_VGetArrayPointer", 0)) return 1;
   Nloc = ProbData->Nloc;
   
-  // MPI equivalent of realtype type
+  /* MPI equivalent of realtype type */
   #if defined(SUNDIALS_SINGLE_PRECISION)
   #define REALTYPE_MPI_TYPE MPI_FLOAT
   #elif defined(SUNDIALS_DOUBLE_PRECISION)
