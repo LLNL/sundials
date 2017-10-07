a269f1809f5ec99f4aa9ddc544454d3a604e6210
David J. Gardner <gardner48@llnl.gov>
2017-10-06 17:33:51 -0700

    clean up ansi warnings about output formatting

--- examples/sunlinsol/pcg/serial/test_sunlinsol_pcg_serial.c
+++ examples/sunlinsol/pcg/serial/test_sunlinsol_pcg_serial.c
@@ -31,6 +31,16 @@
 #include <sundials/sundials_math.h>
 #include "test_sunlinsol.h"
 
+#if defined(SUNDIALS_EXTENDED_PRECISION)
+#define GSYM "Lg"
+#define ESYM "Le"
+#define FSYM "Lf"
+#else
+#define GSYM "g"
+#define ESYM "e"
+#define FSYM "f"
+#endif
+
 /* constants */
 #define FIVE      RCONST(5.0)
 #define THOUSAND  RCONST(1000.0)
@@ -129,9 +139,9 @@ int main(int argc, char *argv[])
   SetTiming(print_timing);
 
   printf("\nPCG linear solver test:\n");
-  printf("  Problem size = %ld\n", ProbData.N);
+  printf("  Problem size = %ld\n", (long int) ProbData.N);
   printf("  Maximum Krylov subspace dimension = %i\n", maxl);
-  printf("  Solver Tolerance = %lg\n", tol);
+  printf("  Solver Tolerance = %"GSYM"\n", tol);
   printf("  timing output flag = %i\n\n", print_timing);
   
   /* Create vectors */
