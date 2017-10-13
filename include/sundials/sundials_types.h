e009adce8789af0f7b485dcdc86dfa3aa6496b58
David J. Gardner <gardner48@llnl.gov>
2017-10-12 22:26:43 -0700

    Change sunindextype to use long int rather than long long
    Update sundials_types.h, types documentation, recent changes, and
    recent changes in solver documentation introductions.

--- include/sundials/sundials_types.h
+++ include/sundials/sundials_types.h
@@ -1,8 +1,4 @@
-/*
- * -----------------------------------------------------------------
- * $Revision$
- * $Date$
- * ----------------------------------------------------------------- 
+/* -----------------------------------------------------------------
  * Programmer(s): Scott Cohen, Alan Hindmarsh, Radu Serban,
  *                Aaron Collier, and Slaven Peles @ LLNL
  * -----------------------------------------------------------------
@@ -15,7 +11,7 @@
  * All rights reserved.
  * For details, see the LICENSE file.
  * LLNS Copyright End
- *------------------------------------------------------------------
+ * -----------------------------------------------------------------
  * This header file exports three types: realtype, sunindextype and 
  * booleantype, as well as the constants TRUE and FALSE.
  *
@@ -47,8 +43,7 @@
  * then RCONST(1.0) expands to 1.0L. There is never a need to
  * explicitly cast 1.0 to (realtype). The macro can be used for 
  * literal constants only. It cannot be used for expressions. 
- *------------------------------------------------------------------
- */
+ * -----------------------------------------------------------------*/
   
 #ifndef _SUNDIALSTYPES_H
 #define _SUNDIALSTYPES_H
@@ -107,7 +102,7 @@ typedef long double realtype;
  * Defines integer type to be used for vector and matrix indices.
  * User can build sundials to use 32- or 64-bit signed integers.
  * If compiler does not support portable data types, 'int' is used
- * in place of 32- and 'long long' is used in place of 64-bit 
+ * in place of 32- and 'long int' is used in place of 64-bit 
  * integer.
  *------------------------------------------------------------------
  */
@@ -117,7 +112,7 @@ typedef long double realtype;
 #if __STDC_VERSION__ >= 199901L
 typedef int64_t sunindextype;
 #else
-typedef long long sunindextype;
+typedef long int sunindextype;
 #endif
 
 #elif defined(SUNDIALS_INT32_T)
