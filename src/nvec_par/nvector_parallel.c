644b5d03b6fb94e768a8a5941476dd21a6b3617e
David J. Gardner <gardner48@llnl.gov>
2017-10-27 13:26:11 -0700

    change printf to fprintf(stderr,...) for error in parallel vector

--- src/nvec_par/nvector_parallel.c
+++ src/nvec_par/nvector_parallel.c
@@ -91,7 +91,7 @@ N_Vector N_VNewEmpty_Parallel(MPI_Comm comm,
   n = local_length;
   MPI_Allreduce(&n, &Nsum, 1, PVEC_INTEGER_MPI_TYPE, MPI_SUM, comm);
   if (Nsum != global_length) {
-    printf(BAD_N);
+    fprintf(stderr, BAD_N);
     return(NULL);
   } 
 
