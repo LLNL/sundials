9b6c9ef7c022c5e8f04c8aaf912fa6def4fe367e
David J. Gardner <gardner48@llnl.gov>
2017-10-27 21:30:37 -0700

    Add SUNDIALS_DEVTESTS flag; Add keyword EXAMPLE_TYPE to SUNDIALS_ADD_TEST
    Add check to see if README_DEV exists to determine if this is a released
    or development version of Sundials and set the SUNDIALS_DEVTESTS flag
    accordingly. If SUNDIALS_DEVTESTS is true, then all examples are
    added to the regression tests (i.e. current make test behavior).
    
    Add the keyword argument EXAMPLE_TYPE to the SUNDIALS_ADD_TEST macro to
    indicate which examples should NOT be in released regression testing.
    If SUNDIALS_DEVTEST if false, then only examples with an empty example
    type are added to regression tests. Excluded examples are given the
    'develop' example type.
    
    Clean up comments in testRunner script.

--- config/SundialsAddTest.cmake
+++ config/SundialsAddTest.cmake
@@ -12,14 +12,17 @@
 # LLNS Copyright End
 # ---------------------------------------------------------------
 #
-# SUNDIALS_ADD_TEST(<test name> <executable> )
+# SUNDIALS_ADD_TEST(<test name> <executable>)
 # 
-# Add Sundials regression test.
-
-# Executable is run and output is compared with output in the
-# test/answers directory.  If output differs significantly then test
-# fails.  Default signicance is 4 decimal points for floating values
-# and 10% for integer values.
+# CMake macro to add a Sundials regression test. Keyword input
+# arguments can be added after <executable> to set regression
+# test options (see oneValueArgs and multiValueArgs below).
+#
+# The executable is run and its output compared with the output in the
+# test/answers directory. If the output differs significantly then the
+# test fails. The default signicance is 4 decimal points for floating
+# values and 10% for integer values.
+# ---------------------------------------------------------------
 
 IF(EXAMPLES_ENABLED)
 
@@ -31,26 +34,42 @@ IF(EXAMPLES_ENABLED)
     ENDIF()
   ENDIF()
 
+  # look for the testRunner script in the test directory
   FIND_PROGRAM(TESTRUNNER testRunner PATHS test)
 ENDIF(EXAMPLES_ENABLED)
 
 macro(SUNDIALS_ADD_TEST NAME EXECUTABLE)
 
+  # macro options
   set(options "")
-  set(oneValueArgs "MPI_NPROCS" "ANSWER_FILE" "FLOAT_PRECISION" "INTEGER_PERCENTAGE")
+
+  # macro keyword inputs followed by a single value
+  # MPI_NPROCS         = number of mpi tasks to use in parallel tests
+  # ANSWER_FILE        = name of answer file if different from the test name
+  # FLOAT_PRECISION    = precision for floating point failure comparision (num digits)
+  # INTEGER_PRECENTAGE = integer percentage difference for failure comparison
+  # EXAMPLE_TYPE       = release or develop examples
+  set(oneValueArgs "MPI_NPROCS" "ANSWER_FILE" "FLOAT_PRECISION" "INTEGER_PERCENTAGE"
+    "EXAMPLE_TYPE")
+
+  # macro keyword inputs followed by multiple values
+  # TEST_ARGS = command line arguments to pass to the test executable
   set(multiValueArgs "TEST_ARGS")
 
+  # parse inputs and create variables SUNDIALS_ADD_TEST_<keyword>
   CMAKE_PARSE_ARGUMENTS(SUNDIALS_ADD_TEST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
   # SGS add check to make sure parallel is integer
   # SGS add check for float and integer precision
 
+  # command line arguments for the test runner script
   set(TEST_ARGS  "-v" "--testname=${NAME}" 
     "--executablename=$<TARGET_FILE:${EXECUTABLE}>"
     "--answersdir=${CMAKE_SOURCE_DIR}/test/answers"
     "--outputdir=${CMAKE_BINARY_DIR}/Testing/output"
     )
 
+  # set the number of mpi tasks to use in parallel tests
   IF("${SUNDIALS_ADD_TEST_MPI_NPROCS}" STREQUAL "")
   ELSE()
 
@@ -67,28 +86,35 @@ macro(SUNDIALS_ADD_TEST NAME EXECUTABLE)
 
   ENDIF()
   
+  # set the test input args
   IF("${SUNDIALS_ADD_TEST_TEST_ARGS}" STREQUAL "")
   ELSE()
     string (REPLACE ";" " " USER_ARGS "${SUNDIALS_ADD_TEST_TEST_ARGS}")
     LIST(APPEND TEST_ARGS "--runargs=\"${USER_ARGS}\"")
   ENDIF()
 
+  # set the tests answer file name
   IF("${SUNDIALS_ADD_TEST_ANSWER_FILE}" STREQUAL "")
   ELSE()
     LIST(APPEND TEST_ARGS "--answerfile=${SUNDIALS_ADD_TEST_ANSWER_FILE}")
   ENDIF()
 
+  # set the precision for floating point failure comparison (number of digits)
   IF("${SUNDIALS_ADD_TEST_FLOAT_PRECISION}" STREQUAL "")
   ELSE()
     LIST(APPEND TEST_ARGS "--floatprecision=${SUNDIALS_ADD_TEST_FLOAT_PRECISION}")
   ENDIF()
 
+  # set the integer percentage difference for failure comparison
   IF("${SUNDIALS_ADD_TEST_INTEGER_PERCENTAGE}" STREQUAL "")
   ELSE()
     LIST(APPEND TEST_ARGS "--integerpercentage=${SUNDIALS_ADD_TEST_INTEGER_PERCENTAGE}")
   ENDIF()
 
-  ADD_TEST(NAME ${NAME}
-    COMMAND ${PYTHON_EXECUTABLE} ${TESTRUNNER} ${TEST_ARGS})
+  # create test case with the corresponding test runner command and arguments
+  # all tests are added during development and only unlabeled tests when released
+  IF(${SUNDIALS_DEVTESTS} OR "${SUNDIALS_ADD_TEST_EXAMPLE_TYPE}" STREQUAL "")
+    ADD_TEST(NAME ${NAME} COMMAND ${PYTHON_EXECUTABLE} ${TESTRUNNER} ${TEST_ARGS})
+  ENDIF()
 
 endmacro()
