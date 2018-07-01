c5c4e7b23bed8933d77463fcf93d16b4d5f03f8a
David J. Gardner <gardner48@llnl.gov>
2018-06-30 22:04:49 -0700

    remove setup from check for required functions

--- src/ida/ida_nls.c
+++ src/ida/ida_nls.c
@@ -56,7 +56,6 @@ int IDASetNonlinearSolver(void *ida_mem, SUNNonlinearSolver NLS)
   /* check for required nonlinear solver functions */
   if ( NLS->ops->gettype    == NULL ||
        NLS->ops->initialize == NULL ||
-       NLS->ops->setup      == NULL ||
        NLS->ops->solve      == NULL ||
        NLS->ops->free       == NULL ||
        NLS->ops->setsysfn   == NULL ) {
