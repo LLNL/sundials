b3cf4ea1abdfd8303cace4fbbd78ee8003820c82
Sandro Dias Pinto Vitenti <sandro@isoftware.com.br>
2019-01-28 16:34:54 -0200

    Check if NLS is owned by the cvode(s) struct before freeing it in CVodeSetNonlinearSolver. This avoids double freeing the non-linear solver in some contexts.

--- src/cvode/cvode_nls.c
+++ src/cvode/cvode_nls.c
@@ -75,7 +75,7 @@ int CVodeSetNonlinearSolver(void *cvode_mem, SUNNonlinearSolver NLS)
   }
 
   /* free any existing nonlinear solver */
-  if (cv_mem->NLS) retval = SUNNonlinSolFree(cv_mem->NLS);
+  if (cv_mem->ownNLS && cv_mem->NLS) retval = SUNNonlinSolFree(cv_mem->NLS);
 
   /* set SUNNonlinearSolver pointer */
   cv_mem->NLS = NLS;
