d49747cb03e1549fcddd05b43c9938ceb0198e62
Balos, Cody Joe <balos1@llnl.gov>
2019-02-21 14:25:06 -0800

    fix parallel builds

--- config/SundialsAddF2003InterfaceLibrary.cmake
+++ config/SundialsAddF2003InterfaceLibrary.cmake
@@ -14,20 +14,32 @@
 # CMake macro for adding F2003 interface libraries.
 # Wraps the add_library command for adding Fortran 2003 interfaces.
 #
+#     sundials_add_f2003_interface_library(<target>
+#         [STATIC | STATIC_OBJECT | SHARED | SHARED_OBJECT])
+#
 # It appends the library type to the Fortran_MODULE_DIRECTORY so
 # that the .mod file generated during compilation of shared and
-# static libraries do not collide.
-# It also adds the .mod file directory to the target's includes.
-# It also adds the C library as a library to link to.
+# static libraries do not collide. It also adds the .mod file
+# directory to the target's includes. It also adds the C library
+# as a library to link to.
 #
-# REQUIRES following the F2003 interface library naming convention.
-# I.e. if the C library/target is sundials_cvode_static then the
-# F2003 target must be sundials_fcvode_mod_static.
+# REQUIRES that <target> follows the SUNDIALS F2003 interface
+# library naming convention. I.e. if the C library/target is
+# sundials_cvode_static then the <target> must be
+# sundials_fcvode_mod_static.
+#
+# THE STATIC_OBJECT option generates a STATIC library target
+# with the name <target> and an object library target with
+# the name <target>_obj.
+# 
+# THE SHARED_OBJECT option generates a SHARED library target
+# with the name <target> and an object library target with
+# the name <target>_obj.
 # ---------------------------------------------------------------
 
-MACRO(sundials_add_f2003_interface_library target libtype)
+MACRO(sundials_add_f2003_interface_library target)
 
-  set(options NO_OBJECT_LIBRARY)
+  set(options STATIC STATIC_OBJECT SHARED SHARED_OBJECT)
   set(oneValueArgs )
   set(multiValueArgs )
 
@@ -38,7 +50,18 @@ MACRO(sundials_add_f2003_interface_library target libtype)
   # source files are unparsed additional arguments
   set(sources ${sundials_add_f2003_interface_library_UNPARSED_ARGUMENTS})
 
-  if(NOT ${sundials_add_f2003_interface_library_NO_OBJECT_LIBRARY})
+  # determine the library type
+  if(sundials_add_f2003_interface_library_STATIC OR sundials_add_f2003_interface_library_STATIC_OBJECT)
+    set(libtype "STATIC")
+  elseif(sundials_add_f2003_interface_library_SHARED OR sundials_add_f2003_interface_library_SHARED_OBJECT)
+    set(libtype "SHARED")
+  elseif(BUILD_SHARED_LIBS)
+    set(libtype "SHARED")
+  else()
+    set(libtype "STATIC")
+  endif()
+
+  if(sundials_add_f2003_interface_library_STATIC_OBJECT OR sundials_add_f2003_interface_library_SHARED_OBJECT)
     # give the object library its own target name
     set(obj_target ${target}_obj)
     # create the target for the object library
