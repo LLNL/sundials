74ab577c25feba33ec5f0cdc2d0b8d2a070d774e
Gardner, David James <gardner48@llnl.gov>
2020-12-03 16:20:22 -0800

    Merge pull request #413 in SUNDIALS/sunrepo from bugfix/vector-test-memory-leak to develop
    Squashed commit of the following:
    
    commit 31dbeff0ca3af9f83f4acbeaccdd5c0706de2a01
    Author: David J. Gardner <gardner48@llnl.gov>
    Date:   Thu Dec 3 14:18:15 2020 -0800
    
        free buffer in pack/unpack vector test

--- examples/nvector/test_nvector.c
+++ examples/nvector/test_nvector.c
@@ -5310,6 +5310,7 @@ int Test_N_VBufPack(N_Vector x, sunindextype local_length, int myid)
   stop_time = get_time();
 
   if (flag != 0) {
+    free(buf);
     printf(">>> FAILED test -- N_VBufPack returned %d, Proc %d \n", flag, myid);
     return(1);
   }
@@ -5320,6 +5321,7 @@ int Test_N_VBufPack(N_Vector x, sunindextype local_length, int myid)
   }
 
   if (failure) {
+    free(buf);
     printf(">>> FAILED test -- N_VBufPack failed, Proc %d \n", myid);
     return(1);
   } else if (myid == 0) {
@@ -5329,6 +5331,9 @@ int Test_N_VBufPack(N_Vector x, sunindextype local_length, int myid)
   /* find max time across all processes */
   maxt = max_time(x, stop_time - start_time);
   PRINT_TIME("N_VBufPack", maxt);
+
+  /* free buffer */
+  free(buf);
 
   return(0);
 }
@@ -5373,6 +5378,7 @@ int Test_N_VBufUnpack(N_Vector x, sunindextype local_length, int myid)
   stop_time = get_time();
 
   if (flag != 0) {
+    free(buf);
     printf(">>> FAILED test -- N_VBufUnPack returned %d, Proc %d \n", flag, myid);
     return(1);
   }
@@ -5381,6 +5387,7 @@ int Test_N_VBufUnpack(N_Vector x, sunindextype local_length, int myid)
   failure = check_ans(ONE, x, local_length);
 
   if (failure) {
+    free(buf);
     printf(">>> FAILED test -- N_VBufUnpack failed, Proc %d \n", myid);
     return(1);
   } else if (myid == 0) {
@@ -5390,6 +5397,9 @@ int Test_N_VBufUnpack(N_Vector x, sunindextype local_length, int myid)
   /* find max time across all processes */
   maxt = max_time(x, stop_time - start_time);
   PRINT_TIME("N_VBufUnpack", maxt);
+
+  /* free buffer */
+  free(buf);
 
   return(0);
 }
