3d7a55a1b3d2fcca90c3195782ac0c1ee42cb971
Timothy Bourke <tim@tbrk.org>
2021-08-04 10:21:29 +0200

    SUNMatCopy: also copy the matvecsetup op
    Signed-off-by: Timothy Bourke <tim@tbrk.org>

--- src/sundials/sundials_matrix.c
+++ src/sundials/sundials_matrix.c
@@ -90,15 +90,16 @@ int SUNMatCopyOps(SUNMatrix A, SUNMatrix B)
   if (A->ops == NULL || B->ops == NULL) return(-1);
 
   /* Copy ops from A to B */
-  B->ops->getid     = A->ops->getid;
-  B->ops->clone     = A->ops->clone;
-  B->ops->destroy   = A->ops->destroy;
-  B->ops->zero      = A->ops->zero;
-  B->ops->copy      = A->ops->copy;
-  B->ops->scaleadd  = A->ops->scaleadd;
-  B->ops->scaleaddi = A->ops->scaleaddi;
-  B->ops->matvec    = A->ops->matvec;
-  B->ops->space     = A->ops->space;
+  B->ops->getid       = A->ops->getid;
+  B->ops->clone       = A->ops->clone;
+  B->ops->destroy     = A->ops->destroy;
+  B->ops->zero        = A->ops->zero;
+  B->ops->copy        = A->ops->copy;
+  B->ops->scaleadd    = A->ops->scaleadd;
+  B->ops->scaleaddi   = A->ops->scaleaddi;
+  B->ops->matvecsetup = A->ops->matvecsetup;
+  B->ops->matvec      = A->ops->matvec;
+  B->ops->space       = A->ops->space;
 
   return(0);
 }
