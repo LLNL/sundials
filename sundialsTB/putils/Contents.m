07a44d8b63201a501b03d301dbf4c11a65c23cae
Balos, Cody Joe <balos1@llnl.gov>
2022-04-21 11:17:24 -0700

    remove sundialsTB

--- sundialsTB/putils/Contents.m
+++
@@ -1,9 +0,0 @@
-% Parallel utilities for the Matlab SUNDIALS interfaces
-%
-% Functions:
-%
-% mpirun     - runs parallel examples
-% mpiruns    - runs the parallel example on a child MATLAB process
-% mpistart   - lamboot and MPI_Init master (if required)
-
-
