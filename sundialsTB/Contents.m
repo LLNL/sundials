bd4e4649004897c167ba08e5f0df3edfa37b292b
Balos, Cody Joe <balos1@llnl.gov>
2022-04-21 11:17:24 -0700

    remove sundialsTB

--- sundialsTB/Contents.m
+++
@@ -1,6 +0,0 @@
-% sundialsTB v.2.4.0 - Matlab interfaces to SUNDIALS solvers
-%
-% sundialsTB provides interfaces to the CVODES, IDAS, and KINSOL
-%   solvers in SUNDIALS.
-%
-% See also cvodes, idas, kinsol, nvector, putils
