e32c87169284ef8bce9595b47e2006dbc42147e1
Cody Balos <balos1@llnl.gov>
2022-05-06 17:12:50 -0700

    fix typo

--- test/env/docker.sh
+++ test/env/docker.sh
@@ -87,8 +87,8 @@ APPROOT=/opt
 source ${APPROOT}/python-venv/sundocs/bin/activate
 
 # allow MPI to run as root in the docker container
-export OMP_ALLOW_RUN_AS_ROOT=1
-export OMP_ALLOW_RUN_AS_ROOT_CONFIRM=1
+export OMPI_ALLOW_RUN_AS_ROOT=1
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 
 # ------------------------------------------------------------------------------
 # Compilers and flags
