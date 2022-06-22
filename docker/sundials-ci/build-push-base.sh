68e936f157dfbed0a80c0969fdb0382481521254
Balos, Cody Joe <balos1@llnl.gov>
2022-06-22 13:25:03 -0700

    correct tag

--- docker/sundials-ci/build-push-base.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-export DOCKER_BUILDKIT=1
-export BUILDKIT_PROGRESS=plain
-set -x
-docker build -t ghcr.io/llnl/sundials-ci-e4s-base:22.05:22.05 e4s-base/
-docker push ghcr.io/llnl/sundials-ci-e4s-base:22.05:22.05
