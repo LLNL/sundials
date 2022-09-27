4c56b97b6ccadc27391f680a19cadbd796f0c726
Balos, Cody, J <balos1@llnl.gov>
2022-09-27 00:13:11 -0700

    update build_and_test to find host configs

--- .gitlab/build_and_test.sh
+++ .gitlab/build_and_test.sh
@@ -121,18 +121,18 @@ if [[ -z ${hostconfig} ]]
 then
     # If no host config file was provided, we assume it was generated.
     # This means we are looking of a unique one in project dir.
-    hostconfigs=( $( ls "${project_dir}/"hc-*.cmake ) )
+    hostconfigs=( $( ls "${project_dir}/"*.cmake ) )
     if [[ ${#hostconfigs[@]} == 1 ]]
     then
         hostconfig_path=${hostconfigs[0]}
         echo "Found host config file: ${hostconfig_path}"
     elif [[ ${#hostconfigs[@]} == 0 ]]
     then
-        echo "No result for: ${project_dir}/hc-*.cmake"
+        echo "No result for: ${project_dir}/*.cmake"
         echo "Spack generated host-config not found."
         exit 1
     else
-        echo "More than one result for: ${project_dir}/hc-*.cmake"
+        echo "More than one result for: ${project_dir}/*.cmake"
         echo "${hostconfigs[@]}"
         echo "Please specify one with HOST_CONFIG variable"
         exit 1
@@ -145,10 +145,15 @@ fi
 # Build Directory
 if [[ -z ${build_root} ]]
 then
-    build_root=$(pwd)
+    build_root="/dev/shm$(pwd)"
+else
+    build_root="/dev/shm${build_root}"
 fi
 
-build_dir="${build_root}/build_${job_unique_id}"
+build_dir="${build_root}/build_${job_unique_id}_${hostconfig//.cmake/}"
+install_dir="${build_root}/install_${job_unique_id}_${hostconfig//.cmake/}"
+
+cmake_exe=`grep 'CMake executable' ${hostconfig_path} | cut -d ':' -f 2 | xargs`
 
 # Build
 if [[ "${option}" != "--deps-only" && "${option}" != "--test-only" ]]
@@ -169,18 +174,25 @@ then
     mkdir -p "${build_dir}" && cd "${build_dir}"
 
     date
-    cmake --version
+    
+    $cmake_exe --version
 
     # configure
-    cmake -C "${hostconfig_path}" "${project_dir}"
+    $cmake_exe \
+        -C "${hostconfig_path}" \
+        -DCMAKE_INSTALL_PREFIX=${install_dir} \
+        "${project_dir}"
 
     # build
     VERBOSE_BUILD=${VERBOSE_BUILD:-"OFF"}
     if [[ "${VERBOSE_BUILD}" == "ON" ]]; then
         verbose_build='--verbose'
     fi
+    $cmake_exe --build . -j ${BUILD_JOBS} ${verbose_build}
 
-    cmake --build . -j ${BUILD_JOBS} ${verbose_build}
+    # install
+    make install
+
     date
 fi
 
