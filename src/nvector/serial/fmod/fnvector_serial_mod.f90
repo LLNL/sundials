be8773835e3710c4e93100e7f12edf0da848d578
Balos, Cody Joe <balos1@llnl.gov>
2022-10-18 09:10:59 -0700

    add patch that fixes GetArrayPointer

--- src/nvector/serial/fmod/fnvector_serial_mod.f90
+++ src/nvector/serial/fmod/fnvector_serial_mod.f90
@@ -755,7 +755,7 @@ type(C_PTR) :: farg1
 
 farg1 = c_loc(v)
 fresult = swigc_FN_VGetArrayPointer_Serial(farg1)
-call c_f_pointer(fresult, swig_result, [1])
+call c_f_pointer(fresult, swig_result, [FN_VGetLength_Serial(v)])
 end function
 
 subroutine FN_VSetArrayPointer_Serial(v_data, v)
