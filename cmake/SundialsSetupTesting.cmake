0802600442fc8a824b6178d832acd4750633a95b
Balos, Cody, J <balos1@llnl.gov>
2023-01-12 01:38:11 -0800

    temporarily disable gtest

--- cmake/SundialsSetupTesting.cmake
+++ cmake/SundialsSetupTesting.cmake
@@ -123,23 +123,22 @@ endif()
 
 # Check if unit tests are enabled
 if(SUNDIALS_TEST_UNITTESTS)
-  message(WARNING "Could not find Google Test so we are cloning it from GitHub...")
-  include(FetchContent)
-  FetchContent_Declare(
-    googletest
-    URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
-  )
-  if(WIN32)
-    # For Windows: Prevent overriding the parent project's compiler/linker settings
-    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-  endif()
-  # FetchContent_MakeAvailable(googletest)
-  FetchContent_GetProperties(googletest)
-  if(NOT googletest_POPULATED)
-    FetchContent_Populate(googletest)
-    add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
-  endif()
-  include(GoogleTest)
+  # message(WARNING "Could not find Google Test so we are cloning it from GitHub...")
+  # include(FetchContent)
+  # FetchContent_Declare(
+  #   googletest
+  #   URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
+  # )
+  # if(WIN32)
+  #   # For Windows: Prevent overriding the parent project's compiler/linker settings
+  #   set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+  # endif()
+  # FetchContent_GetProperties(googletest)
+  # if(NOT googletest_POPULATED)
+  #   FetchContent_Populate(googletest)
+  #   add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
+  # endif()
+  # include(GoogleTest)
 endif()
 
 # If examples are installed, create post install smoke test targets
