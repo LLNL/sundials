548de1f320177ce2883f187e95f0ecf233c1c223
David J. Gardner <gardner48@llnl.gov>
2023-03-17 19:26:56 -0700

    add sludist code to gpu version

--- benchmarks/diffusion_2D/mpi_gpu/diffusion.cpp
+++ benchmarks/diffusion_2D/mpi_gpu/diffusion.cpp
@@ -414,3 +414,223 @@ int laplacian(realtype t, N_Vector u, N_Vector f, UserData* udata)
   // Return success
   return 0;
 }
+
+#if defined(USE_SUPERLU_DIST)
+// Compute the global index of a node
+//   i -- local x index
+//   j -- local y index
+//   x -- x processor coordinate
+//   y -- y processor coordinate
+sunindextype global_index(sunindextype i, sunindextype j, int x, int y,
+                          UserData* udata)
+{
+  // Unpack values (same for all ranks)
+  sunindextype qx = udata->qx;
+  sunindextype qy = udata->qy;
+  sunindextype rx = udata->rx;
+  sunindextype ry = udata->ry;
+  sunindextype nx = udata->nx;
+
+  // offset from previous process rows
+  sunindextype offset_p = nx * ((qy + 1) * min(y, ry) + qy * max(y - ry, 0));
+
+  // offset within current process row
+  sunindextype offset_c;
+  if (y < ry)
+  {
+    offset_c = (qy + 1) * ((qx + 1) * min(x, rx) + qx * max(x - rx, 0));
+  }
+  else
+  {
+    offset_c = qy * ((qx + 1) * min(x, rx) + qx * max(x - rx, 0));
+  }
+
+  // nx_loc for the input (x,y) process (not necessarily the same as nx_loc in
+  // user data because this could be the index for a neighboring rank)
+  sunindextype nx_loc;
+  if (x < rx) nx_loc = qx + 1;
+  else nx_loc = qx;
+
+  // global index for the requested local node
+  return offset_p + offset_c + j * nx_loc + i;
+}
+
+// Compute the global column indices for a row
+//   i -- local x index
+//   j -- local y index
+//   x -- x processor coordinate
+//   y -- y processor coordinate
+int matrix_columns(sunindextype i, sunindextype j, int x, int y,
+                   UserData* udata, sunrealtype* vals, sunindextype* col_idx,
+                   sunindextype* row_nnz)
+{
+  // Unpack values
+  int npx = udata->npx;
+  int npy = udata->npy;
+
+  sunindextype qx = udata->qx;
+  sunindextype qy = udata->qy;
+  sunindextype rx = udata->rx;
+  sunindextype ry = udata->ry;
+
+  // -----------------
+  // global boundaries
+  // -----------------
+
+  if ((y == 0 && j == 0) ||                       // south
+      (x == 0 && i == 0) ||                       // west
+      (x == npx - 1 && i == udata->nx_loc - 1) || // east
+      (y == npy - 1 && j == udata->ny_loc - 1))   // north
+  {
+    vals[0]    = 0;
+    col_idx[0] = global_index(i, j, x, y, udata);
+    *row_nnz   = 1;
+    return 0;
+  }
+
+  // --------
+  // interior
+  // --------
+
+  // Ordering of columns for output
+  //   0. South Neighbor
+  //   1. West Neighbor
+  //   Interior:
+  //      2. South
+  //      3. West
+  //      4. Center
+  //      5. East
+  //      6. North
+  //   7. East Neighbor
+  //   8. North Neighbor
+
+  // Constants for computing Jacobian
+  sunrealtype cx = udata->kx / (udata->dx * udata->dx);
+  sunrealtype cy = udata->ky / (udata->dy * udata->dy);
+  sunrealtype cc = -TWO * (cx + cy);
+
+  // Value and Columns index
+  sunindextype idx = 0;
+
+  // south neighbor
+  if (j == 0)
+  {
+    // neighbor ny_loc
+    sunindextype ny_loc;
+    if (y - 1 < ry) ny_loc = qy + 1;
+    else ny_loc = qy;
+
+    vals[idx]    = cy;
+    col_idx[idx] = global_index(i, ny_loc - 1, x, y - 1, udata);
+    idx += 1;
+  }
+
+  // west neighbor
+  if (i == 0)
+  {
+    // neighbor nx_loc
+    sunindextype nx_loc;
+    if (x - 1 < rx) nx_loc = qx + 1;
+    else nx_loc = qx;
+
+    vals[idx]    = cx;
+    col_idx[idx] = global_index(nx_loc - 1, j, x - 1, y, udata);
+    idx += 1;
+  }
+
+  // south interior
+  if (j > 0)
+  {
+    vals[idx]    = cy;
+    col_idx[idx] = global_index(i, j - 1, x, y, udata);
+    idx += 1;
+  }
+
+  // west interior
+  if (i > 0)
+  {
+    vals[idx]    = cx;
+    col_idx[idx] = global_index(i - 1, j, x, y, udata);
+    idx += 1;
+  }
+
+  // center
+  vals[idx]    = cc;
+  col_idx[idx] = global_index(i, j, x, y, udata);
+  idx += 1;
+
+  // east interior
+  if (i < udata->nx_loc - 1)
+  {
+    vals[idx]    = cx;
+    col_idx[idx] = global_index(i + 1, j, x, y, udata);
+    idx += 1;
+  }
+
+  // north interior
+  if (j < udata->ny_loc - 1)
+  {
+    vals[idx]    = cy;
+    col_idx[idx] = global_index(i, j + 1, x, y, udata);
+    idx += 1;
+  }
+
+  // east neighbor
+  if (i == udata->nx_loc - 1)
+  {
+    vals[idx]    = cx;
+    col_idx[idx] = global_index(0, j, x + 1, y, udata);
+    idx += 1;
+  }
+
+  // north neighbor
+  if (j == udata->ny_loc - 1)
+  {
+    vals[idx]    = cy;
+    col_idx[idx] = global_index(i, 0, x, y + 1, udata);
+    idx += 1;
+  }
+
+  // non-zeros in this row
+  *row_nnz = 5;
+
+  return 0;
+}
+
+int laplacian_matrix_sludist(N_Vector u, SUNMatrix L, UserData* udata)
+{
+  // Set shortcuts
+  SuperMatrix*  Lsuper   = SUNMatrix_SLUNRloc_SuperMatrix(L);
+  NRformat_loc* Lstore   = (NRformat_loc*)Lsuper->Store;
+  sunindextype* row_ptrs = Lstore->rowptr;
+  sunindextype* col_idxs = Lstore->colind;
+  sunrealtype*  data     = (sunrealtype*)Lstore->nzval;
+
+  int x = udata->idx;
+  int y = udata->idy;
+
+  // Initial row pointer value
+  row_ptrs[0] = 0;
+
+  sunindextype idx     = 0;
+  sunindextype row_nnz = 0;
+
+  for (sunindextype j = 0; j < udata->ny_loc; j++)
+  {
+    for (sunindextype i = 0; i < udata->nx_loc; i++)
+    {
+      matrix_columns(i, j, x, y, udata, data + row_ptrs[idx],
+                     col_idxs + row_ptrs[idx], &row_nnz);
+      row_ptrs[idx + 1] = row_ptrs[idx] + row_nnz;
+      idx++;
+    }
+  }
+
+  Lstore->nnz_loc = row_ptrs[idx];
+  Lstore->m_loc   = udata->nx_loc * udata->ny_loc;
+  Lstore->fst_row = global_index(0, 0, x, y, udata);
+
+  // Return success
+  return 0;
+}
+#endif
