397b8957f67c7a46b0a0bddaa56f6d0a2a0e4e5f
Yu Pan <yupan781@gmail.com>
2023-03-21 06:52:11 -0700

    add check for rocm version

--- .gitlab/build_and_test.sh
+++ .gitlab/build_and_test.sh
@@ -48,7 +48,8 @@ BUILD_JOBS=${BUILD_JOBS:-"1"}
 
 # load newer python to try the clingo concretizer
 # Corona does not have python 3.8.2
-if [[ "${hostname}" == "corona" ]]; then
+if [[ "${hostname}" == "corona" ]]
+then
     echo "module load python/3.9.12"
     module load python/3.9.12
 else
@@ -57,9 +58,15 @@ else
 fi
 
 # Rocm version specific to Corona.
-if [[ "${hostname}" == "corona" ]]; then
+if [[ "${hostname}" == "corona" ]]
+then
     echo "module load rocm/5.4.1"
-    module load rocm/5.4.1
+    if [-z "${ROCM_VERSION}"] 
+    then
+        module load rocm
+    else 
+        module load rocm/${ROCM_VERSION}
+    fi
 fi
 
 if [[ "${option}" != "--build-only" && "${option}" != "--test-only" ]]
