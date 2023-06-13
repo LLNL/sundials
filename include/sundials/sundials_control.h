52fd9d2d3f62ce9ee247c9bae7a91dabd744d6b9
Daniel R. Reynolds <reynolds@smu.edu>
2023-06-13 16:02:20 -0500

    Added thorough comments on SUNControl objects to main sundials_control.h header

--- include/sundials/sundials_control.h
+++ include/sundials/sundials_control.h
@@ -113,32 +113,107 @@ struct _generic_SUNControl
  * Functions exported by SUNControl module
  * ----------------------------------------------------------------- */
 
-SUNDIALS_EXPORT SUNControl SUNControlNewEmpty(SUNContext sunctx);
-SUNDIALS_EXPORT void SUNControlFreeEmpty(SUNControl C);
-SUNDIALS_EXPORT SUNControl_ID SUNControlGetID(SUNControl C);
-SUNDIALS_EXPORT void SUNControlDestroy(SUNControl C);
-SUNDIALS_EXPORT int SUNControlEstimateStep(
-                       SUNControl C, realtype h, realtype dsm,
-                       realtype* hnew);
-SUNDIALS_EXPORT int SUNControlEstimateStepAndOrder(
-                       SUNControl C, realtype h, int q,
-                       realtype dsm, realtype* hnew, int *qnew);
-SUNDIALS_EXPORT int SUNControlEstimateMRISteps(
-                       SUNControl C, realtype H, realtype h,
-                       realtype dsm, realtype* Hnew, realtype *hnew);
-SUNDIALS_EXPORT int SUNControlEstimateStepTol(
-                       SUNControl C, realtype H, realtype tolfac,
-                       realtype dsm, realtype *Hnew, realtype* tolfacnew);
-SUNDIALS_EXPORT int SUNControlReset(SUNControl C);
-SUNDIALS_EXPORT int SUNControlSetDefaults(SUNControl C);
-SUNDIALS_EXPORT int SUNControlWrite(SUNControl C, FILE* fptr);
-SUNDIALS_EXPORT int SUNControlSetMethodOrder(SUNControl C, int q);
-SUNDIALS_EXPORT int SUNControlSetEmbeddingOrder(SUNControl C, int p);
-SUNDIALS_EXPORT int SUNControlSetSafetyFactor(SUNControl C, realtype safety);
-SUNDIALS_EXPORT int SUNControlSetErrorBias(SUNControl C, realtype bias);
-SUNDIALS_EXPORT int SUNControlUpdate(SUNControl C, realtype h, realtype dsm);
-SUNDIALS_EXPORT int SUNControlSpace(
-                       SUNControl C, long int *lenrw, long int *leniw);
+/* Function to create an empty SUNHeuristics data structure. */
+SUNDIALS_EXPORT
+SUNControl SUNControlNewEmpty(SUNContext sunctx);
+
+/* Function to free an empty SUNHeuristics data structure. */
+SUNDIALS_EXPORT
+void SUNControlFreeEmpty(SUNControl C);
+
+/* Function to report the ID of a SUNHeuristics object. */
+SUNDIALS_EXPORT
+SUNControl_ID SUNControlGetID(SUNControl C);
+
+/* Function to deallocate a SUNHeuristics object. */
+SUNDIALS_EXPORT
+void SUNControlDestroy(SUNControl C);
+
+/* Main step size controller function.  This is called following
+   a time step with size 'h' and local error factor 'dsm', and the
+   controller should estimate 'hnew' so that the ensuing step
+   will have 'dsm' value JUST BELOW 1. */
+SUNDIALS_EXPORT
+int SUNControlEstimateStep(SUNControl C, realtype h, realtype dsm,
+                           realtype* hnew);
+
+/* Combined step size + order controller function.  This is called
+   following a time step with size 'h' and order 'q' that has local
+   error factor 'dsm'.  The controller should estimate 'hnew' and
+   'qnew' so that the ensuing step will have 'dsm' value JUST BELOW 1
+   with minimal computational effort. */
+SUNDIALS_EXPORT
+int SUNControlEstimateStepAndOrder(SUNControl C, realtype h, int q,
+                                   realtype dsm, realtype* hnew,
+                                   int *qnew);
+
+/* Combined slow/fast multirate step size controller function.  This
+   is called following a slow multirate time step with sizes 'H' and
+   'h' (slow and fast, resp.), and where the slow step has local error
+   factor 'dsm' (the MRI controller should request the fast error
+   factor directly from the fast integrator).  The controller should
+   estimate slow and fast steps 'Hnew' and 'hnew', resp., so that the
+   ensuing step will have 'dsm' value JUST BELOW 1 with minimal
+   computational effort. */
+SUNDIALS_EXPORT
+int SUNControlEstimateMRISteps(SUNControl C, realtype H, realtype h,
+                               realtype dsm, realtype* Hnew,
+                               realtype *hnew);
+
+/* Combined slow step/fast tolerance multirate controller function.
+   This is called following a slow multirate time step with size 'H'
+   and fast/slow relative tolerance ratio 'tolfac', and where the
+   slow step has local error factor 'dsm' (the MRI controller should
+   request the fast error factor directly from the fast integrator).
+   The controller should estimate slow stepsize 'Hnew' and updated
+   relative tolerance ratio 'tolfacnew', so that the ensuing step
+   will have 'dsm' value JUST BELOW 1 with minimal computational
+   effort. */
+SUNDIALS_EXPORT
+int SUNControlEstimateStepTol(SUNControl C, realtype H,
+                              realtype tolfac, realtype dsm,
+                              realtype *Hnew, realtype* tolfacnew);
+
+/* Function to reset the controller to its initial state, e.g., if
+   it stores a small number of previous dsm or step size values. */
+SUNDIALS_EXPORT
+int SUNControlReset(SUNControl C);
+
+/* Function to set the controller parameters to their default values. */
+SUNDIALS_EXPORT
+int SUNControlSetDefaults(SUNControl C);
+
+/* Function to write all controller parameters to the indicated file
+   pointer. */
+SUNDIALS_EXPORT
+int SUNControlWrite(SUNControl C, FILE* fptr);
+
+/* Function to set the asymptotic order of accuracy for the method. */
+SUNDIALS_EXPORT
+int SUNControlSetMethodOrder(SUNControl C, int q);
+
+/* Function to set the asymptotic order of accuracy for the embedding. */
+SUNDIALS_EXPORT
+int SUNControlSetEmbeddingOrder(SUNControl C, int p);
+
+/* Function to set a step size safety factor. */
+SUNDIALS_EXPORT
+int SUNControlSetSafetyFactor(SUNControl C, realtype safety);
+
+/* Function to set an error bias factor to use for scaling the local error
+   'dsm' factors above. */
+SUNDIALS_EXPORT
+int SUNControlSetErrorBias(SUNControl C, realtype bias);
+
+/* Function to notify the controller of a successful a time step with size
+   h and local error factor dsm, indicating the the step size or local error
+   factor can be saved for subsequent controller functions. */
+SUNDIALS_EXPORT
+int SUNControlUpdate(SUNControl C, realtype h, realtype dsm);
+
+/* Function to return the memory requirements of the controller object. */
+SUNDIALS_EXPORT
+int SUNControlSpace(SUNControl C, long int *lenrw, long int *leniw);
 
 
 /* -----------------------------------------------------------------
