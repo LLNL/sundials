aa2372b68a934e6f713e56f330eab676ad3d9d01
shahbajsohal <sohal.shahbaj4@gmail.com>
2023-06-23 10:33:04 -0700

    Created a macro to parse the example tuples and add each tuple into it's own list

---
+++ cmake/macros/SundialsParseTuple.cmake
@@ -0,0 +1,46 @@
+# ------------------------------------------------------------------------------
+# Programmer(s): Shahbaj Sohal @ LLNL
+# ------------------------------------------------------------------------------
+# SUNDIALS Copyright Start
+# Copyright (c) 2002-2023, Lawrence Livermore National Security
+# and Southern Methodist University.
+# All rights reserved.
+#
+# See the top-level LICENSE and NOTICE files for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# SUNDIALS Copyright End
+# ------------------------------------------------------------------------------
+#
+# sundials_parse_tuple(<example_tuple>)
+#
+# CMake macro to parse the lists of example tuples. Keyword input arguments can be
+# added after <examples_tuple> to put each tuple into its own list (see oneValueArgs below).
+
+macro(sundials_parse_tuple example_tuple)
+    set(options )
+    set(oneValueArgs FILENAME_VAR PARAMS_VAR NODES_VAR NPROCS_VAR TYPE_VAR PRECISION_VAR)
+    set(multiValueArgs)
+    
+    cmake_parse_arguments(sundials_parse_tuple
+        "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    if(sundials_parse_tuple_FILENAME_VAR)
+        list(GET example_tuple 0 ${sundials_parse_tuple_FILENAME_VAR})
+    endif()
+    if(sundials_parse_tuple_PARAMS_VAR)
+        list(GET example_tuple 1 ${sundials_parse_tuple_PARAMS_VAR})
+    endif()
+    if(sundials_parse_tuple_NODES_VAR)
+        list(GET example_tuple 2 ${sundials_parse_tuple_NODES_VAR})
+    endif()
+    if(sundials_parse_tuple_NPROCS_VAR)
+        list(GET example_tuple 3 ${sundials_parse_tuple_NPROCS_VAR})
+    endif()
+    if(sundials_parse_tuple_TYPE_VAR)
+        list(GET example_tuple 4 ${sundials_parse_tuple_TYPE_VAR})
+    endif()
+    if(sundials_parse_tuple_PRECISION_VAR)
+        list(GET example_tuple 5 ${sundials_parse_tuple_PRECISION_VAR})
+    endif()
+    
+endmacro()
