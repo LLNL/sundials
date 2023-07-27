399e6dbf02b0573463307ae086d331460eecd1bb
Yu Pan <yupan781@gmail.com>
2023-07-27 10:49:24 -0700

    add copyright to SundialsAddBenchmark.cmake

--- cmake/macros/SundialsAddBenchmark.cmake
+++ cmake/macros/SundialsAddBenchmark.cmake
@@ -1,5 +1,15 @@
-# Take in an executable, determine how to construct the arguments
-
+# ---------------------------------------------------------------
+# Programmer(s): Yu Pan @ LLNL
+# ---------------------------------------------------------------
+# SUNDIALS Copyright Start
+# Copyright (c) 2002-2023, Lawrence Livermore National Security
+# and Southern Methodist University.
+# All rights reserved.
+#
+# See the top-level LICENSE and NOTICE files for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# SUNDIALS Copyright End
 # ---------------------------------------------------------------
 # CMake macro for adding benchmarks to `make benchmark`.
 # ---------------------------------------------------------------
