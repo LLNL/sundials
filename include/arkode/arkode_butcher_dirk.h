d445aa5bcdf503b84f286df15201779c02097317
Balos, Cody, J <balos1@llnl.gov>
2023-10-26 09:05:13 -0700

    remove deprecated arkode code

--- include/arkode/arkode_butcher_dirk.h
+++ include/arkode/arkode_butcher_dirk.h
@@ -23,49 +23,6 @@
 extern "C" {
 #endif
 
-
-/* Butcher table accessor IDs
-     ERK:    0 -  99
-     DIRK: 100 - 199
-     MRI:  200 - 299          */
-
-/* DEPRECATED SDIRK_2_1_2: use ARKODE_SDIRK_2_1_2 */
-#define SDIRK_2_1_2             100
-/* DEPRECATED BILLINGTON_3_3_2: use ARKODE_BILLINGTON_3_3_2 */
-#define BILLINGTON_3_3_2        101
-/* DEPRECATED TRBDF2_3_3_2: use ARKODE_TRBDF2_3_3_2 */
-#define TRBDF2_3_3_2            102
-/* DEPRECATED KVAERNO_4_2_3: use ARKODE_KVAERNO_4_2_3 */
-#define KVAERNO_4_2_3           103
-/* DEPRECATED ARK324L2SA_DIRK_4_2_3: use ARKODE_ARK324L2SA_DIRK_4_2_3 */
-#define ARK324L2SA_DIRK_4_2_3   104
-/* DEPRECATED CASH_5_2_4: use ARKODE_CASH_5_2_4 */
-#define CASH_5_2_4              105
-/* DEPRECATED CASH_5_3_4: use ARKODE_CASH_5_3_4 */
-#define CASH_5_3_4              106
-/* DEPRECATED SDIRK_5_3_4: use ARKODE_SDIRK_5_3_4 */
-#define SDIRK_5_3_4             107
-/* DEPRECATED KVAERNO_5_3_4: use ARKODE_KVAERNO_5_3_4 */
-#define KVAERNO_5_3_4           108
-/* DEPRECATED ARK436L2SA_DIRK_6_3_4: use ARKODE_ARK436L2SA_DIRK_6_3_4 */
-#define ARK436L2SA_DIRK_6_3_4   109
-/* DEPRECATED KVAERNO_7_4_5: use ARKODE_KVAERNO_7_4_5 */
-#define KVAERNO_7_4_5           110
-/* DEPRECATED ARK548L2SA_DIRK_8_4_5: use ARKODE_ARK548L2SA_DIRK_8_4_5 */
-#define ARK548L2SA_DIRK_8_4_5   111
-/* DEPRECATED ARK437L2SA_DIRK_7_3_4: use ARKODE_ARK437L2SA_DIRK_7_3_4 */
-#define ARK437L2SA_DIRK_7_3_4   112
-/* DEPRECATED ARK548L2SAb_DIRK_8_4_5: use ARKODE_ARK548L2SAb_DIRK_8_4_5 */
-#define ARK548L2SAb_DIRK_8_4_5  113
-
-/* Utility #defines to ensure valid input IDs for DIRK tables */
-
-/* DEPRECATED MIN_DIRK_NUM: use ARKODE_MIN_DIRK_NUM */
-#define MIN_DIRK_NUM            100
-
-/* DEPRECATED MAX_DIRK_NUM: use ARKODE_MAX_DIRK_NUM */
-#define MAX_DIRK_NUM            113
-
 typedef enum {
   ARKODE_DIRK_NONE = -1, /* ensure enum is signed int */
   ARKODE_MIN_DIRK_NUM = 100,
