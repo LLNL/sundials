ad876c4186bd79bc948ceaff38aeaeb297b7f550
Cody Balos <balos1@llnl.gov>
2023-12-12 19:31:14 -0800

    error handling in cvode (#382)
    Co-authored-by: Daniel R. Reynolds <reynolds@smu.edu>

--- examples/cvode/parallel/cvAdvDiff_diag_p.c
+++ examples/cvode/parallel/cvAdvDiff_diag_p.c
@@ -44,9 +44,7 @@
 #include <nvector/nvector_parallel.h> /* access to MPI-parallel N_Vector     */
 #include <stdio.h>
 #include <stdlib.h>
-#include <sundials/sundials_logger.h>
-#include <sundials/sundials_mpi_errors.h>
-#include <sundials/sundials_types.h> /* definition of type realtype         */
+#include <sundials/sundials_core.h>
 
 /* Problem Constants */
 
@@ -125,11 +123,6 @@ int main(int argc, char* argv[])
   {
     MPI_Abort(comm, 1);
   }
-
-  /* Setup different error handler stack so that we abort after logging */
-  SUNContext_PopErrHandler(sunctx);
-  SUNContext_PushErrHandler(sunctx, SUNMPIAbortErrHandlerFn, NULL);
-  SUNContext_PushErrHandler(sunctx, SUNLogErrHandlerFn, NULL);
 
   /* Setup different error handler stack so that we abort after logging */
   SUNContext_PopErrHandler(sunctx);
