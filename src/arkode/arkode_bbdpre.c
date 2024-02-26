ead7727175096a03328eff629c11e7eb9df40bc8
Balos, Cody, J <balos1@llnl.gov>
2024-02-26 11:43:48 -0800

    more places where liw lrw dont match

--- src/arkode/arkode_bbdpre.c
+++ src/arkode/arkode_bbdpre.c
@@ -56,7 +56,8 @@ int ARKBBDPrecInit(void* arkode_mem, sunindextype Nlocal, sunindextype mudq,
   ARKodeMem ark_mem;
   ARKLsMem arkls_mem;
   ARKBBDPrecData pdata;
-  sunindextype muk, mlk, storage_mu, lrw1, liw1;
+  sunindextype muk, mlk, storage_mu;
+  long int lrw1, liw1;
   long int lrw, liw;
   int retval;
 
