33fe88b854e588eb358fba5ce300a0065ccb9d6b
Balos, Cody, J <balos1@llnl.gov>
2024-04-10 11:15:44 -0700

    add a implementation of a vector/arraylist

---
+++ src/sundials/sundials_arraylist.h
@@ -0,0 +1,128 @@
+/* -----------------------------------------------------------------
+ * Programmer: Cody J. Balos @ LLNL
+ * -----------------------------------------------------------------
+ * SUNDIALS Copyright Start
+ * Copyright (c) 2002-2024, Lawrence Livermore National Security
+ * and Southern Methodist University.
+ * All rights reserved.
+ *
+ * See the top-level LICENSE and NOTICE files for details.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SUNDIALS Copyright End
+ * -----------------------------------------------------------------
+ * Implementation of a resizable container similar to a std::vector.
+ * The values can be anything but data must be contiguous.
+ #
+ # To use the ArrayList, first define TTYPE with your data type
+ # before including this header. If you need ArrayLists that hold
+ # different types in the same file, then define TTYPE for the first,
+ # include this header, then repeat.
+ * -----------------------------------------------------------------*/
+
+#include <sundials/sundials_core.h>
+#include <stdlib.h>
+
+#ifndef TTYPE
+#error "Must define template type for SUNArrayList"
+#endif
+
+#define CONCAT(a, b) a##b
+#define PASTE(a, b) CONCAT(a, b)
+#define MAKE_NAME(prefix, name) PASTE(prefix, PASTE(_, name))
+
+#define SUNArrayListTtype_s MAKE_NAME(SUNArrayList, PASTE(TTYPE, _s))
+#define SUNArrayListTtype MAKE_NAME(SUNArrayList, TTYPE)
+
+typedef struct SUNArrayListTtype_s* SUNArrayListTtype;
+
+struct SUNArrayListTtype_s {
+  sunindextype size;
+  sunindextype capacity;
+  TTYPE* values;
+};
+
+// This constant controls how much space will be allocated when a resize is needed.
+// The new capacity is GROWTH_FACTOR*current_capacity.
+// Some std::vector implementations use 2, but 1.5 will be more conservative in terms
+// of the memory usage but yields a larger constant factor in terms of the
+// amorirtized constant time complexity.
+#define GROWTH_FACTOR 1.5
+
+static inline
+SUNArrayListTtype MAKE_NAME(SUNArrayListTtype, New)(sunindextype init_capacity) {
+  SUNArrayListTtype list = (SUNArrayListTtype) malloc(sizeof(struct SUNArrayListTtype_s));
+  if (!list) {
+    // Handle memory allocation failure
+    return NULL;
+  }
+  list->size = 0;
+  list->capacity = init_capacity > 0 ? init_capacity : 1;
+  list->values = (TTYPE*) malloc(sizeof(TTYPE) * list->capacity);
+  if (!list->values) {
+    // Handle memory allocation failure for values
+    free(list);
+    return NULL;
+  }
+  return list;
+}
+
+static inline
+void MAKE_NAME(SUNArrayListTtype, Destroy)(SUNArrayListTtype* list) {
+  if (!(*list)) return;
+  free((*list)->values);
+  free(*list);
+  *list = NULL;
+}
+
+static inline
+sunbooleantype MAKE_NAME(SUNArrayListTtype, IsEmpty)(SUNArrayListTtype list) {
+  return list->size == 0;
+}
+
+static inline
+void MAKE_NAME(SUNArrayListTtype, Resize)(SUNArrayListTtype list, sunindextype new_capacity) {
+  if (new_capacity <= list->capacity) return;
+  TTYPE* new_values = (TTYPE*) realloc(list->values, sizeof(TTYPE) * new_capacity);
+  if (!new_values) {
+    // Handle memory reallocation failure
+    return;
+  }
+  list->values = new_values;
+  list->capacity = new_capacity;
+}
+
+static inline
+void MAKE_NAME(SUNArrayListTtype, PushBack)(SUNArrayListTtype list, TTYPE* element) {
+  if (list->size == list->capacity) {
+    sunindextype new_capacity = (sunindextype)(ceil(list->capacity * GROWTH_FACTOR));
+    MAKE_NAME(SUNArrayListTtype, Resize)(list, new_capacity);
+  }
+  list->values[list->size++] = *element;
+}
+
+static inline
+TTYPE* MAKE_NAME(SUNArrayListTtype, Get)(SUNArrayListTtype list, int index) {
+  if (index < 0 || index >= list->size) {
+    // Handle index out of bounds
+    return NULL;
+  }
+  return &(list->values[index]);
+}
+
+static inline
+void MAKE_NAME(SUNArrayListTtype, Set)(SUNArrayListTtype list, int index, TTYPE* element) {
+  if (index < 0 || index >= list->size) {
+    // Handle index out of bounds
+    return;
+  }
+  list->values[index] = *element;
+}
+
+static inline
+void MAKE_NAME(SUNArrayListTtype, PopBack)(SUNArrayListTtype list) {
+  if (list->size == 0) return;
+  list->size--;
+}
+
+#undef TTYPE
---
+++ test/unit_tests/sundials/test_sundials_arraylist.cpp
@@ -0,0 +1,212 @@
+/* -----------------------------------------------------------------
+ * SUNDIALS Copyright Start
+ * Copyright (c) 2002-2024, Lawrence Livermore National Security
+ * and Southern Methodist University.
+ * All rights reserved.
+ *
+ * See the top-level LICENSE and NOTICE files for details.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SUNDIALS Copyright End
+ * -----------------------------------------------------------------*/
+
+#include <iostream>
+#include <string>
+#include <sundials/sundials_core.hpp>
+#include <nvector/nvector_serial.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#define TTYPE int
+#include "sundials_arraylist.h"
+
+class SUNArrayListPODTest : public testing::Test {
+ protected:
+  SUNArrayList_int list;
+
+  virtual void SetUp() override {
+    list = SUNArrayList_int_New(2);
+  }
+
+  virtual void TearDown() override {
+    SUNArrayList_int_Destroy(&list);
+  }
+};
+
+TEST_F(SUNArrayListPODTest, NewAndDestroy) {
+  EXPECT_NE(list, nullptr);
+  EXPECT_EQ(list->size, 0);
+  EXPECT_EQ(list->capacity, 2);
+  SUNArrayList_int_Destroy(&list);
+  EXPECT_EQ(list, nullptr);
+}
+
+TEST_F(SUNArrayListPODTest, IsEmpty) {
+  EXPECT_TRUE(SUNArrayList_int_IsEmpty(list));
+}
+
+TEST_F(SUNArrayListPODTest, PushBack) {
+  int value1 = 10;
+  int value2 = 20;
+
+  SUNArrayList_int_PushBack(list, &value1);
+  EXPECT_EQ(list->size, 1);
+  EXPECT_EQ(*SUNArrayList_int_Get(list, 0), value1);
+
+  SUNArrayList_int_PushBack(list, &value2);
+  EXPECT_EQ(list->size, 2);
+  EXPECT_EQ(*SUNArrayList_int_Get(list, 1), value2);
+
+  // Test resize
+  int value3 = 30;
+  SUNArrayList_int_PushBack(list, &value3);
+  EXPECT_EQ(list->size, 3);
+  EXPECT_EQ(list->capacity, 3);
+  EXPECT_EQ(*SUNArrayList_int_Get(list, 2), value3);
+}
+
+TEST_F(SUNArrayListPODTest, Get) {
+  int value = 10;
+  SUNArrayList_int_PushBack(list, &value);
+
+  EXPECT_EQ(*SUNArrayList_int_Get(list, 0), value);
+  // Test out of bounds
+  EXPECT_EQ(SUNArrayList_int_Get(list, -1), nullptr);
+  EXPECT_EQ(SUNArrayList_int_Get(list, 2), nullptr);
+}
+
+TEST_F(SUNArrayListPODTest, Set) {
+  int value1 = 10;
+  int value2 = 20;
+  SUNArrayList_int_PushBack(list, &value1);
+  SUNArrayList_int_PushBack(list, &value2);
+
+  EXPECT_EQ(*SUNArrayList_int_Get(list, 0), value1);
+  SUNArrayList_int_Set(list, 0, &value2);
+  EXPECT_EQ(*SUNArrayList_int_Get(list, 0), value2);
+  // Test out of bounds
+  SUNArrayList_int_Set(list, -1, &value1);  // No effect
+  SUNArrayList_int_Set(list, 2, &value1);  // No effect
+}
+
+TEST_F(SUNArrayListPODTest, PopBack) {
+  int value1 = 10;
+  int value2 = 20;
+  SUNArrayList_int_PushBack(list, &value1);
+  SUNArrayList_int_PushBack(list, &value2);
+
+  EXPECT_EQ(list->size, 2);
+  SUNArrayList_int_PopBack(list);
+  EXPECT_EQ(list->size, 1);
+  EXPECT_EQ(*SUNArrayList_int_Get(list, 0), value1);
+  // Pop from empty list
+  SUNArrayList_int_PopBack(list);
+  EXPECT_EQ(list->size, 0);
+}
+
+#define TTYPE N_Vector
+#include "sundials_arraylist.h"
+
+class SUNArrayListComplexTest : public testing::Test {
+ protected:
+  SUNArrayList_N_Vector list;
+  sundials::Context sunctx;
+
+  virtual void SetUp() override {
+    list = SUNArrayList_N_Vector_New(2);
+  }
+
+  virtual void TearDown() override {
+    SUNArrayList_N_Vector_Destroy(&list);
+  }
+};
+
+TEST_F(SUNArrayListComplexTest, NewAndDestroy) {
+  EXPECT_NE(list, nullptr);
+  EXPECT_EQ(list->size, 0);
+  EXPECT_EQ(list->capacity, 2);
+  SUNArrayList_N_Vector_Destroy(&list);
+  EXPECT_EQ(list, nullptr);
+}
+
+TEST_F(SUNArrayListComplexTest, IsEmpty) {
+  EXPECT_TRUE(SUNArrayList_N_Vector_IsEmpty(list));
+}
+
+TEST_F(SUNArrayListComplexTest, PushBack) {
+  N_Vector value1 = N_VNew_Serial(1, sunctx);
+  N_Vector value2 = N_VNew_Serial(2, sunctx);
+
+  SUNArrayList_N_Vector_PushBack(list, &value1);
+  EXPECT_EQ(list->size, 1);
+  EXPECT_EQ(*SUNArrayList_N_Vector_Get(list, 0), value1);
+  EXPECT_EQ(N_VGetLength(*SUNArrayList_N_Vector_Get(list, 0)), N_VGetLength(value1));
+
+  SUNArrayList_N_Vector_PushBack(list, &value2);
+  EXPECT_EQ(list->size, 2);
+  EXPECT_EQ(*SUNArrayList_N_Vector_Get(list, 1), value2);
+  EXPECT_EQ(N_VGetLength(*SUNArrayList_N_Vector_Get(list, 1)), N_VGetLength(value2));
+
+  // Test resize
+  N_Vector value3 = N_VNew_Serial(3, sunctx);
+  SUNArrayList_N_Vector_PushBack(list, &value3);
+  EXPECT_EQ(list->size, 3);
+  EXPECT_EQ(list->capacity, 3);
+  EXPECT_EQ(*SUNArrayList_N_Vector_Get(list, 2), value3);
+  EXPECT_EQ(N_VGetLength(*SUNArrayList_N_Vector_Get(list, 2)), N_VGetLength(value3));
+
+  N_VDestroy(value1);
+  N_VDestroy(value2);
+  N_VDestroy(value3);
+}
+
+TEST_F(SUNArrayListComplexTest, Get) {
+  N_Vector value = N_VNew_Serial(1, sunctx);
+
+  SUNArrayList_N_Vector_PushBack(list, &value);
+  EXPECT_EQ(*SUNArrayList_N_Vector_Get(list, 0), value);
+
+  // Test out of bounds
+  EXPECT_EQ(SUNArrayList_N_Vector_Get(list, -1), nullptr);
+  EXPECT_EQ(SUNArrayList_N_Vector_Get(list, 2), nullptr);
+
+  N_VDestroy(value);
+}
+
+TEST_F(SUNArrayListComplexTest, Set) {
+  N_Vector value1 = N_VNew_Serial(1, sunctx);
+  N_Vector value2 = N_VNew_Serial(2, sunctx);
+
+  SUNArrayList_N_Vector_PushBack(list, &value1);
+  SUNArrayList_N_Vector_PushBack(list, &value2);
+  EXPECT_EQ(*SUNArrayList_N_Vector_Get(list, 0), value1);
+  SUNArrayList_N_Vector_Set(list, 0, &value2);
+  EXPECT_EQ(*SUNArrayList_N_Vector_Get(list, 0), value2);
+
+  // Test out of bounds
+  SUNArrayList_N_Vector_Set(list, -1, &value1);
+  SUNArrayList_N_Vector_Set(list, 2, &value1);
+
+  N_VDestroy(value1);
+  N_VDestroy(value2);
+}
+
+TEST_F(SUNArrayListComplexTest, PopBack) {
+  N_Vector value1 = N_VNew_Serial(1, sunctx);
+  N_Vector value2 = N_VNew_Serial(2, sunctx);
+
+  SUNArrayList_N_Vector_PushBack(list, &value1);
+  SUNArrayList_N_Vector_PushBack(list, &value2);
+  EXPECT_EQ(list->size, 2);
+
+  SUNArrayList_N_Vector_PopBack(list);
+  EXPECT_EQ(list->size, 1);
+  EXPECT_EQ(*SUNArrayList_N_Vector_Get(list, 0), value1);
+
+  // Pop from empty list
+  SUNArrayList_N_Vector_PopBack(list);
+  EXPECT_EQ(list->size, 0);
+
+  N_VDestroy(value1);
+  N_VDestroy(value2);
+}
