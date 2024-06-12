a34da9ce13208fe80125a7e6d32384454b559f5e
maggul <maggul@mtu.edu>
2024-06-12 10:30:33 -0500

    A new STS example

---
+++ examples/arkode/C_serial/ark_analytic_sts.c
@@ -0,0 +1,311 @@
+/*-----------------------------------------------------------------
+ * Programmer(s): Daniel R. Reynolds @ SMU
+ *---------------------------------------------------------------
+ * SUNDIALS Copyright Start
+ * Copyright (c) 2002-2024, Lawrence Livermore National Security
+ * and Southern Methodist University.
+ * All rights reserved.
+ *
+ * See the top-level LICENSE and NOTICE files for details.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SUNDIALS Copyright End
+ *---------------------------------------------------------------
+ * Example problem:
+ *
+ * The following is a simple example problem with analytical
+ * solution,
+ *    dy/dt = lamda*y + 1/(1+t^2) - lamda*atan(t)
+ * for t in the interval [0.0, 10.0], with initial condition: y=0.
+ *
+ * The stiffness of the problem is directly proportional to the
+ * value of "lamda".  The value of lamda should be negative to
+ * result in a well-posed ODE; for values with magnitude larger
+ * than 100 the problem becomes quite stiff.
+ *
+ * This program solves the problem with the DIRK method,
+ * Newton iteration with the dense SUNLinearSolver, and a
+ * user-supplied Jacobian routine.
+ * Output is printed every 1.0 units of time (10 total).
+ * Run statistics (optional outputs) are printed at the end.
+ *-----------------------------------------------------------------*/
+
+/* Header files */
+#include <arkode/arkode_arkstep.h> /* prototypes for ARKStep fcts., consts */
+#include <math.h>
+#include <nvector/nvector_serial.h> /* serial N_Vector types, fcts., macros */
+#include <stdio.h>
+#include <sundials/sundials_types.h> /* definition of type sunrealtype          */
+#include <sunlinsol/sunlinsol_dense.h> /* access to dense SUNLinearSolver      */
+#include <sunmatrix/sunmatrix_dense.h> /* access to dense SUNMatrix            */
+
+#if defined(SUNDIALS_EXTENDED_PRECISION)
+#define GSYM "Lg"
+#define ESYM "Le"
+#define FSYM "Lf"
+#else
+#define GSYM "g"
+#define ESYM "e"
+#define FSYM "f"
+#endif
+
+/* User-supplied Functions Called by the Solver */
+static int f(sunrealtype t, N_Vector y, N_Vector ydot, void* user_data);
+static int Jac(sunrealtype t, N_Vector y, N_Vector fy, SUNMatrix J,
+               void* user_data, N_Vector tmp1, N_Vector tmp2, N_Vector tmp3);
+
+/* Private function to check function return values */
+static int check_flag(void* flagvalue, const char* funcname, int opt);
+
+/* Private function to check computed solution */
+static int check_ans(N_Vector y, sunrealtype t, sunrealtype rtol,
+                     sunrealtype atol);
+
+/* Main Program */
+int main(void)
+{
+  /* general problem parameters */
+  sunrealtype T0     = SUN_RCONST(0.0);    /* initial time */
+  sunrealtype Tf     = SUN_RCONST(10.0);   /* final time */
+  sunrealtype dTout  = SUN_RCONST(1.0);    /* time between outputs */
+  sunindextype NEQ   = 1;                  /* number of dependent vars. */
+  sunrealtype reltol = SUN_RCONST(1.0e-6); /* tolerances */
+  sunrealtype abstol = SUN_RCONST(1.0e-10);
+  sunrealtype lamda  = SUN_RCONST(-100.0); /* stiffness parameter */
+
+  /* general problem variables */
+  int flag;                  /* reusable error-checking flag */
+  N_Vector y         = NULL; /* empty vector for storing solution */
+  SUNMatrix A        = NULL; /* empty matrix for linear solver */
+  SUNLinearSolver LS = NULL; /* empty linear solver object */
+  void* arkode_mem   = NULL; /* empty ARKode memory structure */
+  FILE* UFID;
+  sunrealtype t, tout;
+  long int nst, nst_a, nfe, nfi, nsetups, nje, nfeLS, nni, ncfn, netf;
+
+  /* Create the SUNDIALS context object for this simulation */
+  SUNContext ctx;
+  flag = SUNContext_Create(SUN_COMM_NULL, &ctx);
+  if (check_flag(&flag, "SUNContext_Create", 1)) { return 1; }
+
+  /* Initial diagnostics output */
+  printf("\nAnalytical ODE test problem:\n");
+  printf("    lamda = %" GSYM "\n", lamda);
+  printf("   reltol = %.1" ESYM "\n", reltol);
+  printf("   abstol = %.1" ESYM "\n\n", abstol);
+
+  /* Initialize data structures */
+  y = N_VNew_Serial(NEQ, ctx); /* Create serial vector for solution */
+  if (check_flag((void*)y, "N_VNew_Serial", 0)) { return 1; }
+  N_VConst(SUN_RCONST(0.0), y); /* Specify initial condition */
+
+  /* Call ARKStepCreate to initialize the ARK timestepper module and
+     specify the right-hand side function in y'=f(t,y), the inital time
+     T0, and the initial dependent variable vector y.  Note: since this
+     problem is fully implicit, we set f_E to NULL and f_I to f. */
+  arkode_mem = ARKStepCreate(NULL, f, T0, y, ctx);
+  if (check_flag((void*)arkode_mem, "ARKStepCreate", 0)) { return 1; }
+
+  /* Set routines */
+  flag = ARKodeSetUserData(arkode_mem,
+                           (void*)&lamda); /* Pass lamda to user functions */
+  if (check_flag(&flag, "ARKodeSetUserData", 1)) { return 1; }
+  flag = ARKodeSStolerances(arkode_mem, reltol, abstol); /* Specify tolerances */
+  if (check_flag(&flag, "ARKodeSStolerances", 1)) { return 1; }
+
+  /* Initialize dense matrix data structure and solver */
+  A = SUNDenseMatrix(NEQ, NEQ, ctx);
+  if (check_flag((void*)A, "SUNDenseMatrix", 0)) { return 1; }
+  LS = SUNLinSol_Dense(y, A, ctx);
+  if (check_flag((void*)LS, "SUNLinSol_Dense", 0)) { return 1; }
+
+  /* Linear solver interface */
+  flag = ARKodeSetLinearSolver(arkode_mem, LS,
+                               A); /* Attach matrix and linear solver */
+  if (check_flag(&flag, "ARKodeSetLinearSolver", 1)) { return 1; }
+  flag = ARKodeSetJacFn(arkode_mem, Jac); /* Set Jacobian routine */
+  if (check_flag(&flag, "ARKodeSetJacFn", 1)) { return 1; }
+
+  /* Specify linearly implicit RHS, with non-time-dependent Jacobian */
+  flag = ARKodeSetLinear(arkode_mem, 0);
+  if (check_flag(&flag, "ARKodeSetLinear", 1)) { return 1; }
+
+  /* Open output stream for results, output comment line */
+  UFID = fopen("solution.txt", "w");
+  fprintf(UFID, "# t u\n");
+
+  /* output initial condition to disk */
+  fprintf(UFID, " %.16" ESYM " %.16" ESYM "\n", T0, NV_Ith_S(y, 0));
+
+  /* Main time-stepping loop: calls ARKodeEvolve to perform the integration, then
+     prints results.  Stops when the final time has been reached */
+  t    = T0;
+  tout = T0 + dTout;
+  printf("        t           u\n");
+  printf("   ---------------------\n");
+  while (Tf - t > 1.0e-15)
+  {
+    flag = ARKodeEvolve(arkode_mem, tout, y, &t, ARK_NORMAL); /* call integrator */
+    if (check_flag(&flag, "ARKodeEvolve", 1)) { break; }
+    printf("  %10.6" FSYM "  %10.6" FSYM "\n", t,
+           NV_Ith_S(y, 0)); /* access/print solution */
+    fprintf(UFID, " %.16" ESYM " %.16" ESYM "\n", t, NV_Ith_S(y, 0));
+    if (flag >= 0)
+    { /* successful solve: update time */
+      tout += dTout;
+      tout = (tout > Tf) ? Tf : tout;
+    }
+    else
+    { /* unsuccessful solve: break */
+      fprintf(stderr, "Solver failure, stopping integration\n");
+      break;
+    }
+  }
+  printf("   ---------------------\n");
+  fclose(UFID);
+
+  /* Get/print some final statistics on how the solve progressed */
+  flag = ARKodeGetNumSteps(arkode_mem, &nst);
+  check_flag(&flag, "ARKodeGetNumSteps", 1);
+  flag = ARKodeGetNumStepAttempts(arkode_mem, &nst_a);
+  check_flag(&flag, "ARKodeGetNumStepAttempts", 1);
+  flag = ARKStepGetNumRhsEvals(arkode_mem, &nfe, &nfi);
+  check_flag(&flag, "ARKStepGetNumRhsEvals", 1);
+  flag = ARKodeGetNumLinSolvSetups(arkode_mem, &nsetups);
+  check_flag(&flag, "ARKodeGetNumLinSolvSetups", 1);
+  flag = ARKodeGetNumErrTestFails(arkode_mem, &netf);
+  check_flag(&flag, "ARKodeGetNumErrTestFails", 1);
+  flag = ARKodeGetNumNonlinSolvIters(arkode_mem, &nni);
+  check_flag(&flag, "ARKodeGetNumNonlinSolvIters", 1);
+  flag = ARKodeGetNumNonlinSolvConvFails(arkode_mem, &ncfn);
+  check_flag(&flag, "ARKodeGetNumNonlinSolvConvFails", 1);
+  flag = ARKodeGetNumJacEvals(arkode_mem, &nje);
+  check_flag(&flag, "ARKodeGetNumJacEvals", 1);
+  flag = ARKodeGetNumLinRhsEvals(arkode_mem, &nfeLS);
+  check_flag(&flag, "ARKodeGetNumLinRhsEvals", 1);
+
+  printf("\nFinal Solver Statistics:\n");
+  printf("   Internal solver steps = %li (attempted = %li)\n", nst, nst_a);
+  printf("   Total RHS evals:  Fe = %li,  Fi = %li\n", nfe, nfi);
+  printf("   Total linear solver setups = %li\n", nsetups);
+  printf("   Total RHS evals for setting up the linear system = %li\n", nfeLS);
+  printf("   Total number of Jacobian evaluations = %li\n", nje);
+  printf("   Total number of Newton iterations = %li\n", nni);
+  printf("   Total number of linear solver convergence failures = %li\n", ncfn);
+  printf("   Total number of error test failures = %li\n\n", netf);
+
+  /* check the solution error */
+  flag = check_ans(y, t, reltol, abstol);
+
+  /* Clean up and return */
+  N_VDestroy(y);           /* Free y vector */
+  ARKodeFree(&arkode_mem); /* Free integrator memory */
+  SUNLinSolFree(LS);       /* Free linear solver */
+  SUNMatDestroy(A);        /* Free A matrix */
+  SUNContext_Free(&ctx);   /* Free context */
+
+  return flag;
+}
+
+/*-------------------------------
+ * Functions called by the solver
+ *-------------------------------*/
+
+/* f routine to compute the ODE RHS function f(t,y). */
+static int f(sunrealtype t, N_Vector y, N_Vector ydot, void* user_data)
+{
+  sunrealtype* rdata = (sunrealtype*)user_data; /* cast user_data to sunrealtype */
+  sunrealtype lamda = rdata[0];       /* set shortcut for stiffness parameter */
+  sunrealtype u     = NV_Ith_S(y, 0); /* access current solution value */
+
+  /* fill in the RHS function: "NV_Ith_S" accesses the 0th entry of ydot */
+  NV_Ith_S(ydot, 0) = lamda * u + SUN_RCONST(1.0) / (SUN_RCONST(1.0) + t * t) -
+                      lamda * atan(t);
+
+  return 0; /* return with success */
+}
+
+/* Jacobian routine to compute J(t,y) = df/dy. */
+static int Jac(sunrealtype t, N_Vector y, N_Vector fy, SUNMatrix J,
+               void* user_data, N_Vector tmp1, N_Vector tmp2, N_Vector tmp3)
+{
+  sunrealtype* rdata = (sunrealtype*)user_data; /* cast user_data to sunrealtype */
+  sunrealtype lamda  = rdata[0]; /* set shortcut for stiffness parameter */
+  sunrealtype* Jdata = SUNDenseMatrix_Data(J);
+
+  /* Fill in Jacobian of f: set the first entry of the data array to set the (0,0) entry */
+  Jdata[0] = lamda;
+
+  return 0; /* return with success */
+}
+
+/*-------------------------------
+ * Private helper functions
+ *-------------------------------*/
+
+/* Check function return value...
+    opt == 0 means SUNDIALS function allocates memory so check if
+             returned NULL pointer
+    opt == 1 means SUNDIALS function returns a flag so check if
+             flag >= 0
+    opt == 2 means function allocates memory so check if returned
+             NULL pointer
+*/
+static int check_flag(void* flagvalue, const char* funcname, int opt)
+{
+  int* errflag;
+
+  /* Check if SUNDIALS function returned NULL pointer - no memory allocated */
+  if (opt == 0 && flagvalue == NULL)
+  {
+    fprintf(stderr, "\nSUNDIALS_ERROR: %s() failed - returned NULL pointer\n\n",
+            funcname);
+    return 1;
+  }
+
+  /* Check if flag < 0 */
+  else if (opt == 1)
+  {
+    errflag = (int*)flagvalue;
+    if (*errflag < 0)
+    {
+      fprintf(stderr, "\nSUNDIALS_ERROR: %s() failed with flag = %d\n\n",
+              funcname, *errflag);
+      return 1;
+    }
+  }
+
+  /* Check if function returned NULL pointer - no memory allocated */
+  else if (opt == 2 && flagvalue == NULL)
+  {
+    fprintf(stderr, "\nMEMORY_ERROR: %s() failed - returned NULL pointer\n\n",
+            funcname);
+    return 1;
+  }
+
+  return 0;
+}
+
+/* check the computed solution */
+static int check_ans(N_Vector y, sunrealtype t, sunrealtype rtol, sunrealtype atol)
+{
+  int passfail = 0;          /* answer pass (0) or fail (1) flag     */
+  sunrealtype ans, err, ewt; /* answer data, error, and error weight */
+
+  /* compute solution error */
+  ans = atan(t);
+  ewt = SUN_RCONST(1.0) / (rtol * fabs(ans) + atol);
+  err = ewt * fabs(NV_Ith_S(y, 0) - ans);
+
+  /* is the solution within the tolerances? */
+  passfail = (err < SUN_RCONST(1.0)) ? 0 : 1;
+
+  if (passfail)
+  {
+    fprintf(stdout, "\nSUNDIALS_WARNING: check_ans error=%" GSYM "\n\n", err);
+  }
+
+  return (passfail);
+}
+
+/*---- end of file ----*/
