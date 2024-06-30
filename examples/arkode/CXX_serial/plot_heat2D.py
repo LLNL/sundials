e7fbfc0910bfc36831c5f9971f10313ef5cbe338
David J. Gardner <gardner48@llnl.gov>
2024-06-29 22:00:18 -0700

    apply formatting

--- examples/arkode/CXX_serial/plot_heat2D.py
+++ examples/arkode/CXX_serial/plot_heat2D.py
@@ -28,7 +28,7 @@ import matplotlib.pyplot as plt
 # ------------------------------------------------------------------------------
 
 # read problem info file
-infofile = 'heat2d_info.txt'
+infofile = "heat2d_info.txt"
 
 with open(infofile) as fn:
 
@@ -66,26 +66,26 @@ with open(infofile) as fn:
 # ------------------------------------------------------------------------------
 
 # check if the error was output
-fname = 'heat2d_error.txt'
+fname = "heat2d_error.txt"
 
 if os.path.isfile(fname):
-    plottype = ['solution', 'error']
+    plottype = ["solution", "error"]
 else:
-    plottype = ['solution']
+    plottype = ["solution"]
 
 for pt in plottype:
 
     # fill array with data
-    time   = np.zeros(nt)
+    time = np.zeros(nt)
     result = np.zeros((nt, ny, nx))
 
     # load data
-    data = np.loadtxt('heat2d_' + pt + '.txt', dtype=np.double)
+    data = np.loadtxt("heat2d_" + pt + ".txt", dtype=np.double)
 
     # extract data
     for i in range(nt):
-        time[i] = data[i,0]
-        result[i,0:ny+1,0:nx+1] = np.reshape(data[i,1:], (ny,nx))
+        time[i] = data[i, 0]
+        result[i, 0 : ny + 1, 0 : nx + 1] = np.reshape(data[i, 1:], (ny, nx))
 
     # determine extents of plots
     maxtemp = 1.1 * result.max()
@@ -94,7 +94,7 @@ for pt in plottype:
     # set x and y meshgrid objects
     xspan = np.linspace(0.0, xu, nx)
     yspan = np.linspace(0.0, yu, ny)
-    X,Y   = np.meshgrid(xspan, yspan)
+    X, Y = np.meshgrid(xspan, yspan)
 
     nxstr = repr(nx)
     nystr = repr(ny)
@@ -103,24 +103,33 @@ for pt in plottype:
     for tstep in range(nt):
 
         # set string constants for output plots, current time, mesh size
-        pname = 'heat2d_surf_' + pt + '.' + repr(tstep).zfill(3) + '.png'
-        tstr  = str(time[tstep])
+        pname = "heat2d_surf_" + pt + "." + repr(tstep).zfill(3) + ".png"
+        tstr = str(time[tstep])
 
         # plot surface and save to disk
         fig = plt.figure(1)
-        ax = fig.add_subplot(111, projection='3d')
+        ax = fig.add_subplot(111, projection="3d")
 
-        ax.plot_surface(X, Y, result[tstep,:,:], rstride=1, cstride=1,
-                        cmap=cm.jet, linewidth=0, antialiased=True, shade=True)
+        ax.plot_surface(
+            X,
+            Y,
+            result[tstep, :, :],
+            rstride=1,
+            cstride=1,
+            cmap=cm.jet,
+            linewidth=0,
+            antialiased=True,
+            shade=True,
+        )
 
-        ax.set_xlabel('x')
-        ax.set_ylabel('y')
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
         ax.set_zlim((mintemp, maxtemp))
-        ax.view_init(20,45)
-        if (pt == 'solution'):
-            title('u(x,y) at t = ' + tstr)
+        ax.view_init(20, 45)
+        if pt == "solution":
+            title("u(x,y) at t = " + tstr)
         else:
-            title('error(x,y) at t = ' + tstr)
+            title("error(x,y) at t = " + tstr)
         savefig(pname)
         plt.close()
 
--- test/compare_benchmarks.py
+++ test/compare_benchmarks.py
@@ -22,19 +22,55 @@ import thicket as tt
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compare Sundials performance results against previous results')
+    parser = argparse.ArgumentParser(
+        description="Compare Sundials performance results against previous results"
+    )
 
-    parser.add_argument('--release', dest='release', action='store_true', help='indicate if the current run to process is a release')
+    parser.add_argument(
+        "--release",
+        dest="release",
+        action="store_true",
+        help="indicate if the current run to process is a release",
+    )
 
-    parser.add_argument('--calidir', dest='caliDir', type=str, help='path to directory containing caliper files', default="/usr/workspace/sundials/califiles")
+    parser.add_argument(
+        "--calidir",
+        dest="caliDir",
+        type=str,
+        help="path to directory containing caliper files",
+        default="/usr/workspace/sundials/califiles",
+    )
 
-    parser.add_argument('--releasedir', dest='releaseDir', type=str, help='path to directory containing release caliper files', default="/usr/workspace/sundials/califiles/Release")
+    parser.add_argument(
+        "--releasedir",
+        dest="releaseDir",
+        type=str,
+        help="path to directory containing release caliper files",
+        default="/usr/workspace/sundials/califiles/Release",
+    )
 
-    parser.add_argument('--outpath', dest='outPath', type=str, help='path to directory to write results to', default="/dev/null")
+    parser.add_argument(
+        "--outpath",
+        dest="outPath",
+        type=str,
+        help="path to directory to write results to",
+        default="/dev/null",
+    )
 
-    parser.add_argument('--jobid', dest='jobID', type=int, help='job id of the current run to identify .cali files')
+    parser.add_argument(
+        "--jobid",
+        dest="jobID",
+        type=int,
+        help="job id of the current run to identify .cali files",
+    )
 
-    parser.add_argument('--threshold', dest="threshold", type=float, help='the percentage threshold in performance difference that indicates a regression', default=2.0)
+    parser.add_argument(
+        "--threshold",
+        dest="threshold",
+        type=float,
+        help="the percentage threshold in performance difference that indicates a regression",
+        default=2.0,
+    )
 
     args = parser.parse_args()
 
@@ -50,38 +86,42 @@ def main():
 
     if not os.path.exists(outPath):
         os.makedirs(outPath)
-    outFile = open("%s/benchmark_output.out" % outPath, 'w')
+    outFile = open("%s/benchmark_output.out" % outPath, "w")
 
     # thread per file
     with mp.Pool() as pool:
-        for res in pool.starmap(process_benchmark, [(jobID, release, releaseDir, i, threshold) for i in benchFiles]):
+        for res in pool.starmap(
+            process_benchmark,
+            [(jobID, release, releaseDir, i, threshold) for i in benchFiles],
+        ):
             if res:
                 outFile.write(res + "\n")
     outFile.close()
 
-    outFile = open("%s/benchmark_output.out" % outPath, 'r')
+    outFile = open("%s/benchmark_output.out" % outPath, "r")
     try:
         outLines = outFile.readlines()
     finally:
         outFile.close()
 
-    if (len(outLines) == 0):
+    if len(outLines) == 0:
         return -1
     return 0
+
 
 def process_benchmark(jobID, isRelease, releaseDir, benchmarkDir, threshold):
     # Get the current benchmark run
     benchmarkFiles = glob.glob("%s/*.cali" % benchmarkDir)
     # Don't compare if the run didn't include this benchmark
-    if (len(benchmarkFiles) == 0):
+    if len(benchmarkFiles) == 0:
         return
 
     th_files = tt.Thicket.from_caliperreader(benchmarkFiles)
-    curFilter = lambda x: x['job_id'] == jobID
+    curFilter = lambda x: x["job_id"] == jobID
     th_current = th_files.filter_metadata(curFilter)
 
     # Get the release caliper file
-    cluster = th_current.metadata['cluster'].values[0]
+    cluster = th_current.metadata["cluster"].values[0]
     if isRelease:
         # Get the last release
         versionDirs = glob.glob("%s/%s/*" % (releaseDir, cluster))
@@ -89,18 +129,23 @@ def process_benchmark(jobID, isRelease, releaseDir, benchmarkDir, threshold):
         versionDir = versionDirs[1]
     else:
         # Get the release the run is a part of
-        version = th_current.metadata['sundials_version'].values[0]
+        version = th_current.metadata["sundials_version"].values[0]
         versionDir = "%s/%s/%s" % (releaseDir, cluster, version)
-    benchmarkName = th_current.metadata['env.TEST_NAME'].values[0]
-    releaseFile = glob.glob("%s/Benchmarking/*/%s/*.cali" % (versionDir, benchmarkName), recursive=True)
+    benchmarkName = th_current.metadata["env.TEST_NAME"].values[0]
+    releaseFile = glob.glob(
+        "%s/Benchmarking/*/%s/*.cali" % (versionDir, benchmarkName), recursive=True
+    )
     th_compare = tt.Thicket.from_caliperreader(releaseFile)
-    metrics = ['Max time/rank']
+    metrics = ["Max time/rank"]
     tt.mean(th_current, columns=metrics)
     tt.mean(th_compare, columns=metrics)
 
-    ratio = th_current.statsframe.dataframe['Max time/rank_mean'] / th_compare.statsframe.dataframe['Max time/rank_mean']
+    ratio = (
+        th_current.statsframe.dataframe["Max time/rank_mean"]
+        / th_compare.statsframe.dataframe["Max time/rank_mean"]
+    )
 
-    tolerance = threshold/100
+    tolerance = threshold / 100
     if 1 - ratio[0] < tolerance:
         return benchmarkName
 
