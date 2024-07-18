7e982f3ebff2f4aac7df44bd4dc9d6aad647f9c7
David J. Gardner <gardner48@llnl.gov>
2024-07-18 14:29:52 +0800

    workaround bug in older CMake versions

--- cmake/tpl/SundialsLapack.cmake
+++ cmake/tpl/SundialsLapack.cmake
@@ -322,11 +322,23 @@ if(NOT LAPACK_WORKS)
 
   # Attempt to build and link the test executable, pass --debug-trycompile to
   # the cmake command to save build files for debugging
-  try_compile(
-    COMPILE_OK ${LAPACK_TEST_DIR}
-    ${LAPACK_TEST_DIR}/test.c
-    LINK_LIBRARIES LAPACK::LAPACK
-    OUTPUT_VARIABLE COMPILE_OUTPUT)
+  if (CMAKE_VERSION VERSION_LESS 3.20)
+    # Workaround bug in older versions of CMake where the BLAS::BLAS target,
+    # which LAPACK::LAPACK depends on, is not defined in the file
+    # ${LAPACK_TEST_DIR}/CMakeFiles/CMakeTmp/<random_name>Targets.cmake
+    # created by try_compile
+    try_compile(
+      COMPILE_OK ${LAPACK_TEST_DIR}
+      ${LAPACK_TEST_DIR}/test.c
+      LINK_LIBRARIES LAPACK::LAPACK BLAS::BLAS
+      OUTPUT_VARIABLE COMPILE_OUTPUT)
+  else()
+    try_compile(
+      COMPILE_OK ${LAPACK_TEST_DIR}
+      ${LAPACK_TEST_DIR}/test.c
+      LINK_LIBRARIES LAPACK::LAPACK
+      OUTPUT_VARIABLE COMPILE_OUTPUT)
+  endif()
 
   # Check the result
   if(COMPILE_OK)
