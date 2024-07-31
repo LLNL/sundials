c932aab50a930bf812f2af28a32f119dee05ff47
Balos, Cody, J <balos1@llnl.gov>
2024-07-31 14:31:08 -0700

    correct prefix

--- .gitlab/build_and_test.sh
+++ .gitlab/build_and_test.sh
@@ -110,7 +110,7 @@ then
     python3 .gitlab/uberenv/uberenv.py --spec="${spec}" "${mirror_opt}" "${prefix_opt}"
     if [[ -d $buildcache ]]
     then
-        ${prefix_opt}/spack/bin/spack buildcache push ${buildcache} ${spec}
+        ${prefix}/spack/bin/spack buildcache push ${buildcache} ${spec}
     fi
 
     # Ensure correct CUDA module is loaded, only works for module naming
