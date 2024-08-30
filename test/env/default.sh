e7487647f3295a28ce0a6eac9a88592ae8c451ba
David J. Gardner <gardner48@llnl.gov>
2024-08-29 18:41:49 -0700

    set cuda arch

--- test/env/default.sh
+++ test/env/default.sh
@@ -107,6 +107,9 @@ if [[ ":${LD_LIBRARY_PATH}:" != *":/usr/local/cuda/lib64:"* ]]; then
     export LD_LIBRARY_PATH="/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
 fi
 
+# set CUDA architecture
+export CUDAARCHS=60
+
 # ------------------------------------------------------------------------------
 # Compilers and flags
 # ------------------------------------------------------------------------------
