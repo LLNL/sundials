4a7bd60c12a0c77b40de23e5310a205da0731117
Steven Roberts <roberts115@llnl.gov>
2024-09-03 16:28:26 -0700

    Comments in analytic splitting example

--- examples/arkode/C_serial/ark_analytic_splitting.c
+++ examples/arkode/C_serial/ark_analytic_splitting.c
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------
- * Programmer(s): Daniel R. Reynolds @ SMU
+ * Programmer(s): Steven B. Roberts @ LLNL
  *---------------------------------------------------------------
  * SUNDIALS Copyright Start
  * Copyright (c) 2002-2024, Lawrence Livermore National Security
@@ -50,7 +50,7 @@ typedef struct
   sunrealtype lambda;
 } UserData;
 
-
+/* RHS for f^1(t, y) = -lambda * y */
 static int f_linear(const sunrealtype t, const N_Vector y, N_Vector ydot, void* const user_data)
 {
   sunrealtype lambda = ((UserData*) user_data)->lambda;
@@ -58,12 +58,14 @@ static int f_linear(const sunrealtype t, const N_Vector y, N_Vector ydot, void* 
   return 0;
 }
 
+/* RHS for f^2(t, y) = y^2 */
 static int f_nonlinear(const sunrealtype t, const N_Vector y, N_Vector ydot, void* const user_data)
 {
   N_VProd(y, y, ydot);
   return 0;
 }
 
+/* Compute the exact analytic solution */
 static N_Vector exact_sol(const N_Vector y0, const sunrealtype tf, const UserData* const user_data)
 {
   N_Vector sol = N_VClone(y0);
@@ -72,6 +74,7 @@ static N_Vector exact_sol(const N_Vector y0, const sunrealtype tf, const UserDat
   NV_Ith_S(sol, 0) = lambda * y0_val / (y0_val - (y0_val - lambda) * SUNRexp(lambda * tf));
   return sol;
 }
+
 /* Check function return value...
     opt == 0 means SUNDIALS function allocates memory so check if
              returned NULL pointer
@@ -118,11 +121,11 @@ static int check_flag(void* flagvalue, const char* funcname, int opt)
 int main()
 {
   /* Problem parameters */
-  const sunrealtype t0 = SUN_RCONST(0.0);
-  const sunrealtype tf = SUN_RCONST(1.0);
-  const sunrealtype dt = SUN_RCONST(0.01);
-  const sunrealtype dt_linear = dt / 5;
-  const sunrealtype dt_nonlinear = dt / 10;
+  const sunrealtype t0 = SUN_RCONST(0.0);   /* initial time */
+  const sunrealtype tf = SUN_RCONST(1.0);   /* final time */
+  const sunrealtype dt = SUN_RCONST(0.01);  /* operator splitting time step */
+  const sunrealtype dt_linear = dt / 5;     /* linear integrator time step */
+  const sunrealtype dt_nonlinear = dt / 10; /* nonlinear integrator time step */
 
   UserData user_data = {
     .lambda = SUN_RCONST(2.0)
@@ -143,6 +146,7 @@ int main()
   printf("\nAnalytical ODE test problem:\n");
   printf("   lambda = %" GSYM "\n", user_data.lambda);
 
+  /* Create the integrator for the linear partition */
   void *linear_mem = ARKStepCreate(f_linear, NULL, t0, y, ctx);
   if (check_flag(linear_mem, "N_VNew_Serial", 0)) { return 1; }
 
@@ -152,32 +156,46 @@ int main()
   flag = ARKodeSetFixedStep(linear_mem, dt_linear);
   if (check_flag(&flag, "ARKodeSetFixedStep", 1)) { return 1; }
 
+  /* Create the integrator for the nonlinear partition */
   void *nonlinear_mem = ARKStepCreate(f_nonlinear, NULL, t0, y, ctx);
   if (check_flag(nonlinear_mem, "N_VNew_Serial", 0)) { return 1; }
 
   flag = ARKodeSetFixedStep(nonlinear_mem, dt_nonlinear);
   if (check_flag(&flag, "ARKodeSetFixedStep", 1)) { return 1; }
 
+  /* Create SUNSteppers out of the integrators */
   SUNStepper steppers[2];
   ARKStepCreateSUNStepper(linear_mem, &steppers[0]);
   ARKStepCreateSUNStepper(nonlinear_mem, &steppers[1]);
 
+  /* Create the operator splitting method */
   void *splitting_mem = SplittingStepCreate(steppers, 2, t0, y, ctx);
   if (check_flag(splitting_mem, "SplittingStepCreate", 0)) { return 1; }
 
   flag = ARKodeSetFixedStep(splitting_mem, dt);
   if (check_flag(&flag, "ARKodeSetFixedStep", 1)) { return 1; }
 
+  /* Compute the operator splitting solution */
   sunrealtype tret;
   flag = ARKodeEvolve(splitting_mem, tf, y, &tret, ARK_NORMAL);
   if (check_flag(&flag, "ARKodeEvolve", 1)) { return 1; }
 
+  /* Print the numerical error */
   N_Vector y_err = N_VClone(y);
   if (check_flag(y_err, "N_VClone", 0)) { return 1; }
-
   N_VLinearSum(SUN_RCONST(1.0), y, -SUN_RCONST(1.0), y_exact, y_err);
+  printf("Error: %" GSYM "\n", N_VMaxNorm(y_err));
 
-  printf("Error: %" GSYM "\n", NV_Ith_S(y_err, 0));
+  /* Free memory */
+  N_VDestroy(y);
+  N_VDestroy(y_exact);
+  N_VDestroy(y_err);
+  ARKodeFree(&linear_mem);
+  SUNStepper_Destroy(&steppers[0]);
+  ARKodeFree(&nonlinear_mem);
+  SUNStepper_Destroy(&steppers[1]);
+  ARKodeFree(&splitting_mem);
+  SUNContext_Free(&ctx);
   
   return 0;
 }
