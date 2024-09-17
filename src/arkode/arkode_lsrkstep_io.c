9a3c241ca28b9135f86aef72b4709bff986bf61f
Daniel R. Reynolds <reynolds@smu.edu>
2024-09-17 09:59:50 -0500

    formatting

--- src/arkode/arkode_lsrkstep_io.c
+++ src/arkode/arkode_lsrkstep_io.c
@@ -456,8 +456,10 @@ int lsrkStep_SetDefaults(ARKodeMem ark_mem)
   step_mem->domeignfe      = 0;
   step_mem->stagemax       = 0;
   step_mem->ndomeigupdates = 0;
-  step_mem->stagemaxlimit = round(SUNRsqrt(ark_mem->reltol / (10.0 * ark_mem->uround)));
-  step_mem->stagemaxlimit = (step_mem->stagemaxlimit > 2) ? step_mem->stagemaxlimit : 2;
+  step_mem->stagemaxlimit =
+    round(SUNRsqrt(ark_mem->reltol / (10.0 * ark_mem->uround)));
+  step_mem->stagemaxlimit =
+    (step_mem->stagemaxlimit > 2) ? step_mem->stagemaxlimit : 2;
   step_mem->nstsig = 0;
 
   /* Spectral info */
