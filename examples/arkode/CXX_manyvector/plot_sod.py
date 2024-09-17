5544e8d4bcb758134147f96b711d5cc44b2f6c8e
Daniel R. Reynolds <reynolds@smu.edu>
2024-09-17 12:49:45 -0500

    formatting

--- examples/arkode/CXX_manyvector/plot_sod.py
+++ examples/arkode/CXX_manyvector/plot_sod.py
@@ -72,14 +72,14 @@ def fsecant(p4, p1, p5, rho1, rho5, gamma):
     """
     Utility routine for exact_Riemann function below
     """
-    z = p4/p5 - 1.0
-    c1 = np.sqrt(gamma*p1/rho1)
-    c5 = np.sqrt(gamma*p5/rho5)
+    z = p4 / p5 - 1.0
+    c1 = np.sqrt(gamma * p1 / rho1)
+    c5 = np.sqrt(gamma * p5 / rho5)
     gm1 = gamma - 1.0
     gp1 = gamma + 1.0
-    g2  = 2.0*gamma
+    g2 = 2.0 * gamma
     fact = gm1 / g2 * (c5 / c1) * z / np.sqrt(1.0 + gp1 / g2 * z)
-    fact = (1.0 - fact)**(g2 / gm1)
+    fact = (1.0 - fact) ** (g2 / gm1)
     return p1 * fact - p4
 
 
@@ -96,33 +96,33 @@ def exact_Riemann(t, x, xI):
 
     # begin solution
     rho1 = 1.0
-    p1   = 1.0
-    u1   = 0.0
+    p1 = 1.0
+    u1 = 0.0
     rho5 = 0.125
-    p5   = 0.1
-    u5   = 0.0
+    p5 = 0.1
+    u5 = 0.0
 
     # solve for post-shock pressure by secant method initial guesses
     p40 = p1
     p41 = p5
     f0 = fsecant(p40, p1, p5, rho1, rho5, gamma)
     itmax = 50
-    eps = 1.e-14
+    eps = 1.0e-14
     for iter in range(itmax):
         f1 = fsecant(p41, p1, p5, rho1, rho5, gamma)
-        if (f1 == f0):
+        if f1 == f0:
             break
         p4 = p41 - (p41 - p40) * f1 / (f1 - f0)
-        if ((np.abs(p4 - p41) / np.abs(p41)) < eps):
+        if (np.abs(p4 - p41) / np.abs(p41)) < eps:
             break
         p40 = p41
         p41 = p4
-        f0  = f1
-        if (iter == itmax-1):
+        f0 = f1
+        if iter == itmax-1:
             raise ValueError("exact_Riemann iteration failed to converge")
 
     # compute post-shock density and velocity
-    z  = (p4 / p5 - 1.0)
+    z  = p4 / p5 - 1.0
     c5 = np.sqrt(gamma * p5 / rho5)
 
     gm1 = gamma - 1.0
@@ -139,7 +139,7 @@ def exact_Riemann(t, x, xI):
     # compute values at foot of rarefaction
     p3 = p4
     u3 = u4
-    rho3 = rho1 * (p3 / p1)**(1.0/gamma)
+    rho3 = rho1 * (p3 / p1) ** (1.0 / gamma)
 
     # compute positions of waves
     c1 = np.sqrt(gamma * p1 / rho1)
@@ -151,27 +151,27 @@ def exact_Riemann(t, x, xI):
     xhd = xI - c1 * t
 
     # compute solution as a function of position
-    if (x < xhd):
+    if x < xhd:
         rho = rho1
-        p   = p1
-        u   = u1
-    elif (x < xft):
-        u    = 2.0 / gp1 * (c1 + (x - xI) / t)
+        p = p1
+        u = u1
+    elif x < xft:
+        u = 2.0 / gp1 * (c1 + (x - xI) / t)
         fact = 1.0 - 0.5 * gm1 * u / c1
-        rho  = rho1 * fact**(2.0/gm1)
-        p    = p1 * fact**(2.0 * gamma / gm1)
-    elif (x < xcd):
+        rho = rho1 * fact ** (2.0/gm1)
+        p = p1 * fact ** (2.0 * gamma / gm1)
+    elif x < xcd:
         rho = rho3
-        p   = p3
-        u   = u3
-    elif (x < xsh):
+        p = p3
+        u = u3
+    elif x < xsh:
         rho = rho4
-        p   = p4
-        u   = u4
+        p = p4
+        u = u4
     else:
         rho = rho5
-        p   = p5
-        u   = u5
+        p = p5
+        u = u5
 
     # return with success
     return rho, u, p
@@ -182,7 +182,7 @@ utrue = np.zeros((nt, nx), dtype=float)
 ptrue = np.zeros((nt, nx), dtype=float)
 for it in range(nt):
     for ix in range(nx):
-        rhotrue[it,ix], utrue[it,ix], ptrue[it,ix] = exact_Riemann(t[it], x[ix], 0.5)
+        rhotrue[it, ix], utrue[it, ix], ptrue[it, ix] = exact_Riemann(t[it], x[ix], 0.5)
 
 #   plot defaults: increase default font size, increase plot width, enable LaTeX rendering
 plt.rc("font", size=15)
@@ -203,24 +203,24 @@ ax02 = fig.add_subplot(gs[0, 2])  # right column
 ax12 = fig.add_subplot(gs[1, 2])
 ax22 = fig.add_subplot(gs[2, 2])
 it = 0
-ax00.plot(x, rho[it, :], '-b', x, rhotrue[it, :], ':k')
-ax10.plot(x, u[it, :], '-b', x, utrue[it, :], ':k')
-ax20.plot(x, p[it, :], '-b', x, ptrue[it, :], ':k')
+ax00.plot(x, rho[it, :], "-b", x, rhotrue[it, :], ":k")
+ax10.plot(x, u[it, :], "-b", x, utrue[it, :], ":k")
+ax20.plot(x, p[it, :], "-b", x, ptrue[it, :], ":k")
 ax00.set_title(r"$t =$ " + repr(t[it]).zfill(3))
 ax00.set_ylabel(r"$\rho$")
 ax10.set_ylabel(r"$v_x$")
 ax20.set_ylabel(r"$p$")
 ax20.set_xlabel(r"$x$")
 it = nt // 2
-ax01.plot(x, rho[it, :], '-b', x, rhotrue[it, :], ':k')
-ax11.plot(x, u[it, :], '-b', x, utrue[it, :], ':k')
-ax21.plot(x, p[it, :], '-b', x, ptrue[it, :], ':k')
+ax01.plot(x, rho[it, :], "-b", x, rhotrue[it, :], ":k")
+ax11.plot(x, u[it, :], "-b", x, utrue[it, :], ":k")
+ax21.plot(x, p[it, :], "-b", x, ptrue[it, :], ":k")
 ax01.set_title(r"$t =$ " + repr(t[it]).zfill(3))
 ax21.set_xlabel(r"$x$")
 it = nt - 1
-ax02.plot(x, rho[it, :], '-b', x, rhotrue[it, :], ':k')
-ax12.plot(x, u[it, :], '-b', x, utrue[it, :], ':k')
-ax22.plot(x, p[it, :], '-b', x, ptrue[it, :], ':k')
+ax02.plot(x, rho[it, :], "-b", x, rhotrue[it, :], ":k")
+ax12.plot(x, u[it, :], "-b", x, utrue[it, :], ":k")
+ax22.plot(x, p[it, :], "-b", x, ptrue[it, :], ":k")
 ax02.set_title(r"$t =$ " + repr(t[it]).zfill(3))
 ax22.set_xlabel(r"$x$")
 plt.savefig("sod_frames.png")
