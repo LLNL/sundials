fdb3d4a18e61b4fb3c139832b7ef01b740bf81ca
Steven Roberts <roberts115@llnl.gov>
2024-09-26 13:37:41 -0700

    Fix assertion to allow order 0

--- src/sundials/sundials_adaptcontroller.c
+++ src/sundials/sundials_adaptcontroller.c
@@ -133,7 +133,7 @@ SUNErrCode SUNAdaptController_EstimateStep(SUNAdaptController C, sunrealtype h,
   if (C == NULL) { return SUN_ERR_ARG_CORRUPT; }
   SUNFunctionBegin(C->sunctx);
   SUNAssert(isfinite(h), SUN_ERR_ARG_OUTOFRANGE);
-  SUNAssert(p > 0, SUN_ERR_ARG_OUTOFRANGE);
+  SUNAssert(p >= 0, SUN_ERR_ARG_OUTOFRANGE);
   SUNAssert(dsm >= SUN_RCONST(0.0), SUN_ERR_ARG_OUTOFRANGE);
   SUNAssert(hnew, SUN_ERR_ARG_CORRUPT);
   *hnew = h; /* initialize output with identity */
