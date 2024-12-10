afef24380dfd8a0d635212da58f001fec0575d3a
David J. Gardner <gardner48@llnl.gov>
2024-12-09 21:54:37 -0800

    apply formatting

--- test/unit_tests/logging/test_logging_arkode_splittingstep.cpp
+++ test/unit_tests/logging/test_logging_arkode_splittingstep.cpp
@@ -21,8 +21,8 @@
 #include <limits>
 
 // Include desired integrators and vectors
-#include "arkode/arkode_splittingstep.h"
 #include "arkode/arkode_erkstep.h"
+#include "arkode/arkode_splittingstep.h"
 #include "nvector/nvector_serial.h"
 #include "sundials/sundials_context.hpp"
 #include "sundials/sundials_logger.h"
