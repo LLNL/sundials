d8f1930e7a69f633e4089265f2f391def3345274
David J. Gardner <gardner48@llnl.gov>
2024-12-15 10:05:56 -0800

    use arg for macro inputs

--- cmake/macros/SundialsAddBenchmark.cmake
+++ cmake/macros/SundialsAddBenchmark.cmake
@@ -29,16 +29,15 @@ macro(sundials_add_benchmark NAME EXECUTABLE BASE_BENCHMARK_NAME)
   # ENABLE_GPU        = indicate this benchmark should be run with GPUs
   set(options ENABLE_GPU)
 
-  cmake_parse_arguments(sundials_add_benchmark "${options}" "${oneValueArgs}"
-                        "${multiValueArgs}" ${ARGN})
+  cmake_parse_arguments(arg "${options}" "${oneValueArgs}" "${multiValueArgs}"
+                        ${ARGN})
 
   # set the target name
-  if(sundials_add_benchmark_IDENTIFIER)
-    set(TARGET_NAME ${NAME}_${sundials_add_benchmark_IDENTIFIER})
+  if(arg_IDENTIFIER)
+    set(TARGET_NAME ${NAME}_${arg_IDENTIFIER})
   else()
-    if(sundials_add_benchmark_BENCHMARK_ARGS)
-      string(REPLACE " " "_" TEST_SUFFIX
-                     "${sundials_add_benchmark_BENCHMARK_ARGS}")
+    if(arg_BENCHMARK_ARGS)
+      string(REPLACE " " "_" TEST_SUFFIX "${arg_BENCHMARK_ARGS}")
       set(TARGET_NAME ${NAME}_${TEST_SUFFIX})
     else()
       set(TARGET_NAME ${NAME}_run)
@@ -46,45 +45,41 @@ macro(sundials_add_benchmark NAME EXECUTABLE BASE_BENCHMARK_NAME)
   endif()
 
   # make the the output directory if it doesn't exist
-  set(_output_dir "${SUNDIALS_BENCHMARK_OUTPUT_DIR}/${BASE_BENCHMARK_NAME}/${TARGET_NAME}")
+  set(_output_dir
+      "${SUNDIALS_BENCHMARK_OUTPUT_DIR}/${BASE_BENCHMARK_NAME}/${TARGET_NAME}")
   if(NOT EXISTS ${_output_dir})
     file(MAKE_DIRECTORY ${_output_dir})
-  endif()
-
-  # make the caliper output directory if it doesn't exist
-  if(ENABLE_CALIPER)
-    set(_caliper_dir "${SUNDIALS_BENCHMARK_CALIPER_OUTPUT_DIR}/${BASE_BENCHMARK_NAME}/${TARGET_NAME}")
-    if(NOT EXISTS ${_caliper_dir})
-      file(MAKE_DIRECTORY ${_caliper_dir})
-    endif()
   endif()
 
   # command line arguments for the test runner script
   set(TEST_RUNNER_ARGS
       "--verbose" "--executablename=$<TARGET_FILE:${EXECUTABLE}>"
-      "--outputdir=${_output_dir}"
-      "--nodiff")
+      "--outputdir=${_output_dir}" "--nodiff")
 
   if(ENABLE_CALIPER)
+    set(_caliper_dir
+        "${SUNDIALS_BENCHMARK_CALIPER_OUTPUT_DIR}/${BASE_BENCHMARK_NAME}/${TARGET_NAME}"
+    )
+    if(NOT EXISTS ${_caliper_dir})
+      file(MAKE_DIRECTORY ${_caliper_dir})
+    endif()
     list(APPEND TEST_RUNNER_ARGS "--profile")
     list(APPEND TEST_RUNNER_ARGS "--calidir=${_caliper_dir}")
   endif()
 
   # incorporate scheduler arguments into test_runner
   if(SUNDIALS_SCHEDULER_COMMAND STREQUAL "flux run")
-    set(SCHEDULER_STRING " -n${sundials_add_benchmark_NUM_CORES}")
+    set(SCHEDULER_STRING " -n${arg_NUM_CORES}")
   elseif(SUNDIALS_SCHEDULER_COMMAND STREQUAL "jsrun")
-    if(${sundials_add_benchmark_ENABLE_GPU})
+    if(${arg_ENABLE_GPU})
       set(SCHEDULER_STRING
-        " --smpiargs=\\\"-gpu\\\" -n${sundials_add_benchmark_NUM_CORES} -a1 -c1 -g1"
-      )
+          " --smpiargs=\\\"-gpu\\\" -n${arg_NUM_CORES} -a1 -c1 -g1")
     else()
-      set(SCHEDULER_STRING " -n${sundials_add_benchmark_NUM_CORES} -a1 -c1")
+      set(SCHEDULER_STRING " -n${arg_NUM_CORES} -a1 -c1")
     endif()
   elseif(SUNDIALS_SCHEDULER_COMMAND STREQUAL "srun")
     set(SCHEDULER_STRING
-        " -n${sundials_add_benchmark_NUM_CORES} --cpus-per-task=1 --ntasks-per-node=1"
-    )
+        " -n${arg_NUM_CORES} --cpus-per-task=1 --ntasks-per-node=1")
   endif()
   string(REPLACE " " ";" SCHEDULER_ARGS "${SCHEDULER_STRING}")
   string(REPLACE " " ";" SCHEDULER_COMMAND_ARGS "${SUNDIALS_SCHEDULER_COMMAND}")
@@ -93,8 +88,7 @@ macro(sundials_add_benchmark NAME EXECUTABLE BASE_BENCHMARK_NAME)
   set(RUN_COMMAND ${SCHEDULER_COMMAND_ARGS} ${SCHEDULER_ARGS})
   list(APPEND TEST_RUNNER_ARGS "--runcommand=\"${RUN_COMMAND}\"")
 
-  list(APPEND TEST_RUNNER_ARGS
-       "--runargs=${sundials_add_benchmark_BENCHMARK_ARGS}"
+  list(APPEND TEST_RUNNER_ARGS "--runargs=${arg_BENCHMARK_ARGS}"
        "--testname=${TARGET_NAME}")
   add_custom_target(
     ${TARGET_NAME}
