cca3c61735334a9aabfb06930646b5af3a0399d9
Balos, Cody, J <balos1@llnl.gov>
2024-12-27 12:56:30 -0800

    expand example to show classes that hold user data

--- src/bindings/python/test_interface.py
+++ src/bindings/python/test_interface.py
@@ -4,30 +4,49 @@ import numpy as np
 from pysundials.core import *
 from pysundials.arkode import *
 
+class MyODEProblem:
+  lamb = 1.0
+
+  def rhs(t, yvec, ydotvec, user_data):
+    # NOTE: user_data should not be messed with since we use it for the callback wrapper
+    y = N_VGetArrayPointer(yvec)
+    ydot = N_VGetArrayPointer(ydotvec)
+    ydot[0] = MyODEProblem.lamb * y[0] + 1.0 / (1.0 + t * t) - 1.0 * np.atan(t)
+    return 0
+
+class MyODEProblem2:
+  def __init__(self, lamb = 1.0):
+    self.lamb = lamb
+
+  def rhs(self, t, yvec, ydotvec):
+    y = N_VGetArrayPointer(yvec)
+    ydot = N_VGetArrayPointer(ydotvec)
+    ydot[0] = self.lamb * y[0] + 1.0 / (1.0 + t * t) - 1.0 * np.atan(t)
+    return 0
+
 sunctx = SUNContextView()
 nv = NVectorView(N_VNew_Serial(1, sunctx.get()))
 
 # Get the array and change a value in it
 arr = N_VGetArrayPointer(nv.get()) # Option 1: have to call get when passing the NVectorView
 # arr = nv.GetArrayPointer() # Option 2: wrap N_V calls as NVectorView class methods
-arr[0] = 0.0
+arr[0] = 0.0 # set initial condition
 
-def rhs(t, yvec, ydotvec, user_data):
-  y = N_VGetArrayPointer(yvec)
-  ydot = N_VGetArrayPointer(ydotvec)
-  ydot[0] = 1.0 * y[0] + 1.0 / (1.0 + t * t) - 1.0 * np.atan(t)
-  return 0
-
-erk = ARKodeView(ERKStepCreate(rhs, 0, nv.get(), sunctx.get()))
+erk = ARKodeView(ERKStepCreate(MyODEProblem.rhs, 0, nv.get(), sunctx.get()))
 ARKodeSStolerances(erk.get(), 1e-6, 1e-6)
 
 tout, tret = 10.0, 0.0
 status = ARKodeEvolve(erk.get(), tout, nv.get(), tret, ARK_NORMAL)
 print(arr)
 
-# Try again
-erk = ARKodeView(ERKStepCreate(rhs, 0, nv.get(), sunctx.get()))
+# Try again with lambda
+
+ode_problem = MyODEProblem2()
+arr[0] = 0.0 # reset initial condition
+
+erk = ARKodeView(ERKStepCreate(lambda t, y, ydot, user_data: ode_problem.rhs(t, y, ydot), 0, nv.get(), sunctx.get()))
 ARKodeSStolerances(erk.get(), 1e-6, 1e-6)
 
 tret = 0.0
 status = ARKodeEvolve(erk.get(), tout, nv.get(), tret, ARK_NORMAL)
+print(arr)
