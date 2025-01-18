72b88cc51bc2847133c466c9f6215c35fa671602
Steven Roberts <roberts115@llnl.gov>
2025-01-18 08:19:28 -0800

    Maintenance: Remove unused macros (#644)
    Add -Wunused-macros and -Wunused-local-typedefs with ENABLE_ALL_WARNINGS
    
    --------
    
    Co-authored-by: David Gardner <gardner48@llnl.gov>

--- src/nvector/trilinos/nvector_trilinos.cpp
+++ src/nvector/trilinos/nvector_trilinos.cpp
@@ -27,10 +27,8 @@
 
 #include "sundials_macros.h"
 
-#define ZERO   SUN_RCONST(0.0)
-#define HALF   SUN_RCONST(0.5)
-#define ONE    SUN_RCONST(1.0)
-#define ONEPT5 SUN_RCONST(1.5)
+#define ZERO SUN_RCONST(0.0)
+#define ONE  SUN_RCONST(1.0)
 
 /*
  * -----------------------------------------------------------------
--- test/unit_tests/sunnonlinsol/petsc/test_sunnonlinsol_petscsnes.c
+++ test/unit_tests/sunnonlinsol/petsc/test_sunnonlinsol_petscsnes.c
@@ -23,12 +23,8 @@
 
 #if defined(SUNDIALS_EXTENDED_PRECISION)
 #define GSYM "Lg"
-#define ESYM "Le"
-#define FSYM "Lf"
 #else
 #define GSYM "g"
-#define ESYM "e"
-#define FSYM "f"
 #endif
 
 #define NEQ   3                  /* number of equations        */
