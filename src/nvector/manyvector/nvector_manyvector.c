d8404d3dd4025699469687a64163b7ad007d8d9d
David J. Gardner <gardner48@llnl.gov>
2025-01-17 20:39:21 -0800

    apply formatting

--- src/nvector/manyvector/nvector_manyvector.c
+++ src/nvector/manyvector/nvector_manyvector.c
@@ -42,7 +42,7 @@
 
 #define ZERO SUN_RCONST(0.0)
 #ifdef MANYVECTOR_BUILD_WITH_MPI
-#define ONE  SUN_RCONST(1.0)
+#define ONE SUN_RCONST(1.0)
 #endif
 
 /* -----------------------------------------------------------------
