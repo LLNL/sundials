9ae65a70e37f9549f0b72983cc6884c89c5ef917
Steven Roberts <roberts115@llnl.gov>
2025-01-17 17:22:59 -0800

    Apply formatter

--- examples/arkode/CXX_parhyp/ark_heat2D_hypre_pfmg_mri.cpp
+++ examples/arkode/CXX_parhyp/ark_heat2D_hypre_pfmg_mri.cpp
@@ -61,10 +61,10 @@
 #include "sunlinsol/sunlinsol_spgmr.h" // access to GMRES SUNLinearSolver
 
 // Macros for problem constants
-#define PI    SUN_RCONST(3.141592653589793238462643383279502884197169)
-#define ZERO  SUN_RCONST(0.0)
-#define ONE   SUN_RCONST(1.0)
-#define TWO   SUN_RCONST(2.0)
+#define PI   SUN_RCONST(3.141592653589793238462643383279502884197169)
+#define ZERO SUN_RCONST(0.0)
+#define ONE  SUN_RCONST(1.0)
+#define TWO  SUN_RCONST(2.0)
 
 // Macro to access (x,y) location in 1D NVector array
 #define IDX(x, y, n) ((n) * (y) + (x))
