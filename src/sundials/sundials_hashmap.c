42dabb6c2af7ec6c1802a1ccb70c85fb1a2fb217
Balos, Cody, J <balos1@llnl.gov>
2025-02-10 22:11:14 -0800

    add maybe_unused to Values function

--- src/sundials/sundials_hashmap.c
+++ src/sundials/sundials_hashmap.c
@@ -60,7 +60,7 @@ static inline int64_t sunHashMapIdxFromKey(SUNHashMap map, const char* key)
 
   **Arguments:**
     * ``capacity`` -- the initial capactity number of the hashmap
-    * ``destroyKeyValue`` -- a callback function that frees the KVP, including both the key and value. 
+    * ``destroyKeyValue`` -- a callback function that frees the KVP, including both the key and value.
     * ``map`` -- on input, a SUNHasMap pointer, on output the SUNHashMap will be
                  allocated
 
@@ -449,6 +449,7 @@ SUNErrCode SUNHashMap_Sort(SUNHashMap map, SUNHashMapKeyValue** sorted,
   **Returns:**
     * A SUNErrCode indicating success or a failure
  */
+SUNDIALS_MAYBE_UNUSED
 SUNErrCode SUNHashMap_Values(SUNHashMap map, void*** values, int64_t value_size)
 {
   int count = 0;
