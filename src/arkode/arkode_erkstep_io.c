a4513415a579bb38c009512c8f86dabb269b3e98
Daniel R. Reynolds <reynolds@smu.edu>
2025-03-01 22:16:48 -0600

    Added command-line support to ERKStep

--- src/arkode/arkode_erkstep_io.c
+++ src/arkode/arkode_erkstep_io.c
@@ -23,6 +23,7 @@
 #include <stdlib.h>
 #include <sundials/sundials_math.h>
 #include <sundials/sundials_types.h>
+#include <sundials/priv/sundials_cli.h>
 
 #include "arkode_erkstep_impl.h"
 
@@ -254,6 +255,39 @@ int ERKStepGetTimestepperStats(void* arkode_mem, long int* expsteps,
 /*===============================================================
   Private functions attached to ARKODE
   ===============================================================*/
+
+/*---------------------------------------------------------------
+  erkStep_SetFromCommandLine:
+
+  Provides command-line control over ERKStep-specific "set" routines.
+  ---------------------------------------------------------------*/
+int erkStep_SetFromCommandLine(ARKodeMem ark_mem, int* i, char* argv[],
+                               const size_t offset, sunbooleantype* arg_used)
+{
+
+  /* Set lists of command-line arguments, and the corresponding set routines */
+  static struct sunKeyCharPair char_pairs[] = {{"table_name", ERKStepSetTableName}};
+  static const int num_char_keys = sizeof(char_pairs) / sizeof(*char_pairs);
+
+  /* check all "char" command-line options */
+  int j, retval;
+  for (j = 0; j < num_char_keys; j++)
+  {
+    retval = sunCheckAndSetCharArg((void*) ark_mem, i, argv, offset,
+                                  char_pairs[j].key,
+                                  char_pairs[j].set, arg_used);
+    if (retval != ARK_SUCCESS)
+    {
+      arkProcessError(ark_mem, retval, __LINE__, __func__, __FILE__,
+                      "error setting command-line argument: %s",
+                      char_pairs[j].key);
+      return retval;
+    }
+    if (*arg_used) { return ARK_SUCCESS; }
+  }
+
+  return (ARK_SUCCESS);
+}
 
 /*---------------------------------------------------------------
   erkStep_SetRelaxFn:
