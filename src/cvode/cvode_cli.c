d35479ed3695d6bd8b62c0becd5162a499eeaf5f
Daniel R. Reynolds <reynolds@smu.edu>
2025-03-12 17:40:10 -0500

    Added command-line control for CVODE

---
+++ src/cvode/cvode_cli.c
@@ -0,0 +1,217 @@
+/*---------------------------------------------------------------
+ * Programmer(s): Daniel R. Reynolds @ SMU
+ *---------------------------------------------------------------
+ * SUNDIALS Copyright Start
+ * Copyright (c) 2002-2025, Lawrence Livermore National Security
+ * and Southern Methodist University.
+ * All rights reserved.
+ *
+ * See the top-level LICENSE and NOTICE files for details.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SUNDIALS Copyright End
+ *---------------------------------------------------------------
+ * This file provides command-line control over optional inputs
+ * to CVODE.
+ *--------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sundials/sundials_types.h>
+#include "sundials_cli.h"
+#include "cvode/cvode.h"
+#include "cvode/cvode_ls.h"
+#include "cvode/cvode_proj.h"
+#include "cvode_impl.h"
+
+/*---------------------------------------------------------------
+  CVodeSetFromCommandLine:
+
+  Parses the command line to control scalar-valued CVODE options.
+  ---------------------------------------------------------------*/
+
+int CVodeSetFromCommandLine(void* cvode_mem, const char* cvid, int argc,
+                            char* argv[])
+{
+  CVodeMem cv_mem;
+  if (cvode_mem == NULL)
+  {
+    cvProcessError(NULL, CV_MEM_NULL, __LINE__, __func__, __FILE__,
+                    MSGCV_NO_MEM);
+    return (CV_MEM_NULL);
+  }
+  cv_mem = (CVodeMem)cvode_mem;
+
+  /* Set lists of command-line arguments, and the corresponding set routines */
+  static struct sunKeyIntPair int_pairs[] =
+    {{"max_conv_fails", CVodeSetMaxConvFails},
+     {"max_err_test_fails", CVodeSetMaxErrTestFails},
+     {"max_hnil_warns", CVodeSetMaxHnilWarns},
+     {"max_nonlin_iters", CVodeSetMaxNonlinIters},
+     {"max_order", CVodeSetMaxOrd},
+     {"stab_lim_det", CVodeSetStabLimDet},
+     {"interpolate_stop_time", CVodeSetInterpolateStopTime},
+     {"use_integrator_fused_kernels", CVodeSetUseIntegratorFusedKernels},
+     {"num_efails_eta_max_err_fail", CVodeSetNumFailsEtaMaxErrFail},
+     {"linear_solution_scaling", CVodeSetLinearSolutionScaling},
+     {"proj_err_est", CVodeSetProjErrEst},
+     {"max_num_proj_fails", CVodeSetMaxNumProjFails}};
+  static const int num_int_keys = sizeof(int_pairs) / sizeof(*int_pairs);
+
+  static struct sunKeyLongPair long_pairs[] = {
+    {"lsetup_frequency", CVodeSetLSetupFrequency},
+    {"max_num_steps", CVodeSetMaxNumSteps},
+    {"monitor_frequency", CVodeSetMonitorFrequency},
+    {"num_steps_eta_max_early_step", CVodeSetNumStepsEtaMaxEarlyStep},
+    {"jac_eval_frequency", CVodeSetJacEvalFrequency},
+    {"proj_frequency", CVodeSetProjFrequency}};
+  static const int num_long_keys = sizeof(long_pairs) / sizeof(*long_pairs);
+
+  static struct sunKeyRealPair real_pairs[] =
+    {{"dgmax_lsetup", CVodeSetDeltaGammaMaxLSetup},
+     {"init_step", CVodeSetInitStep},
+     {"max_step", CVodeSetMaxStep},
+     {"min_step", CVodeSetMinStep},
+     {"stop_time", CVodeSetStopTime},
+     {"nonlin_conv_coef", CVodeSetNonlinConvCoef},
+     {"eta_max_first_step", CVodeSetEtaMaxFirstStep},
+     {"eta_max_early_step", CVodeSetEtaMaxEarlyStep},
+     {"eta_max", CVodeSetEtaMax},
+     {"eta_min", CVodeSetEtaMin},
+     {"eta_min_err_fail", CVodeSetEtaMinErrFail},
+     {"eta_max_err_fail", CVodeSetEtaMaxErrFail},
+     {"eta_conv_fail", CVodeSetEtaConvFail},
+     {"delta_gamma_max_bad_jac", CVodeSetDeltaGammaMaxBadJac},
+     {"eps_lin", CVodeSetEpsLin},
+     {"ls_norm_factor", CVodeSetLSNormFactor},
+     {"eps_proj", CVodeSetEpsProj},
+     {"proj_fail_eta", CVodeSetProjFailEta}};
+  static const int num_real_keys = sizeof(real_pairs) / sizeof(*real_pairs);
+
+  static struct sunKeyTwoRealPair tworeal_pairs[] =
+    {{"eta_fixed_step_bounds", CVodeSetEtaFixedStepBounds},
+     {"scalar_tolerances", CVodeSStolerances}};
+  static const int num_tworeal_keys = sizeof(tworeal_pairs) / sizeof(*tworeal_pairs);
+
+  static struct sunKeyActionPair action_pairs[] =
+    {{"clear_stop_time", CVodeClearStopTime},
+     {"no_inactive_root_warn", CVodeSetNoInactiveRootWarn}};
+  static const int num_action_keys = sizeof(action_pairs) / sizeof(*action_pairs);
+
+  int i, j, retval;
+  for (i = 1; i < argc; i++)
+  {
+    sunbooleantype arg_used = SUNFALSE;
+
+    /* if cvid is supplied, skip command-line arguments that do not begin with cvid;
+       else, skip command-line arguments that do not begin with "cvode." */
+    size_t offset;
+    if (strlen(cvid) > 0)
+    {
+      if (strncmp(argv[i], cvid, strlen(cvid)) != 0) { continue; }
+      offset = strlen(cvid) + 1;
+    }
+    else
+    {
+      static const char* prefix = "cvode.";
+      if (strncmp(argv[i], prefix, strlen(prefix)) != 0) { continue; }
+      offset = strlen(prefix);
+    }
+
+    /* check all "int" command-line options */
+    for (j = 0; j < num_int_keys; j++)
+    {
+      retval = sunCheckAndSetIntArg(cvode_mem, &i, argv, offset,
+                                    int_pairs[j].key, int_pairs[j].set,
+                                    &arg_used);
+      if (retval != CV_SUCCESS)
+      {
+        cvProcessError(cv_mem, retval, __LINE__, __func__, __FILE__,
+                        "error setting command-line argument: %s",
+                        int_pairs[j].key);
+        return retval;
+      }
+      if (arg_used) break;
+    }
+    if (arg_used) continue;
+
+    /* check all long int command-line options */
+    for (j = 0; j < num_long_keys; j++)
+    {
+      retval = sunCheckAndSetLongArg(cvode_mem, &i, argv, offset,
+                                     long_pairs[j].key, long_pairs[j].set,
+                                     &arg_used);
+      if (retval != CV_SUCCESS)
+      {
+        cvProcessError(cv_mem, retval, __LINE__, __func__, __FILE__,
+                        "error setting command-line argument: %s",
+                        long_pairs[j].key);
+        return retval;
+      }
+      if (arg_used) break;
+    }
+    if (arg_used) continue;
+
+    /* check all real command-line options */
+    for (j = 0; j < num_real_keys; j++)
+    {
+      retval = sunCheckAndSetRealArg(cvode_mem, &i, argv, offset,
+                                     real_pairs[j].key, real_pairs[j].set,
+                                     &arg_used);
+      if (retval != CV_SUCCESS)
+      {
+        cvProcessError(cv_mem, retval, __LINE__, __func__, __FILE__,
+                        "error setting command-line argument: %s",
+                        real_pairs[j].key);
+        return retval;
+      }
+      if (arg_used) break;
+    }
+    if (arg_used) continue;
+
+    /* check all pair-of-real command-line options */
+    for (j = 0; j < num_tworeal_keys; j++)
+    {
+      retval = sunCheckAndSetTwoRealArg(cvode_mem, &i, argv, offset,
+                                        tworeal_pairs[j].key,
+                                        tworeal_pairs[j].set, &arg_used);
+      if (retval != CV_SUCCESS)
+      {
+        cvProcessError(cv_mem, retval, __LINE__, __func__, __FILE__,
+                        "error setting command-line argument: %s",
+                        tworeal_pairs[j].key);
+        return retval;
+      }
+      if (arg_used) break;
+    }
+    if (arg_used) continue;
+
+    /* check all action command-line options */
+    for (j = 0; j < num_action_keys; j++)
+    {
+      retval = sunCheckAndSetActionArg(cvode_mem, &i, argv, offset,
+                                       action_pairs[j].key, action_pairs[j].set,
+                                       &arg_used);
+      if (retval != CV_SUCCESS)
+      {
+        cvProcessError(cv_mem, retval, __LINE__, __func__, __FILE__,
+                        "error setting command-line argument: %s",
+                        action_pairs[j].key);
+        return retval;
+      }
+      if (arg_used) break;
+    }
+    if (arg_used) continue;
+
+    /* warn for uninterpreted cvid.X arguments */
+    cvProcessError(cv_mem, CV_WARNING, __LINE__, __func__, __FILE__,
+                    "WARNING: argument %s was not handled\n", argv[i]);
+  }
+
+  return (CV_SUCCESS);
+}
+
+/*===============================================================
+  EOF
+  ===============================================================*/
