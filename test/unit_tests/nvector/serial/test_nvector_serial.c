5f27d2fd2da543ede19a5fceb0fa9d582c137424
Balos, Cody, J <balos1@llnl.gov>
2025-03-19 19:36:39 -0400

    separate out complex vector test, fixes in serial vector

--- test/unit_tests/nvector/serial/test_nvector_serial.c
+++ test/unit_tests/nvector/serial/test_nvector_serial.c
@@ -22,6 +22,7 @@
 #include <sundials/sundials_types.h>
 
 #include "test_nvector.h"
+#include "test_nvector_complex.h"
 
 /* ----------------------------------------------------------------------
  * Main NVector Testing Routine
@@ -77,23 +78,23 @@ int main(int argc, char* argv[])
   }
 
   /* Check vector ID */
-  fails += Test_N_VGetVectorID(X, SUNDIALS_NVEC_SERIAL, 0);
+  fails += Test_N_VGetVectorID_Z(X, SUNDIALS_NVEC_SERIAL, 0);
 
   /* Check vector length */
-  fails += Test_N_VGetLength(X, 0);
+  fails += Test_N_VGetLength_Z(X, 0);
 
   /* Check vector communicator */
-  fails += Test_N_VGetCommunicator(X, SUN_COMM_NULL, 0);
+  fails += Test_N_VGetCommunicator_Z(X, SUN_COMM_NULL, 0);
 
   /* Test clone functions */
-  fails += Test_N_VCloneEmpty(X, 0);
-  fails += Test_N_VClone(X, length, 0);
-  fails += Test_N_VCloneEmptyVectorArray(5, X, 0);
-  fails += Test_N_VCloneVectorArray(5, X, length, 0);
+  fails += Test_N_VCloneEmpty_Z(X, 0);
+  fails += Test_N_VClone_Z(X, length, 0);
+  fails += Test_N_VCloneEmptyVectorArray_Z(5, X, 0);
+  fails += Test_N_VCloneVectorArray_Z(5, X, length, 0);
 
   /* Test setting/getting array data */
-  fails += Test_N_VSetArrayPointer(W, length, 0);
-  fails += Test_N_VGetArrayPointer(X, length, 0);
+  fails += Test_N_VSetArrayPointer_Z(W, length, 0);
+  fails += Test_N_VGetArrayPointer_Z(X, length, 0);
 
   /* Clone additional vectors for testing */
   Y = N_VClone(X);
@@ -120,25 +121,25 @@ int main(int argc, char* argv[])
   /* Standard vector operation tests */
   printf("\nTesting standard vector operations:\n\n");
 
-  fails += Test_N_VConst(X, length, 0);
-  fails += Test_N_VLinearSum(X, Y, Z, length, 0);
-  fails += Test_N_VProd(X, Y, Z, length, 0);
-  fails += Test_N_VDiv(X, Y, Z, length, 0);
-  fails += Test_N_VScale(X, Z, length, 0);
-  fails += Test_N_VAbs(X, Z, length, 0);
-  fails += Test_N_VInv(X, Z, length, 0);
-  fails += Test_N_VAddConst(X, Z, length, 0);
-  fails += Test_N_VDotProd(X, Y, length, 0);
-  fails += Test_N_VMaxNorm(X, length, 0);
-  fails += Test_N_VWrmsNorm(X, Y, length, 0);
-  fails += Test_N_VWrmsNormMask(X, Y, Z, length, 0);
-  fails += Test_N_VMin(X, length, 0);
-  fails += Test_N_VWL2Norm(X, Y, length, 0);
-  fails += Test_N_VL1Norm(X, length, 0);
-  fails += Test_N_VCompare(X, Z, length, 0);
-  fails += Test_N_VInvTest(X, Z, length, 0);
-  fails += Test_N_VConstrMask(X, Y, Z, length, 0);
-  fails += Test_N_VMinQuotient(X, Y, length, 0);
+  fails += Test_N_VConst_Z(X, length, 0);
+  fails += Test_N_VLinearSum_Z(X, Y, Z, length, 0);
+  fails += Test_N_VProd_Z(X, Y, Z, length, 0);
+  fails += Test_N_VDiv_Z(X, Y, Z, length, 0);
+  fails += Test_N_VScale_Z(X, Z, length, 0);
+  fails += Test_N_VAbs_Z(X, Z, length, 0);
+  fails += Test_N_VInv_Z(X, Z, length, 0);
+  fails += Test_N_VAddConst_Z(X, Z, length, 0);
+  fails += Test_N_VDotProd_Z(X, Y, length, 0);
+  fails += Test_N_VMaxNorm_Z(X, length, 0);
+  fails += Test_N_VWrmsNorm_Z(X, Y, length, 0);
+  fails += Test_N_VWrmsNormMask_Z(X, Y, Z, length, 0);
+  fails += Test_N_VMin_Z(X, length, 0);
+  fails += Test_N_VWL2Norm_Z(X, Y, length, 0);
+  fails += Test_N_VL1Norm_Z(X, length, 0);
+  fails += Test_N_VCompare_Z(X, Z, length, 0);
+  fails += Test_N_VInvTest_Z(X, Z, length, 0);
+  fails += Test_N_VConstrMask_Z(X, Y, Z, length, 0);
+  fails += Test_N_VMinQuotient_Z(X, Y, length, 0);
 
   /* Fused and vector array operations tests (disabled) */
   printf("\nTesting fused and vector array operations (disabled):\n\n");
@@ -158,18 +159,18 @@ int main(int argc, char* argv[])
   }
 
   /* fused operations */
-  fails += Test_N_VLinearCombination(U, length, 0);
-  fails += Test_N_VScaleAddMulti(U, length, 0);
-  fails += Test_N_VDotProdMulti(U, length, 0);
+  fails += Test_N_VLinearCombination_Z(U, length, 0);
+  fails += Test_N_VScaleAddMulti_Z(U, length, 0);
+  fails += Test_N_VDotProdMulti_Z(U, length, 0);
 
   /* vector array operations */
-  fails += Test_N_VLinearSumVectorArray(U, length, 0);
-  fails += Test_N_VScaleVectorArray(U, length, 0);
-  fails += Test_N_VConstVectorArray(U, length, 0);
-  fails += Test_N_VWrmsNormVectorArray(U, length, 0);
-  fails += Test_N_VWrmsNormMaskVectorArray(U, length, 0);
-  fails += Test_N_VScaleAddMultiVectorArray(U, length, 0);
-  fails += Test_N_VLinearCombinationVectorArray(U, length, 0);
+  fails += Test_N_VLinearSumVectorArray_Z(U, length, 0);
+  fails += Test_N_VScaleVectorArray_Z(U, length, 0);
+  fails += Test_N_VConstVectorArray_Z(U, length, 0);
+  fails += Test_N_VWrmsNormVectorArray_Z(U, length, 0);
+  fails += Test_N_VWrmsNormMaskVectorArray_Z(U, length, 0);
+  fails += Test_N_VScaleAddMultiVectorArray_Z(U, length, 0);
+  fails += Test_N_VLinearCombinationVectorArray_Z(U, length, 0);
 
   /* Fused and vector array operations tests (enabled) */
   printf("\nTesting fused and vector array operations (enabled):\n\n");
@@ -190,42 +191,42 @@ int main(int argc, char* argv[])
   }
 
   /* fused operations */
-  fails += Test_N_VLinearCombination(V, length, 0);
-  fails += Test_N_VScaleAddMulti(V, length, 0);
-  fails += Test_N_VDotProdMulti(V, length, 0);
+  fails += Test_N_VLinearCombination_Z(V, length, 0);
+  fails += Test_N_VScaleAddMulti_Z(V, length, 0);
+  fails += Test_N_VDotProdMulti_Z(V, length, 0);
 
   /* vector array operations */
-  fails += Test_N_VLinearSumVectorArray(V, length, 0);
-  fails += Test_N_VScaleVectorArray(V, length, 0);
-  fails += Test_N_VConstVectorArray(V, length, 0);
-  fails += Test_N_VWrmsNormVectorArray(V, length, 0);
-  fails += Test_N_VWrmsNormMaskVectorArray(V, length, 0);
-  fails += Test_N_VScaleAddMultiVectorArray(V, length, 0);
-  fails += Test_N_VLinearCombinationVectorArray(V, length, 0);
+  fails += Test_N_VLinearSumVectorArray_Z(V, length, 0);
+  fails += Test_N_VScaleVectorArray_Z(V, length, 0);
+  fails += Test_N_VConstVectorArray_Z(V, length, 0);
+  fails += Test_N_VWrmsNormVectorArray_Z(V, length, 0);
+  fails += Test_N_VWrmsNormMaskVectorArray_Z(V, length, 0);
+  fails += Test_N_VScaleAddMultiVectorArray_Z(V, length, 0);
+  fails += Test_N_VLinearCombinationVectorArray_Z(V, length, 0);
 
   /* local reduction operations */
   printf("\nTesting local reduction operations:\n\n");
 
-  fails += Test_N_VDotProdLocal(X, Y, length, 0);
-  fails += Test_N_VMaxNormLocal(X, length, 0);
-  fails += Test_N_VMinLocal(X, length, 0);
-  fails += Test_N_VL1NormLocal(X, length, 0);
-  fails += Test_N_VWSqrSumLocal(X, Y, length, 0);
-  fails += Test_N_VWSqrSumMaskLocal(X, Y, Z, length, 0);
-  fails += Test_N_VInvTestLocal(X, Z, length, 0);
-  fails += Test_N_VConstrMaskLocal(X, Y, Z, length, 0);
-  fails += Test_N_VMinQuotientLocal(X, Y, length, 0);
+  fails += Test_N_VDotProdLocal_Z(X, Y, length, 0);
+  fails += Test_N_VMaxNormLocal_Z(X, length, 0);
+  fails += Test_N_VMinLocal_Z(X, length, 0);
+  fails += Test_N_VL1NormLocal_Z(X, length, 0);
+  fails += Test_N_VWSqrSumLocal_Z(X, Y, length, 0);
+  fails += Test_N_VWSqrSumMaskLocal_Z(X, Y, Z, length, 0);
+  fails += Test_N_VInvTestLocal_Z(X, Z, length, 0);
+  fails += Test_N_VConstrMaskLocal_Z(X, Y, Z, length, 0);
+  fails += Test_N_VMinQuotientLocal_Z(X, Y, length, 0);
 
   /* local fused reduction operations */
   printf("\nTesting local fused reduction operations:\n\n");
-  fails += Test_N_VDotProdMultiLocal(V, length, 0);
+  fails += Test_N_VDotProdMultiLocal_Z(V, length, 0);
 
   /* XBraid interface operations */
   printf("\nTesting XBraid interface operations:\n\n");
 
-  fails += Test_N_VBufSize(X, length, 0);
-  fails += Test_N_VBufPack(X, length, 0);
-  fails += Test_N_VBufUnpack(X, length, 0);
+  fails += Test_N_VBufSize_Z(X, length, 0);
+  fails += Test_N_VBufPack_Z(X, length, 0);
+  fails += Test_N_VBufUnpack_Z(X, length, 0);
 
   /* Free vectors */
   N_VDestroy(W);
@@ -246,7 +247,12 @@ int main(int argc, char* argv[])
 /* ----------------------------------------------------------------------
  * Implementation specific utility functions for vector tests
  * --------------------------------------------------------------------*/
-int check_ans(sunrealtype ans, N_Vector X, sunindextype local_length)
+ int check_ans(sunrealtype ans, N_Vector X, sunindextype local_length)
+ {
+    return check_ans_Z(ans, X, local_length);
+ }
+
+int check_ans_Z(sunscalartype ans, N_Vector X, sunindextype local_length)
 {
   int failure = 0;
   sunindextype i;
@@ -255,7 +261,8 @@ int check_ans(sunrealtype ans, N_Vector X, sunindextype local_length)
   Xdata = N_VGetArrayPointer(X);
 
   /* check vector data */
-  for (i = 0; i < local_length; i++) { failure += SUNRCompare(Xdata[i], ans); }
+  for (i = 0; i < local_length; i++) { failure += SUNCompare(Xdata[i], ans);
+    if (failure) fprintf(stderr, ">>>> Xdata[%d]=%g, ans=%g\n", i, SUN_REAL(Xdata[i]), ans); }
 
   return (failure > ZERO) ? (1) : (0);
 }
@@ -272,8 +279,20 @@ void set_element(N_Vector X, sunindextype i, sunrealtype val)
   set_element_range(X, i, i, val);
 }
 
+void set_element_Z(N_Vector X, sunindextype i, sunscalartype val)
+{
+  /* set i-th element of data array */
+  set_element_range_Z(X, i, i, val);
+}
+
 void set_element_range(N_Vector X, sunindextype is, sunindextype ie,
-                       sunrealtype val)
+  sunrealtype val)
+{
+  return set_element_range_Z(X, is, ie, val);
+}
+
+void set_element_range_Z(N_Vector X, sunindextype is, sunindextype ie,
+                         sunscalartype val)
 {
   sunindextype i;
 
