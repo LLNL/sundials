acee18415d98289baf11cf1c2cd4c82f755c26c3
maggul <maggul@smu.edu>
2025-05-17 03:37:55 -0500

    arnoldi_Create_Common

--- include/sundials/sundials_arnoldi.h
+++ include/sundials/sundials_arnoldi.h
@@ -11,8 +11,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * SUNDIALS Copyright End
  * -----------------------------------------------------------------
- * This is the implementation file for the eigenvalue implementation of
- * the SUNLINSOL package.
+ * This is the header file for the eigenvalue estimation of
+ * the SUNARNOLDI package.
  * -----------------------------------------------------------------*/
 
 #ifndef _ARNOLDI_H
@@ -28,13 +28,11 @@ extern "C" {
 
 /* Default ARNOLDI parameters */
 #define ARNOLDI_MAXL_DEFAULT 3
-#define MSG_ARK_NULL_ATIMES   "ATimes is null."
-#define MSG_ARK_NULL_q        "q is null."
-#define MSG_ARK_NULL_SUNCTX   "sunctx is null."
-#define MSG_ARK_BAD_NVECTOR   "Bad NVector."
-#define MSG_ARNOLDI_MEM_FAIL  "ARNOLDI memory fail."
+#define DEFAULT_POWER_OF_A   0
 
-#define DEFAULT_POWER_OF_A    0
+/* SUNRhsFn type definition */
+typedef int (*SUNRhsFn)(sunrealtype t, N_Vector y, N_Vector ydot,
+                        void* user_data);
 
 /*===============================================================
   ARNOLDI module data structure
@@ -51,6 +49,7 @@ typedef struct ARNOLDIMemRec
 {
   /* ARNOLDI MEMORY specification */
   SUNATimesFn ATimes;   /* User provided ATimes function */
+  SUNRhsFn SUNRhs;      /* User provided SUNRhs function */
   void* Adata;          /* ATimes function data*/
 
   N_Vector *V, q;       /* Krylov subspace vectors */
@@ -71,11 +70,12 @@ typedef struct {
  * Exported Functions for ARNOLDI
  * ------------------------------------- */
 
-/* Creation and Reinitialization functions */
+/* Creation and Estimation functions */
 
-SUNDIALS_EXPORT void* ArnoldiCreate(SUNATimesFn ATimes, void* Adata,
+SUNDIALS_EXPORT void* ArnoldiCreateATimes(SUNATimesFn ATimes, void* Adata,
                 N_Vector q, int maxl, SUNContext sunctx);
-
+SUNDIALS_EXPORT void* ArnoldiCreateSUNRhs(SUNRhsFn SUNRhs, void* Adata,
+                N_Vector q, int maxl, SUNContext sunctx);
 SUNDIALS_EXPORT int ArnoldiComputeHess(ARNOLDIMem arnoldi_mem);
 
 SUNDIALS_EXPORT int ArnoldiPreProcess(ARNOLDIMem arnoldi_mem);
--- src/sundials/sundials_arnoldi.c
+++ src/sundials/sundials_arnoldi.c
@@ -11,36 +11,16 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * SUNDIALS Copyright End
  * -----------------------------------------------------------------
- * This is the implementation file for the eigenvalue implementation of
- * the SUNLINSOL package.
+ * This is the implementation file for the eigenvalue estimation of
+ * the SUNARNOLDI package.
  * -----------------------------------------------------------------*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <arkode/arkode.h>
-#include <sundials/sundials_arnoldi.h>
+#include <sundials/priv/sundials_arnoldi_impl.h>
 
 #define ZERO SUN_RCONST(0.0)
-
-/*
- * -----------------------------------------------------------------
- * internal functions
- * -----------------------------------------------------------------
- */
-
-sunbooleantype arnoldi_CheckNVector(N_Vector tmpl);
-double arnoldi_magnitude(const suncomplextype *c);
-int arnoldi_compare(const void *a, const void *b);
-
-/*
- * -----------------------------------------------------------------
- * LAPACK function
- * -----------------------------------------------------------------
- */
-
-extern void dgeev_(char* jobvl, char* jobvr, int* n, double* a, int* lda,
-                   double* wr, double* wi, double* vl, int* ldvl, double* vr,
-                   int* ldvr, double* work, int* lwork, int* info);
 
 /*
  * -----------------------------------------------------------------
@@ -48,45 +28,276 @@ extern void dgeev_(char* jobvl, char* jobvr, int* n, double* a, int* lda,
  * -----------------------------------------------------------------
  */
 
-void* ArnoldiCreate(SUNATimesFn ATimes, void* Adata,
+void* ArnoldiCreateATimes(SUNATimesFn ATimes, void* Adata,
                 N_Vector q, int maxl, SUNContext sunctx)
 {
+  ARNOLDIMem arnoldi_mem;
+
+  /* Test if Atimes is provided */
+  if (ATimes == NULL)
+  {
+    printf(MSG_ARNOLDI_NULL_ATIMES);
+
+    return NULL;
+  }
+
+  /* Create shared ARNOLDI memory structure */
+  arnoldi_mem = arnoldi_Create_Common(Adata, q, maxl, sunctx);
+
+  /* Copy ATimes into ARNOLDI memory */
+  arnoldi_mem->ATimes      = ATimes;
+
+  return (void*)arnoldi_mem;
+}
+
+void* ArnoldiCreateSUNRhs(SUNRhsFn SUNRhs, void* RhsData,
+                N_Vector q, int maxl, SUNContext sunctx)
+{
+  ARNOLDIMem arnoldi_mem;
+
+  /* Test if Atimes is provided */
+  if (SUNRhs == NULL)
+  {
+    printf(MSG_ARNOLDI_NULL_SUNRHSFN);
+
+    return NULL;
+  }
+
+  /* Create shared ARNOLDI memory structure */
+  arnoldi_mem = arnoldi_Create_Common(RhsData, q, maxl, sunctx);
+
+  /* Copy SUNRhs into ARNOLDI memory */
+  arnoldi_mem->SUNRhs      = SUNRhs;
+
+  return (void*)arnoldi_mem;
+}
+
+/* Set the initial q = A^{power_of_A}q/||A^{power_of_A}q|| */
+int ArnoldiPreProcess(ARNOLDIMem arnoldi_mem)
+{
   int retval;
+
+  /* Check if Arnoldi memory is allocated */
+  if(arnoldi_mem == NULL)
+  {
+    printf(MSG_ARNOLDI_MEM_FAIL);
+
+    return -1;
+  }
+
+  /* Check if ATimes is provided */
+  if(arnoldi_mem->ATimes == NULL)
+  {
+    printf(MSG_ARNOLDI_NULL_ATIMES);
+
+    return -1;
+  }
+
+  sunrealtype normq;
+
+  /* Set the initial q = A^{power_of_A}q/||A^{power_of_A}q|| */
+  for(int i = 0; i < arnoldi_mem->power_of_A; i++) {
+    retval = arnoldi_mem->ATimes(arnoldi_mem->Adata, arnoldi_mem->V[0], arnoldi_mem->q);
+    if (retval != 0)
+    {
+      (retval < 0) ?
+      printf(MSG_ARNOLDI_ATIMES_FAIL_UNREC) :
+      printf(MSG_ARNOLDI_ATIMES_FAIL_REC);
+
+      return ARK_ILL_INPUT;
+    }
+    normq = N_VDotProd(arnoldi_mem->q, arnoldi_mem->q);
+    normq = SUNRsqrt(normq);
+    N_VScale(SUN_RCONST(1.0)/normq, arnoldi_mem->q, arnoldi_mem->V[0]);
+  }
+
+  return 0;
+}
+
+/* Compute the Hessenberg matrix Arnoldi_mem->Hes*/
+int ArnoldiComputeHess(ARNOLDIMem arnoldi_mem)
+{
+  int retval;
+
+  /* Check if Arnoldi memory is allocated */
+  if(arnoldi_mem == NULL)
+  {
+    printf(MSG_ARNOLDI_MEM_FAIL);
+
+    return -1;
+  }
+
+  /* Initialize the Hessenberg matrix Hes with zeros */
+  for (int i = 0; i < arnoldi_mem->maxl; i++)
+  {
+    for (int j = 0; j < arnoldi_mem->maxl; j++) { arnoldi_mem->Hes[i][j] = ZERO; }
+  }
+
+  for (int i = 0; i < arnoldi_mem->maxl; i++)
+  {
+    /* Compute the next Krylov vector */
+    retval = arnoldi_mem->ATimes(arnoldi_mem->Adata, arnoldi_mem->V[i], arnoldi_mem->V[i+1]);
+    if (retval != 0)
+    {
+      (retval < 0) ?
+      printf(MSG_ARNOLDI_ATIMES_FAIL_UNREC) :
+      printf(MSG_ARNOLDI_ATIMES_FAIL_REC);
+
+      return ARK_ILL_INPUT;
+    }
+
+    if(SUNModifiedGS(arnoldi_mem->V, arnoldi_mem->Hes, i + 1, arnoldi_mem->maxl, &(arnoldi_mem->Hes[i + 1][i])) != SUN_SUCCESS)
+    {
+      printf(MSG_ARNOLDI_GS_FAIL);
+
+      return -1;
+    }
+
+    /* Unitize the computed orthogonal vector */
+    N_VScale(SUN_RCONST(1.0)/arnoldi_mem->Hes[i + 1][i], arnoldi_mem->V[i+1], arnoldi_mem->V[i+1]);
+  }
+
+  return 0;
+}
+
+/* Estimate the dominant eigvalues of the Hessenberg matrix */
+suncomplextype ArnoldiEstimate(ARNOLDIMem arnoldi_mem) {
+  suncomplextype dom_eig;
+  dom_eig.real = ZERO;
+  dom_eig.imag = ZERO;
+
+  /* Check if Arnoldi memory is allocated */
+  if(arnoldi_mem == NULL)
+  {
+    printf(MSG_ARNOLDI_MEM_FAIL);
+
+    return dom_eig;
+  }
+
+  int n = arnoldi_mem->maxl;
+
+  /* Create the vector A which holds rows of the Hessenberg matrix in the given order */
+  sunrealtype* A;
+  A = (sunrealtype*)malloc((n*n) * sizeof(sunrealtype));
+  int k = 0;
+  for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+          A[k] = arnoldi_mem->Hes[i][j];
+          k++;
+      }
+  }
+
+  sunrealtype wr[n], wi[n];     // Real and imaginary parts of eigenvalues
+  sunrealtype vl[n*n], vr[n*n]; // Left and right eigenvectors (optional, can be NULL)
+  int lda = n, ldvl = n, ldvr = n;
+  int info, lwork = 4 * n;
+  sunrealtype work[4 * n]; // Workspace array
+
+  char jobvl = 'N'; // Do not compute left eigenvectors
+  char jobvr = 'N'; // Do not compute right eigenvectors
+
+  // Call LAPACK's dgeev function
+  dgeev_(&jobvl, &jobvr, &n, A, &lda, wr, wi, vl, &ldvl, vr, &ldvr, work, &lwork, &info);
+
+  if (info != 0) {
+      printf(MSG_ARNOLDI_LAPACK_FAIL, info);
+      return dom_eig;
+  }
+
+  //Following part will order the eigenvalues by their magnitude
+  {
+    // Create an array of suncomplextype structs
+    suncomplextype *arr = (suncomplextype *)malloc(n * sizeof(suncomplextype));
+    for (int i = 0; i < n; i++) {
+        arr[i].real = wr[i];
+        arr[i].imag = wi[i];
+    }
+
+    // Sort the array using qsort
+    qsort(arr, n, sizeof(suncomplextype), arnoldi_Compare);
+
+    // Update the original arrays
+    for (int i = 0; i < n; i++) {
+        wr[i] = arr[i].real;
+        wi[i] = arr[i].imag;
+    }
+
+    // Cleanup
+    free(arr);
+  }
+
+  // Print eigenvalues
+  printf("\nEigenvalues:\n");
+  for (int i = 0; i < n; i++) {
+      if (wi[i] == 0.0) {
+          printf("%f\n", wr[i]); // sunrealtype eigenvalue
+      } else {
+          printf("%f + %fi\n", wr[i], wi[i]); // suncomplextype eigenvalue
+      }
+  }
+
+  dom_eig.real = wr[0];
+  dom_eig.imag = wi[0];
+
+  return dom_eig;
+}
+
+/*===============================================================
+  Internal utility routines
+  ===============================================================*/
+
+/*---------------------------------------------------------------
+  arnoldi_Create_Common:
+
+  This routine creates common memory for both ATimes and SUNRhsFn.
+  ---------------------------------------------------------------*/
+
+void* arnoldi_Create_Common(void* Adata, N_Vector q, int maxl, SUNContext sunctx)
+{
   ARNOLDIMem arnoldi_mem;
   sunbooleantype nvectorOK;
+
+  /* Test if q is provided */
+  if (q == NULL)
+  {
+    printf(MSG_ARNOLDI_NULL_q);
+
+    return NULL;
+  }
 
   /* Test if all required vector operations are implemented */
   nvectorOK = arnoldi_CheckNVector(q);
   if (!nvectorOK)
   {
-    return NULL;
-  }
+    printf(MSG_ARNOLDI_BAD_NVECTOR);
 
-  /* Test if Atimes is provided */
-  if (ATimes == NULL)
-  {
     return NULL;
   }
 
   /* Check if maxl > 2 */
   if (maxl < 3)
   {
+    printf(MSG_ARNOLDI_NOT_ENOUGH_ITER);
+
     return NULL;
   }
 
   if (sunctx == NULL)
   {
+    printf(MSG_ARNOLDI_NULL_SUNCTX);
+
     return NULL;
   }
-
 
   /* Allocate ARNOLDIMem structure, and initialize to zero */
   arnoldi_mem = (ARNOLDIMem)calloc(1, sizeof(*arnoldi_mem));
 
-  arnoldi_mem->ATimes      = ATimes;
+  /* Copy the inputs into ARNOLDI memory */
   arnoldi_mem->Adata       = Adata;
   arnoldi_mem->q           = q;
   arnoldi_mem->maxl        = maxl;
+
+  /* Set the default power of A to start with (# of warm-ups) */
   arnoldi_mem->power_of_A  = DEFAULT_POWER_OF_A;
 
   /* Hessenberg matrix Hes */
@@ -116,133 +327,11 @@ void* ArnoldiCreate(SUNATimesFn ATimes, void* Adata,
   return (void*)arnoldi_mem;
 }
 
-/* Set the initial q = A^{power_of_A}q/||A^{power_of_A}q|| */
-int ArnoldiPreProcess(ARNOLDIMem arnoldi_mem)
-{
-  /* Check if Arnoldi memory is allocated */
-  if(arnoldi_mem == NULL)
-  {
-    printf("Error: Arnoldi memory is not allocated yet!");
-    return -1;
-  }
-
-  /* Check if ATimes is provided */
-  if(arnoldi_mem->ATimes == NULL)
-  {
-    printf("Error: ATimes function is NULL!");
-    return -1;
-  }
-
-  sunrealtype normq;
-
-  /* Set the initial q = A^{power_of_A}q/||A^{power_of_A}q|| */
-  for(int i = 0; i < arnoldi_mem->power_of_A; i++) {
-    arnoldi_mem->ATimes(arnoldi_mem->Adata, arnoldi_mem->V[0], arnoldi_mem->q);
-    normq = N_VDotProd(arnoldi_mem->q, arnoldi_mem->q);
-    normq = SUNRsqrt(normq);
-    N_VScale(SUN_RCONST(1.0)/normq, arnoldi_mem->q, arnoldi_mem->V[0]);
-  }
-}
-
-/* Compute the Hessenberg matrix Arnoldi_mem->Hes*/
-int ArnoldiComputeHess(ARNOLDIMem arnoldi_mem)
-{
-  /* Initialize the Hessenberg matrix Hes with zeros */
-  for (int i = 0; i < arnoldi_mem->maxl; i++)
-  {
-    for (int j = 0; j < arnoldi_mem->maxl; j++) { arnoldi_mem->Hes[i][j] = ZERO; }
-  }
-
-  for (int i = 0; i < arnoldi_mem->maxl; i++)
-  {
-    /* Compute the next Krylov vector */
-    arnoldi_mem->ATimes(arnoldi_mem->Adata, arnoldi_mem->V[i], arnoldi_mem->V[i+1]);
-    SUNModifiedGS(arnoldi_mem->V, arnoldi_mem->Hes, i + 1, arnoldi_mem->maxl, &(arnoldi_mem->Hes[i + 1][i]));
-
-    /* Unitize the computed orthogonal vector */
-    N_VScale(SUN_RCONST(1.0)/arnoldi_mem->Hes[i + 1][i], arnoldi_mem->V[i+1], arnoldi_mem->V[i+1]);
-  }
-}
-
-/* Estimate the dominant eigvalues of the Hessenberg matrix */
-suncomplextype ArnoldiEstimate(ARNOLDIMem arnoldi_mem) {
-    suncomplextype dom_eig;
-    dom_eig.real = ZERO;
-    dom_eig.imag = ZERO;
-
-    int n = arnoldi_mem->maxl;
-
-    /* Create the vector A which holds rows of the Hessenberg matrix in the given order */
-    sunrealtype* A;
-    A = (sunrealtype*)malloc((n*n) * sizeof(sunrealtype));
-    int k = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            A[k] = arnoldi_mem->Hes[i][j];
-            k++;
-        }
-    }
-
-    double wr[n], wi[n];     // Real and imaginary parts of eigenvalues
-    double vl[n*n], vr[n*n]; // Left and right eigenvectors (optional, can be NULL)
-    int lda = n, ldvl = n, ldvr = n;
-    int info, lwork = 4 * n;
-    double work[4 * n]; // Workspace array
-
-    char jobvl = 'N'; // Do not compute left eigenvectors
-    char jobvr = 'N'; // Do not compute right eigenvectors
-
-    // Call LAPACK's dgeev function
-    dgeev_(&jobvl, &jobvr, &n, A, &lda, wr, wi, vl, &ldvl, vr, &ldvr, work, &lwork, &info);
-
-    if (info != 0) {
-        printf("Error: LAPACK dgeev failed with info = %d\n", info);
-        return dom_eig;
-    }
-
-    //Following part will order the eigenvalues by their magnitude
-    {
-      // Create an array of suncomplextype structs
-      suncomplextype *arr = (suncomplextype *)malloc(n * sizeof(suncomplextype));
-      for (int i = 0; i < n; i++) {
-          arr[i].real = wr[i];
-          arr[i].imag = wi[i];
-      }
-
-      // Sort the array
-      qsort(arr, n, sizeof(suncomplextype), arnoldi_compare);
-
-      // Update the original arrays
-      for (int i = 0; i < n; i++) {
-          wr[i] = arr[i].real;
-          wi[i] = arr[i].imag;
-      }
-
-      // Cleanup
-      free(arr);
-    }
-
-    // Print eigenvalues
-    printf("\nEigenvalues:\n");
-    for (int i = 0; i < n; i++) {
-        if (wi[i] == 0.0) {
-            printf("%f\n", wr[i]); // sunrealtype eigenvalue
-        } else {
-            printf("%f + %fi\n", wr[i], wi[i]); // suncomplextype eigenvalue
-        }
-    }
-
-    dom_eig.real = wr[0];
-    dom_eig.imag = wi[0];
-
-    return dom_eig;
-}
-
 /*---------------------------------------------------------------
   arnoldi_CheckNVector:
 
   This routine checks if all required vector operations are
-  present.  If any of them is missing it returns SUNFALSE.
+  present. If any of them is missing it returns SUNFALSE.
   ---------------------------------------------------------------*/
 sunbooleantype arnoldi_CheckNVector(N_Vector tmpl)
 { // TO DO: check required vector operations
@@ -257,15 +346,15 @@ sunbooleantype arnoldi_CheckNVector(N_Vector tmpl)
 }
 
 // Function to calculate the magnitude of a suncomplextype number
-double arnoldi_magnitude(const suncomplextype *c) {
+sunrealtype arnoldi_Magnitude(const suncomplextype *c) {
     return sqrt(c->real * c->real + c->imag * c->imag);
 }
 
 // Comparison function for qsort
-int arnoldi_compare(const void *a, const void *b) {
+int arnoldi_Compare(const void *a, const void *b) {
     const suncomplextype *c1 = (const suncomplextype *)a;
     const suncomplextype *c2 = (const suncomplextype *)b;
-    double mag1 = arnoldi_magnitude(c1);
-    double mag2 = arnoldi_magnitude(c2);
+    sunrealtype mag1 = arnoldi_Magnitude(c1);
+    sunrealtype mag2 = arnoldi_Magnitude(c2);
     return (mag2 > mag1) - (mag2 < mag1); // Descending order
 }
