b1f7daab08b862262754aea020c0bd130e176fec
Cody Balos <balos1@llnl.gov>
2025-06-28 00:17:08 -0700

    change long int to long with preprocess function

--- bindings/pysundials/generate.py
+++ bindings/pysundials/generate.py
@@ -59,8 +59,13 @@ def strip_sundials_export(code):
     return code.replace("SUNDIALS_EXPORT", "")
 
 
+def change_long_int_to_long(code):
+    return code.replace("long int", "long")
+
+
 def preprocess_header(code):
     code = strip_sundials_export(code)
+    code = change_long_int_to_long(code)
     return code
 
 
