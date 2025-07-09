c77e5da565c5e92e4e9d0968cd04f17acb2ede6d
David J. Gardner <gardner48@llnl.gov>
2025-07-09 10:32:59 -0700

    separate out aa and orth allocations

---
+++ src/kinsol/kinsol_aa.c
@@ -0,0 +1,185 @@
+/* -----------------------------------------------------------------------------
+ * Programmer(s): David J. Gardner @ LLNL
+ * -----------------------------------------------------------------------------
+ * SUNDIALS Copyright Start
+ * Copyright (c) 2002-2025, Lawrence Livermore National Security
+ * and Southern Methodist University.
+ * All rights reserved.
+ *
+ * See the top-level LICENSE and NOTICE files for details.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SUNDIALS Copyright End
+ * -----------------------------------------------------------------------------
+ * Anderson acceleration utilities
+ * ---------------------------------------------------------------------------*/
+
+#include "sundials/sundials_nvector.h"
+#include "sundials/sundials_types.h"
+
+#include "kinsol_impl.h"
+
+int KINInitAA(KINMem kin_mem)
+{
+  // Do we need to (re)allocate the AA workspace?
+  sunbooleantype allocate = kin_mem->kin_m_aa > kin_mem->kin_m_aa_alloc;
+
+  if (allocate)
+  {
+    // Free any existing workspace allocations
+    KINFreeAA(kin_mem);
+
+    // Template vector for creating clones
+    N_Vector tmpl = kin_mem->kin_unew;
+
+    // Update the AA workspace size
+    kin_mem->kin_m_aa_alloc = kin_mem->kin_m_aa;
+
+    // Array of acceleration weights
+    kin_mem->kin_gamma_aa =
+      (sunrealtype*)malloc(kin_mem->kin_m_aa * sizeof(sunrealtype));
+    if (kin_mem->kin_gamma_aa == NULL)
+    {
+      KINFreeAA(kin_mem);
+      KINProcessError(kin_mem, 0, __LINE__, __func__, __FILE__, MSG_MEM_FAIL);
+      return KIN_MEM_FAIL;
+    }
+
+    // R matrix for QR factorization
+    kin_mem->kin_R_aa = (sunrealtype*)malloc(
+      (kin_mem->kin_m_aa * kin_mem->kin_m_aa) * sizeof(sunrealtype));
+    if (kin_mem->kin_R_aa == NULL)
+    {
+      KINFreeAA(kin_mem);
+      KINProcessError(kin_mem, 0, __LINE__, __func__, __FILE__, MSG_MEM_FAIL);
+      return KIN_MEM_FAIL;
+    }
+
+    // Q matrix for QR factorization
+    kin_mem->kin_q_aa = N_VCloneVectorArray((int)kin_mem->kin_m_aa, tmpl);
+    if (kin_mem->kin_q_aa == NULL)
+    {
+      KINFreeAA(kin_mem);
+      KINProcessError(kin_mem, 0, __LINE__, __func__, __FILE__, MSG_MEM_FAIL);
+      return KIN_MEM_FAIL;
+    }
+
+    // History of residual vector differences
+    kin_mem->kin_df_aa = N_VCloneVectorArray((int)kin_mem->kin_m_aa, tmpl);
+    if (kin_mem->kin_df_aa == NULL)
+    {
+      KINFreeAA(kin_mem);
+      KINProcessError(kin_mem, 0, __LINE__, __func__, __FILE__, MSG_MEM_FAIL);
+      return KIN_MEM_FAIL;
+    }
+
+    // History of fixed point function vector differences
+    kin_mem->kin_dg_aa = N_VCloneVectorArray((int)kin_mem->kin_m_aa, tmpl);
+    if (kin_mem->kin_dg_aa == NULL)
+    {
+      KINFreeAA(kin_mem);
+      KINProcessError(kin_mem, 0, __LINE__, __func__, __FILE__, MSG_MEM_FAIL);
+      return KIN_MEM_FAIL;
+    }
+
+    // Previous residual vector, F(u_{i-1}) = G(u_{i-1}) - u_{i-1}
+    kin_mem->kin_fold_aa = N_VClone(tmpl);
+    if (kin_mem->kin_fold_aa == NULL)
+    {
+      KINFreeAA(kin_mem);
+      KINProcessError(kin_mem, 0, __LINE__, __func__, __FILE__, MSG_MEM_FAIL);
+      return KIN_MEM_FAIL;
+    }
+
+    // Previous fixed point function vector, G(u_{i-1})
+    kin_mem->kin_gold_aa = N_VClone(tmpl);
+    if (kin_mem->kin_gold_aa == NULL)
+    {
+      KINFreeAA(kin_mem);
+      KINProcessError(kin_mem, 0, __LINE__, __func__, __FILE__, MSG_MEM_FAIL);
+      return KIN_MEM_FAIL;
+    }
+
+    // Workspace array for fused operation constants
+    kin_mem->kin_cv =
+      (sunrealtype*)malloc(2 * (kin_mem->kin_m_aa + 1) * sizeof(sunrealtype));
+    if (kin_mem->kin_cv == NULL)
+    {
+      KINFreeAA(kin_mem);
+      KINProcessError(kin_mem, 0, __LINE__, __func__, __FILE__, MSG_MEM_FAIL);
+      return KIN_MEM_FAIL;
+    }
+
+    // Workspace array for fused operation vectors
+    kin_mem->kin_Xv =
+      (N_Vector*)malloc(2 * (kin_mem->kin_m_aa + 1) * sizeof(N_Vector));
+    if (kin_mem->kin_Xv == NULL)
+    {
+      KINFreeAA(kin_mem);
+      KINProcessError(kin_mem, 0, __LINE__, __func__, __FILE__, MSG_MEM_FAIL);
+      return KIN_MEM_FAIL;
+    }
+  }
+
+  return KIN_SUCCESS;
+}
+
+void KINFreeAA(KINMem kin_mem)
+{
+  if (kin_mem->kin_gamma_aa)
+  {
+    free(kin_mem->kin_gamma_aa);
+    kin_mem->kin_gamma_aa = NULL;
+  }
+
+  if (kin_mem->kin_R_aa)
+  {
+    free(kin_mem->kin_R_aa);
+    kin_mem->kin_R_aa = NULL;
+  }
+
+  if (kin_mem->kin_q_aa)
+  {
+    N_VDestroyVectorArray(kin_mem->kin_q_aa, (int)kin_mem->kin_m_aa);
+    kin_mem->kin_q_aa = NULL;
+  }
+
+  if (kin_mem->kin_df_aa)
+  {
+    N_VDestroyVectorArray(kin_mem->kin_df_aa, (int)kin_mem->kin_m_aa);
+    kin_mem->kin_df_aa = NULL;
+  }
+
+  if (kin_mem->kin_dg_aa)
+  {
+    N_VDestroyVectorArray(kin_mem->kin_dg_aa, (int)kin_mem->kin_m_aa);
+    kin_mem->kin_dg_aa = NULL;
+  }
+
+  if (kin_mem->kin_fold_aa)
+  {
+    N_VDestroy(kin_mem->kin_fold_aa);
+    kin_mem->kin_fold_aa = NULL;
+  }
+
+  if (kin_mem->kin_gold_aa)
+  {
+    N_VDestroy(kin_mem->kin_gold_aa);
+    kin_mem->kin_gold_aa = NULL;
+  }
+
+  if (kin_mem->kin_cv)
+  {
+    free(kin_mem->kin_cv);
+    kin_mem->kin_cv = NULL;
+  }
+
+  if (kin_mem->kin_Xv)
+  {
+    free(kin_mem->kin_Xv);
+    kin_mem->kin_Xv = NULL;
+  }
+
+  // Reset AA workspace size
+  kin_mem->kin_m_aa_alloc = 0;
+}
---
+++ src/kinsol/kinsol_orth.c
@@ -0,0 +1,142 @@
+/* -----------------------------------------------------------------------------
+ * Programmer(s): David J. Gardner @ LLNL
+ * -----------------------------------------------------------------------------
+ * SUNDIALS Copyright Start
+ * Copyright (c) 2002-2025, Lawrence Livermore National Security
+ * and Southern Methodist University.
+ * All rights reserved.
+ *
+ * See the top-level LICENSE and NOTICE files for details.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SUNDIALS Copyright End
+ * -----------------------------------------------------------------------------
+ * Orthogonalization utilities
+ * ---------------------------------------------------------------------------*/
+
+#include "sundials/sundials_nvector.h"
+#include "sundials/sundials_types.h"
+
+#include "kinsol_impl.h"
+
+int KINInitOrth(KINMem kin_mem)
+{
+  // Do we need to (re)allocate the orthogonalization workspace?
+  sunbooleantype allocate = kin_mem->kin_m_aa > kin_mem->kin_orth_aa_alloc;
+
+  if (allocate)
+  {
+    // Free any existing workspace allocations
+    KINFreeOrth(kin_mem);
+
+    // Template vector for creating clones
+    N_Vector tmpl = kin_mem->kin_unew;
+
+    // Update the AA workspace size
+    kin_mem->kin_orth_aa_alloc = kin_mem->kin_m_aa;
+
+    // Structure of orthogonalization data for QR solve
+    kin_mem->kin_qr_data = (SUNQRData)malloc(sizeof(*kin_mem->kin_qr_data));
+    if (kin_mem->kin_qr_data == NULL)
+    {
+      KINFreeOrth(kin_mem);
+      KINProcessError(kin_mem, 0, __LINE__, __func__, __FILE__, MSG_MEM_FAIL);
+      return KIN_MEM_FAIL;
+    }
+
+    if (kin_mem->kin_orth_aa != KIN_ORTH_MGS)
+    {
+      kin_mem->kin_vtemp3 = N_VClone(tmpl); // Orth owns
+      if (kin_mem->kin_vtemp3 == NULL)
+      {
+        KINFreeOrth(kin_mem);
+        KINProcessError(kin_mem, 0, __LINE__, __func__, __FILE__, MSG_MEM_FAIL);
+        return KIN_MEM_FAIL;
+      }
+    }
+
+    if (kin_mem->kin_orth_aa == KIN_ORTH_ICWY)
+    {
+      // T matrix for ICWY
+      kin_mem->kin_T_aa = (sunrealtype*)malloc(
+        ((kin_mem->kin_m_aa * kin_mem->kin_m_aa)) * sizeof(sunrealtype));
+      if (kin_mem->kin_T_aa == NULL)
+      {
+        KINFreeOrth(kin_mem);
+        KINProcessError(kin_mem, 0, __LINE__, __func__, __FILE__, MSG_MEM_FAIL);
+        return KIN_MEM_FAIL;
+      }
+    }
+  }
+
+  // Does the vector support dot product with single buffer reductions
+  kin_mem->kin_dot_prod_sb = SUNFALSE;
+  if ((kin_mem->kin_unew->ops->nvdotprodlocal ||
+       kin_mem->kin_unew->ops->nvdotprodmultilocal) &&
+      kin_mem->kin_unew->ops->nvdotprodmultiallreduce)
+  {
+    kin_mem->kin_dot_prod_sb = SUNTRUE;
+  }
+
+  // Initialize the QRData and set the QRAdd function
+  if (kin_mem->kin_orth_aa == KIN_ORTH_MGS)
+  {
+    kin_mem->kin_qr_func        = (SUNQRAddFn)SUNQRAdd_MGS;
+    kin_mem->kin_qr_data->vtemp = kin_mem->kin_vtemp2; // KINSOL owns
+  }
+  else if (kin_mem->kin_orth_aa == KIN_ORTH_ICWY)
+  {
+    if (kin_mem->kin_dot_prod_sb)
+    {
+      kin_mem->kin_qr_func = (SUNQRAddFn)SUNQRAdd_ICWY_SB;
+    }
+    else { kin_mem->kin_qr_func = (SUNQRAddFn)SUNQRAdd_ICWY; }
+    kin_mem->kin_qr_data->vtemp      = kin_mem->kin_vtemp2; // KINSOL owns
+    kin_mem->kin_qr_data->vtemp2     = kin_mem->kin_vtemp3; // Orth owns
+    kin_mem->kin_qr_data->temp_array = kin_mem->kin_T_aa;   // Orth owns
+  }
+  else if (kin_mem->kin_orth_aa == KIN_ORTH_CGS2)
+  {
+    kin_mem->kin_qr_func             = (SUNQRAddFn)SUNQRAdd_CGS2;
+    kin_mem->kin_qr_data->vtemp      = kin_mem->kin_vtemp2; // KINSOL owns
+    kin_mem->kin_qr_data->vtemp2     = kin_mem->kin_vtemp3; // Orth owns
+    kin_mem->kin_qr_data->temp_array = kin_mem->kin_cv;     // AA owns
+  }
+  else if (kin_mem->kin_orth_aa == KIN_ORTH_DCGS2)
+  {
+    if (kin_mem->kin_dot_prod_sb)
+    {
+      kin_mem->kin_qr_func = (SUNQRAddFn)SUNQRAdd_DCGS2_SB;
+    }
+    else { kin_mem->kin_qr_func = (SUNQRAddFn)SUNQRAdd_DCGS2; }
+    kin_mem->kin_qr_data->vtemp      = kin_mem->kin_vtemp2; // KINSOL owns
+    kin_mem->kin_qr_data->vtemp2     = kin_mem->kin_vtemp3; // Orth owns
+    kin_mem->kin_qr_data->temp_array = kin_mem->kin_cv;     // AA owns
+  }
+
+  return KIN_SUCCESS;
+}
+
+void KINFreeOrth(KINMem kin_mem)
+{
+  if (kin_mem->kin_qr_data)
+  {
+    free(kin_mem->kin_qr_data);
+    kin_mem->kin_qr_data = NULL;
+  }
+
+  if (kin_mem->kin_vtemp3)
+  {
+    N_VDestroy(kin_mem->kin_vtemp3);
+    kin_mem->kin_vtemp3 = NULL;
+  }
+
+  if (kin_mem->kin_T_aa)
+  {
+    free(kin_mem->kin_T_aa);
+    kin_mem->kin_T_aa = NULL;
+  }
+
+  // Reset AA workspace size
+  kin_mem->kin_orth_aa_alloc = 0;
+}
