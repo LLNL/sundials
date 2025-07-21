1cc17fdac726c30a2cbfdc6b52c890682f3a3c80
Daniel R. Reynolds <dreynolds@umbc.edu>
2025-07-21 08:48:12 -0400

    Removed unused variables

--- src/sunlinsol/magmadense/sunlinsol_magmadense.cpp
+++ src/sunlinsol/magmadense/sunlinsol_magmadense.cpp
@@ -283,9 +283,7 @@ static SUNErrCode setFromCommandLine_MagmaDense(SUNLinearSolver S,
 
   for (int idx = 1; idx < argc; idx++)
   {
-    SUNErrCode sunretval;
-    int j, retval;
-    sunbooleantype arg_used = SUNFALSE;
+    int retval;
 
     /* skip command-line arguments that do not begin with correct prefix */
     if (strncmp(argv[idx], prefix, strlen(prefix)) != 0) { continue; }
