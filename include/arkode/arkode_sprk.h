919bf8812367027ccfae0c58cf3efcad8fa84a62
Cody Balos <balos1@llnl.gov>
2025-10-02 11:56:51 -0700

    fix enum min/max values and comment about why

--- include/arkode/arkode_sprk.h
+++ include/arkode/arkode_sprk.h
@@ -30,6 +30,11 @@ extern "C" {
 enum ARKODE_SPRKMethodID
 {
   ARKODE_SPRK_NONE      = -1, /* ensure enum is signed int */
+  /* WARNING:  ARKODE_MIN_SPRK_NUM must come after the first entry, ARKODE_SPRK_EULER_1_1,
+     because Python enums will only expose the member that is defined first. Due to
+     this and how pybind/nanobind handle the enums, if we defined ARKODE_MRI_NUM first,
+     then ARKODE_SPRK_EULER_1_1 would not be usable from the module scope (the MIN/MAX) entries
+     will still be usable when accessing through the IntEnum object, but not from module scope. */
   ARKODE_MIN_SPRK_NUM   = 0,
   ARKODE_SPRK_EULER_1_1 = 0,
   ARKODE_SPRK_LEAPFROG_2_2,
