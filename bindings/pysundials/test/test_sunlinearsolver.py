f06f585c1df24647dadb43db80e79c5eaa713649
Cody Balos <balos1@llnl.gov>
2025-10-03 16:49:45 -0700

    add linear solver smoke tests

--- bindings/pysundials/test/test_sunlinearsolver.py
+++ bindings/pysundials/test/test_sunlinearsolver.py
@@ -14,3 +14,62 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # SUNDIALS Copyright End
 # -----------------------------------------------------------------
+
+import pytest
+from fixtures import *
+from pysundials.core import *
+
+def test_smoke_create_dense(sunctx, nvec):
+	A = SUNMatrixView.Create(SUNDenseMatrix(2, 2, sunctx.get()))
+	LS = SUNLinearSolverView.Create(SUNLinSol_Dense(nvec.get(), A.get(), sunctx.get()))
+	assert LS is not None
+
+def test_smoke_create_band(sunctx, nvec):
+	A = SUNMatrixView.Create(SUNBandMatrix(2, 1, 1, sunctx.get()))
+	LS = SUNLinearSolverView.Create(SUNLinSol_Band(nvec.get(), A.get(), sunctx.get()))
+	assert LS is not None
+
+def test_smoke_create_spgmr(sunctx, nvec):
+	# SUNLinSol_SPGMR expects (N_Vector, int, int, SUNContext)
+	LS = SUNLinearSolverView.Create(SUNLinSol_SPGMR(nvec.get(), SUN_PREC_NONE, 0, sunctx.get()))
+	assert LS is not None
+
+def test_smoke_create_pcg(sunctx, nvec):
+	# SUNLinSol_PCG expects (N_Vector, int, SUNContext)
+	LS = SUNLinearSolverView.Create(SUNLinSol_PCG(nvec.get(), SUN_PREC_NONE, 0, sunctx.get()))
+	assert LS is not None
+
+def test_smoke_create_spbcgs(sunctx, nvec):
+	# SUNLinSol_SPBCGS expects (N_Vector, int, SUNContext)
+	LS = SUNLinearSolverView.Create(SUNLinSol_SPBCGS(nvec.get(), SUN_PREC_NONE, 0, sunctx.get()))
+	assert LS is not None
+
+def test_smoke_create_sptfqmr(sunctx, nvec):
+	# SUNLinSol_SPTFQMR expects (N_Vector, int, SUNContext)
+	LS = SUNLinearSolverView.Create(SUNLinSol_SPTFQMR(nvec.get(), SUN_PREC_NONE, 0, sunctx.get()))
+	assert LS is not None
+
+def test_smoke_get_type_and_id(sunctx, nvec):
+	A = SUNMatrixView.Create(SUNDenseMatrix(2, 2, sunctx.get()))
+	LS = SUNLinearSolverView.Create(SUNLinSol_Dense(nvec.get(), A.get(), sunctx.get()))
+	typ = SUNLinSolGetType(LS.get())
+	id_ = SUNLinSolGetID(LS.get())
+	assert isinstance(typ, int)
+	assert isinstance(id_, int)
+
+def test_smoke_initialize_setup(sunctx, nvec):
+	A = SUNMatrixView.Create(SUNDenseMatrix(2, 2, sunctx.get()))
+	LS = SUNLinearSolverView.Create(SUNLinSol_Dense(nvec.get(), A.get(), sunctx.get()))
+	ret_init = SUNLinSolInitialize(LS.get())
+	ret_setup = SUNLinSolSetup(LS.get(), A.get())
+	assert isinstance(ret_init, int)
+	assert isinstance(ret_setup, int)
+
+def test_smoke_num_iters_resnorm_lastflag(sunctx, nvec):
+    LS = SUNLinearSolverView.Create(SUNLinSol_SPGMR(nvec.get(), 0, 0, sunctx.get()))
+    niters = SUNLinSolNumIters(LS.get())
+    resnorm = SUNLinSolResNorm(LS.get())
+    lastflag = SUNLinSolLastFlag(LS.get())
+    assert isinstance(niters, int)
+    assert isinstance(resnorm, float)
+    assert isinstance(lastflag, int)
