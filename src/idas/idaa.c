be1e7dafdc995f9b81f9107fe13d1efe843d1d40
David J. Gardner <gardner48@llnl.gov>
2025-10-06 23:08:45 -0700

    apply formatting

--- src/idas/idaa.c
+++ src/idas/idaa.c
@@ -715,7 +715,7 @@ int IDACreateB(void* ida_mem, int* which)
   new_IDAB_mem->ida_rhsQ  = NULL;
   new_IDAB_mem->ida_rhsQS = NULL;
 
-  new_IDAB_mem->ida_user_data = NULL;
+  new_IDAB_mem->ida_user_data     = NULL;
   new_IDAB_mem->ida_own_user_data = SUNFALSE;
 
   new_IDAB_mem->ida_lmem  = NULL;
